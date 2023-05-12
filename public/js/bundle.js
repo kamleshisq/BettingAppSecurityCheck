da8cff4c1eb1839aca77781adac514db7b2af13d
jayesh khuman <isquare.jayesh@gmail.com>
2023-05-12 15:23:18 +0530

    dianamic

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6303,7 +6303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50093" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55408" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
