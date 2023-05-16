325919943f61071bf6b93eefa43fbe1a10a9ce57
jayesh khuman <isquare.jayesh@gmail.com>
2023-05-16 16:49:11 +0530

    dianamic 38

--- controller/viewController.js
+++ controller/viewController.js
@@ -335,8 +335,8 @@ exports.APIcall = catchAsync(async(req, res, next) => {
             "operatorId": "sheldon",
             "partnerId": "SHPID01",
             "platformId": "DESKTOP",
-            "userId": `6438f3b5d2eb67c8f67fe0651`,
-            "username": "user11"
+            "userId": `6438f3b5d2eb67c8f67fe06`,
+            "username": "userDemo"
            })
 
     })
