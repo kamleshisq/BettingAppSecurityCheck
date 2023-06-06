c23d84b480941c67ad03d883abcb1f2cfbb338b2
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-06 16:02:32 +0530

    commmit

--- utils/verify.js
+++ utils/verify.js
@@ -6,9 +6,10 @@ function verify(sig, pub, body){
 const signature = sig; // The signature you want to decrypt and verify
 const publicKey = pub; // The public key for verification
 let x = JSON.stringify(body)
+console.log(x)
 // Convert the signature and public key from base64 to buffers
 const signatureBuffer = Buffer.from(signature, 'base64');
-console.log(signatureBuffer)
+// console.log(signatureBuffer)
 // Create a verifier object using the public key
 const verifier = crypto.createVerify('RSA-SHA256');
 verifier.update(x); // Replace 'data to verify' with the actual data you want to verify against the signature
