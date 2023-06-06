021e82cd1b120fb932c99e030b7a686407b16fa8
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-06 15:42:57 +0530

    commmit

--- controller/walletController.js
+++ controller/walletController.js
@@ -16,11 +16,11 @@ exports.consoleBodyAndURL = catchAsync(async(req, res, next) => {
     console.log(req.body)
     console.log(req.originalUrl)
     console.log(req.headers)
-    // let x  = req.body
-    // const publicKey = readPem("public.pem")
-    // console.log(publicKey)
-    // let result = verify(req.headers.signature, publicKey, x)
-    // console.log(result, 564)
+    let x  = req.body
+    const publicKey = readPem("public.pem")
+    console.log(publicKey)
+    let result = verify(req.headers.signature, publicKey, x)
+    console.log(result, 564)
     // if(result){
         next()
     // }else{
