6fd0415de694af3af37ca51b73d50076e25f1197
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-13 18:39:23 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -826,11 +826,11 @@ exports.getSportList = catchAsync(async(req, res, next) => {
     fetch(fullUrl, {
         method: 'GET'
     })
-    .then(res =>console.log(res.text()))
-    // .then(result => {
-    //     console.log(result)
-    //     // res.status(200).json({
-    //     //     result
-    //     // })
-    // })
+    .then(res =>res.text())
+    .then(result => {
+        console.log(result)
+        // res.status(200).json({
+        //     result
+        // })
+    })
 })
