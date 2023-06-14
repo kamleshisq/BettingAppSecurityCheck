27f2812ac18bffb8f1cb1f6b40a1f2f219f8d41a
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-14 19:14:34 +0530

    commit

--- server.js
+++ server.js
@@ -737,7 +737,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on("marketId", async(data) => {
-        // console.log(data)
+        console.log(data)
         const result = await marketDetailsBymarketID(data)
         let finalResult = result.data.items
         socket.emit("marketId", finalResult)
