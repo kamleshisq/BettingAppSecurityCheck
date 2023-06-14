eb753ee3a10db3bde36a33997475a226a7925660
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-14 11:03:59 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -865,7 +865,7 @@ exports.getFootballData = catchAsync(async(req, res, next) => {
 });
 
 exports.getmarketDetailsByMarketId = catchAsync(async(req, res, next) => {
-    var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
+    var fullUrl = 'http://dbm9.com:3000/dream/m/get_odds';
     fetch(fullUrl, {
         method: 'POST',
         body:["1.215076435", "4.1686476623-BM"]
