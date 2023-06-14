30e3c85b66178b5f492c1db2d65ef1d17ffdf18d
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-14 17:55:43 +0530

    commit

--- utils/sha256.js
+++ utils/sha256.js
@@ -9,16 +9,4 @@ function signTextWithPrivateKey(privateKey, text) {
   // console.log(signature)
   return signature;
 }
-
-// Example usage
-// const privateKey = `-----BEGIN PRIVATE KEY-----
-// MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDmvMgN9Z07zfnU
-// ...
-// z8yHvXJsmY7zSPVTdBZiYkECAwEAAQ==
-// -----END PRIVATE KEY-----`;
-
-// const textToSign = 'Hello, World!';
-// const signature = signTextWithPrivateKey(privateKey, textToSign);
-// console.log('Signature:', signature);
-
 module.exports = signTextWithPrivateKey
