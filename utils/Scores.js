6ad42cc7953e6400b6fbc64311e5b53f18d014f9
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-15 19:18:33 +0530

    commit

--- utils/Scores.js
+++ utils/Scores.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function getScore(id){
     let DATA
-    console.log(id)
+    // console.log(id)
     var fullUrl = `https://score-session.dbm9.com/api/get-live-score?event_id=${id}`;
     await fetch(fullUrl, {
         method: 'GET'
