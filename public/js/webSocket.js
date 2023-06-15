14b3d8d3225a039361e521a1c99d1b5ec4ead994
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-15 11:08:06 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2331,17 +2331,9 @@ socket.on('connect', () => {
             $(document).ready(function() {
           
                 $(".0").each(function() {
-                //   const update = data.items.find(item => item.odds.some(item1 => item1.selectionId === this.id))
-                //   const foundItem = data.items.find(item => item.odds.find(odd => odd.selectionId === this.id));
                 let id = this.id
                 const foundItem = data.items.find(item => item.odds.find(odd => odd.selectionId == id));
-                //   const foundMainArray = data.find(mainArray =>
-                //     mainArray.find(mainItem => mainItem.odds.some(item => item.selectionId === selectionIdToFind))
-                //   );
-                  console.log(this.id)
-                  console.log(foundItem)
-                this.innerHTML = ` updating`
-
+                this.innerHTML = `${foundItem.odds[0].backPrice3}, ${foundItem.odds[0].layPrice3}`
                 });
             })
         })
