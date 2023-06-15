d29589a9c1c8f4e0b1a8f6615708e1e9016e7f6a
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-15 17:12:18 +0530

    commit

--- server.js
+++ server.js
@@ -773,7 +773,7 @@ io.on('connection', (socket) => {
 
     socket.on("eventId", async(data) => {
         let matchScore = await scores(data)
-        console.log(matchScore)
+        console.log(matchScore.JSON.parse(matchScore))
         socket.emit("eventId", matchScore)
     })
 
