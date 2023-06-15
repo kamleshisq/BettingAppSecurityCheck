98f82d246a10f0074e96a3b4c809cdf763fd3924
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-15 18:33:10 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2383,9 +2383,10 @@ socket.on('connect', () => {
                 for(let i = 0; i < 3; i++){
                     if($(this).hasClass(`${i}`)){
                         // this.innerHTML = `<button id="123">${foundItem.odds[i].backPrice1}</button>, <button id="123">${foundItem.odds[i].backPrice2}</button>, <button id="123">${foundItem.odds[i].backPrice3}</button>`
-                        document.getElementById(`${this.id}0`).innerHTML = `${foundItem.odds[i].layPrice1}`
-                        document.getElementById(`${this.id}1`).innerHTML = `${foundItem.odds[i].layPrice2}`
-                        document.getElementById(`${this.id}2`).innerHTML = `${foundItem.odds[i].layPrice3}`
+                        document.getElementById(`${this.id}0`).innerHTML = `${foundItem.odds[i].backSize1}`
+                        document.getElementById(`${this.id}1`).innerHTML = `${foundItem.odds[i].backSize2}`
+                        document.getElementById(`${this.id}2`).innerHTML = `${foundItem.odds[i].backSize3}`
+
                     }
                 }
                 });
@@ -2396,9 +2397,9 @@ socket.on('connect', () => {
                     for(let i = 0; i < 3; i++){
                         if($(this).hasClass(`${i}`)){
                             // this.innerHTML = `<button id="123">${foundItem.odds[i].layPrice1}</button>, <button id="123">${foundItem.odds[i].layPrice2}</button>, <button id="123">${foundItem.odds[i].layPrice3}</button>`
-                            document.getElementById(`${this.id}0`).innerHTML = `${foundItem.odds[i].backSize1}`
-                            document.getElementById(`${this.id}1`).innerHTML = `${foundItem.odds[i].backSize2}`
-                            document.getElementById(`${this.id}2`).innerHTML = `${foundItem.odds[i].backSize3}`
+                            document.getElementById(`${this.id}0`).innerHTML = `${foundItem.odds[i].layPrice1}`
+                            document.getElementById(`${this.id}1`).innerHTML = `${foundItem.odds[i].layPrice2}`
+                            document.getElementById(`${this.id}2`).innerHTML = `${foundItem.odds[i].layPrice3}`
                         }
                     }
                     });
