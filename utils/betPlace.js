bde0b456255fe451e614561332dfed7af3160312
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-16 16:15:09 +0530

    commit

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -1,6 +1,17 @@
+const userModel = require('../model/userModel');
+const betmodel = require('../model/betmodel');
+const cricketAndOtherSport = require('../utils/getSportAndCricketList');
 
 async function placeBet(data){
-    console.log(data)
+    const sportData = await cricketAndOtherSport()
+    let gameList
+    if(data.data.spoetId == 4){
+        gameList = sportData[0].gameList[0].eventList
+
+    }
+    let liveBetGame = gameList.find(item => item.eventData.eventId == data.data.eventId);
+    console.log(liveBetGame)
+
 }
 
 module.exports = placeBet
