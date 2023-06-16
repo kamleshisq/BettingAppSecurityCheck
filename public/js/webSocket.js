50091399016d2a29e429da6bd42613f0e94171ad
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-16 14:33:48 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2472,9 +2472,7 @@ socket.on('connect', () => {
         let modleName = $(".popup")
         let form = $(modleName).find('#bet-form')
         let formOddsbuttonId = form.find('input[name = "title"]').attr("class");
-        console.log(formOddsbuttonId)
         let odds = $(`#${formOddsbuttonId}`).text()
-        console.log(odds , 123)
         if(form.find('input[name = "odds"]').val() != odds && form.find('input[name = "odds"]').val() != ''){
             alert('odds value change')
             form.find('input[name = "odds"]').val(odds)
