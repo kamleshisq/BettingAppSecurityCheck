2849682d006359114814e6e6ae36a30efe2e9ea4
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-19 15:53:01 +0530

    commit

--- crones/crones.js
+++ crones/crones.js
@@ -7,7 +7,7 @@ const userModel = require("../model/userModel");
 module.exports = () => {
     cron.schedule('*/5 * * * * *', async() => {
         const openBets = await betModel.find({status:"OPEN"});
-        // const marketIds = [...new Set(openBets.map(item => item.name))];
-
+        const marketIds = [...new Set(openBets.map(item => item.marketId))];
+        console.log(marketIds)
     })
 }
