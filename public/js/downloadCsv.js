54cb74de9f0d04e63daea25dcc96ee15d55ce06a
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-23 12:44:33 +0530

    commit

---
+++ public/js/downloadCsv.js
@@ -0,0 +1,9 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    console.log("working New Script")
+    // const table = document.getElementById('yourTableId'); 
+
+    // if (table) {
+    //   const csvContent = convertToCSV(table);
+    //   downloadCSV(csvContent, 'table_data.csv');
+    // }
+  });
