84fc0b3ed6e32b827d0c1da2547c33c70a6fd0c1
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-23 16:08:59 +0530

    commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(fileUpload());
 app.use(express.urlencoded({ extended:true, limit: '10kb'}));
 app.use(cookieParser());
 app.use(middlewares);
-crone();
+// crone();
 // app.get("/", (req, res)=> {
     //     res.send("hello word")
     // })
