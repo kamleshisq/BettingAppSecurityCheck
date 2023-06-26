c90dad04d9a804107193cd65a5ddaccd6fa1130a
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-26 12:19:01 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -975,13 +975,9 @@ exports.getExchangePage = catchAsync(async(req, res, next) => {
         },
         { LiveCricket: [], marketArray: [] }
       );
-    //   console.log(LiveCricket)
-      const marketdetails1 = await getmarketDetails(marketArray)
-    //   console.log(marketArray)
     res.status(200).render("./user/exchange",{
         user,
-        LiveCricket,
-        marketdetails1
+        LiveCricket
     })
 });
 
