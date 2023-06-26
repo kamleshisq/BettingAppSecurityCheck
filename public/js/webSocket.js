7130b2408e5d643be3bf7a27b02ae3a9b8e3bc62
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-26 11:39:07 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2748,7 +2748,32 @@ socket.on('connect', () => {
         marketId()
 
         socket.on("marketId", async(data) => {
-            console.log(data)
+            $(document).ready(function() {
+          
+                $(".0").each(function() {
+                let id = this.id
+                const foundItem = data.items.find(item => item.odds.find(odd => odd.selectionId == id));
+                this.innerHTML = `${foundItem.odds[0].backPrice1}, ${foundItem.odds[0].layPrice1}`
+                });
+
+                $(".1").each(function() {
+                    let id = this.id
+                    const foundItem = data.items.find(item => item.odds.find(odd => odd.selectionId == id));
+                    this.innerHTML = `${foundItem.odds[1].backPrice1}, ${foundItem.odds[1].layPrice1}`
+                });
+
+                $(".2").each(function() {
+                    let id = this.id
+                    const foundItem = data.items.find(item => item.odds.find(odd => odd.selectionId == id));
+                    this.innerHTML = `${foundItem.odds[2].backPrice1}, ${foundItem.odds[2].layPrice1}`
+                });
+
+            })
+
+            $(document).on("click", ".click", function(){
+                window.location.href = `/exchange_sports/live_match?id=${this.id}`
+            })
+            
         })
     }
 
