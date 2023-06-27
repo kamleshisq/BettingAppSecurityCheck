c3ab1768d0ce0d94c8cb2a623fba396f745471cf
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-27 14:58:14 +0530

    commit

--- controller/userController.js
+++ controller/userController.js
@@ -40,12 +40,12 @@ exports.createUser = catchAsync(async(req, res, next)=>{
         req.body.parentUsers = req.currentUser.parentUsers
     }
     req.body.parentUsers.push(req.currentUser._id)
-    console.log(req.body)
-    // const newUser = await User.create(req.body);
-    // res.status(200).json({
-    //     status:'success',
-    //     User: newUser
-    // })
+    // console.log(req.body)
+    const newUser = await User.create(req.body);
+    res.status(200).json({
+        status:'success',
+        User: newUser
+    })
 })
 
 exports.deletUser = catchAsync(async(req, res, next) =>{
