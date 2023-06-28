b9aa5cfdb34f6da7e736c4a6bea1e2c3b30403ac
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-28 12:18:55 +0530

    commit

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const walletController = require("../controller/walletController")
 
 router.post('/balance', walletController.consoleBodyAndURL, walletController.getUserBalancebyiD);
-router.post('/betrequest', walletController.consoleBodyAndURL, walletController.betrequest);
+router.post('/betrequest', walletController.betrequest);
 router.post('/resultrequest', walletController.consoleBodyAndURL, walletController.betResult);
 router.post('/rollbackrequest', walletController.consoleBodyAndURL, walletController.rollBack)
 
