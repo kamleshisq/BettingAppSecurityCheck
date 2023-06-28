38960841f04a40214712f3f2882fed596522fc4d
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-28 15:44:51 +0530

    commit

--- controller/verticalMenuController.js
+++ controller/verticalMenuController.js
@@ -7,6 +7,7 @@ exports.createVerticalMenu = catchAsync(async(req, res, next) => {
     if((req.body.num * 1) > (allMenu.length + 1) ){
         req.body.num = (allMenu.length + 1)
     }
+    console.log(req.body)
     let newMenu = await verticalMenuModel.create(req.body);
     res.status(200).json({
         status:"success",
