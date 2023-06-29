83ae6c557607eac2e8d4f81aa8b3585a5779796b
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 18:37:13 +0530

    commit

--- controller/horizontalMenuController.js
+++ controller/horizontalMenuController.js
@@ -34,32 +34,34 @@ exports.createHorizontalMenu = catchAsybc(async(req, res, next) => {
 
 
 exports.updateHorizontalMenu = catchAsybc(async(req, res, next) => {
-    let check = await horizontalMenuModel.findById(req.body.id);
-    if(!(check.Number == req.body.Number)){
-        console.log("not same")
-    }
-    if(req.files){
-        if(req.files.file.mimetype.startsWith('image')){
-            const image = req.files.file
-            // console.log(logo)
-            image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
-                if(err) 
-                return next(new AppError("Something went wrong please try again later", 400))
-                // console.log(err)
-            })
-        }else{
-            return next(new AppError("Please upload an image file", 400))
-        }
-        req.body.icon = req.body.menuName
-    }
-    if(req.body.check){
-        req.body.status = true
-    }else{
-        req.body.status = false
-    }
-    // const updatedMenu = await horizontalMenuModel.findByIdAndUpdate(req.body.id, req.body)
-    res.status(200).json({
-        status:'success',
-        // updatedMenu
-    })
+    console.log(req.body)
+    // let check = await horizontalMenuModel.findById(req.body.id);
+    // if(!(check.Number == req.body.Number)){
+    //     let newNumber = req.body.Number
+    //     await horizontalMenuModel.findOneAndUpdate({Number:newNumber}, {Number:check.Number})
+    // }
+    // if(req.files){
+    //     if(req.files.file.mimetype.startsWith('image')){
+    //         const image = req.files.file
+    //         // console.log(logo)
+    //         image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
+    //             if(err) 
+    //             return next(new AppError("Something went wrong please try again later", 400))
+    //             // console.log(err)
+    //         })
+    //     }else{
+    //         return next(new AppError("Please upload an image file", 400))
+    //     }
+    //     req.body.icon = req.body.menuName
+    // }
+    // if(req.body.check){
+    //     req.body.status = true
+    // }else{
+    //     req.body.status = false
+    // }
+    // // const updatedMenu = await horizontalMenuModel.findByIdAndUpdate(req.body.id, req.body)
+    // res.status(200).json({
+    //     status:'success',
+    //     updatedMenu
+    // })
 })
