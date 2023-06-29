eab8d3880dcdc9fe45dc3968adc4c9f4dcdda2ff
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 12:39:51 +0530

    commit

--- controller/horizontalMenuController.js
+++ controller/horizontalMenuController.js
@@ -9,7 +9,9 @@ exports.createHorizontalMenu = catchAsybc(async(req, res, next) => {
             const image = req.files.Icon
             // console.log(logo)
             image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
-                if(err) return next(new AppError("Something went wrong please try again later", 400))
+                if(err) 
+                // return next(new AppError("Something went wrong please try again later", 400))
+                console.log(err)
             })
         }else{
             return next(new AppError("Please upload an image file", 400))
