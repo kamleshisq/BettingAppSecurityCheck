9f324960a727d94d86b6d92654e6ec4335acff72
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 12:31:40 +0530

    commit

--- controller/promotionController.js
+++ controller/promotionController.js
@@ -4,20 +4,38 @@ const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createPosition = catchAsync(async(req, res, next) => {
-    console.log(req.files)
-    // if(req.files){
-    //     if(req.files.Icon.mimetype.startsWith('image')){
-    //         const image = req.files.Icon
-    //         // console.log(logo)
-    //         image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
-    //             if(err) return next(new AppError("Something went wrong please try again later", 400))
-    //         })
-    //     }else{
-    //         return next(new AppError("Please upload an Image file", 400))
-    //     }
-    //     req.body.Icon = req.body.menuName
-    // }
-    // console.log(req.body)
+    let data = {}
+    if(req.files){
+        if(req.files.image.mimetype.startsWith('image')){
+            data.video = false
+            const image = req.files.image
+            // console.log(logo)
+            image.mv(`public/img/${req.body.position}.png`, (err)=>{
+                if(err) return next(new AppError("Something went wrong please try again later", 400))
+            })
+        }else if(req.files.image.mimetype.startsWith('video')){
+            // console.log(req.files)
+            data.video = true
+            // return next(new AppError('Please Upload An Image', 404))
+            const image = req.files.image
+            // console.log(logo)
+            image.mv(`public/img/${req.body.position}.mp4`, (err)=>{
+                if(err) return next(new AppError("Something went wrong please try again later", 400))
+            })
+        }else{
+            return next(new AppError("Please upload a midea file", 400))
+        }
+
+        data.position = req.body.position
+        data.Image = req.body.position
+        data.link = req.body.link
+        const newPosition = await promotion.create(data)
+        res.status(200).json({
+            status:"success",
+            newPosition
+        })
+
+    }
 
 });
 
