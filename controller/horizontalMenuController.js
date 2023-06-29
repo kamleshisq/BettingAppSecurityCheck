e636ed3414cff6be899e32ee2b2a9d8a756d2838
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 17:35:33 +0530

    commit

--- controller/horizontalMenuController.js
+++ controller/horizontalMenuController.js
@@ -36,20 +36,28 @@ exports.createHorizontalMenu = catchAsybc(async(req, res, next) => {
 exports.updateHorizontalMenu = catchAsybc(async(req, rws, next) => {
     console.log(req.body, "WORKING")
     console.log(req.files, 45)
-    // if(req.files){
-    //     if(req.files.Icon.mimetype.startsWith('image')){
-    //         const image = req.files.file
-    //         // console.log(logo)
-    //         image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
-    //             if(err) 
-    //             return next(new AppError("Something went wrong please try again later", 400))
-    //             // console.log(err)
-    //         })
-    //     }else{
-    //         return next(new AppError("Please upload an image file", 400))
-    //     }
-    //     req.body.icon = req.body.menuName
-    // }
-    // if(req.body.status){
-    // }
+    if(req.files){
+        if(req.files.Icon.mimetype.startsWith('image')){
+            const image = req.files.file
+            // console.log(logo)
+            image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
+                if(err) 
+                return next(new AppError("Something went wrong please try again later", 400))
+                // console.log(err)
+            })
+        }else{
+            return next(new AppError("Please upload an image file", 400))
+        }
+        req.body.icon = req.body.menuName
+    }
+    if(req.body.check){
+        req.body.status = true
+    }else{
+        req.body.status = false
+    }
+    const updatedMenu = await horizontalMenuModel.findByIdAndUpdate(req.body.id, req.body)
+    res.status(200).json({
+        status:'success',
+        updatedMenu
+    })
 })
