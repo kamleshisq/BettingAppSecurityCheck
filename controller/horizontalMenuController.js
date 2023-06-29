d88ae196ee7fe5eb6200a40499a0856cee89c010
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 12:37:19 +0530

    commit

--- controller/horizontalMenuController.js
+++ controller/horizontalMenuController.js
@@ -4,34 +4,17 @@ const AppError = require("../utils/AppError");
 
 
 exports.createHorizontalMenu = catchAsybc(async(req, res, next) => {
-    let data = {}
     if(req.files){
-        if(req.files.image.mimetype.startsWith('image')){
-            data.video = false
-            const image = req.files.image
+        if(req.files.Icon.mimetype.startsWith('image')){
+            const image = req.files.Icon
             // console.log(logo)
-            image.mv(`public/img/${req.body.position}.png`, (err)=>{
-                if(err) return next(new AppError("Something went wrong please try again later", 400))
-            })
-        }else if(req.files.image.mimetype.startsWith('video')){
-            // console.log(req.files)
-            data.video = true
-            // return next(new AppError('Please Upload An Image', 404))
-            const image = req.files.image
-            // console.log(logo)
-            image.mv(`public/img/${req.body.position}.mp4`, (err)=>{
+            image.mv(`public/imgForHMenu/${req.body.menuName}.png`, (err)=>{
                 if(err) return next(new AppError("Something went wrong please try again later", 400))
             })
         }else{
-            return next(new AppError("Please upload a midea file", 400))
+            return next(new AppError("Please upload an image file", 400))
         }
-
-        data.position = req.body.position
-        data.Image = req.body.position
-        data.link = req.body.link
-        const newPosition = await promotion.create(data)
-        res.status(200).json({
-            status:"success",
-            newPosition
-        })}
+        req.body.Icon = req.body.menuName
+    }
+    console.log(req.body)
 })
