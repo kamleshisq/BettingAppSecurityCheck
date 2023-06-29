15f37a8cd4915cdf63a7c1ec1d93e0e606742cac
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-29 11:56:27 +0530

    commit

--- controller/horizontalMenuController.js
+++ controller/horizontalMenuController.js
@@ -4,7 +4,35 @@ const AppError = require("../utils/AppError");
 
 
 exports.createHorizontalMenu = catchAsybc(async(req, res, next) => {
-    console.log(req.body, 98765430)
-    console.log(req.files, 1)
-    console.log(req.file, 2)
+    console.log(req.files.Icon.mimetype)
+    // let data = {}
+    // if(req.files){
+    //     if(req.files.image.mimetype.startsWith('image')){
+    //         data.video = false
+    //         const image = req.files.image
+    //         // console.log(logo)
+    //         image.mv(`public/img/${req.body.position}.png`, (err)=>{
+    //             if(err) return next(new AppError("Something went wrong please try again later", 400))
+    //         })
+    //     }else if(req.files.image.mimetype.startsWith('video')){
+    //         // console.log(req.files)
+    //         data.video = true
+    //         // return next(new AppError('Please Upload An Image', 404))
+    //         const image = req.files.image
+    //         // console.log(logo)
+    //         image.mv(`public/img/${req.body.position}.mp4`, (err)=>{
+    //             if(err) return next(new AppError("Something went wrong please try again later", 400))
+    //         })
+    //     }else{
+    //         return next(new AppError("Please upload a midea file", 400))
+    //     }
+
+    //     data.position = req.body.position
+    //     data.Image = req.body.position
+    //     data.link = req.body.link
+    //     const newPosition = await promotion.create(data)
+    //     res.status(200).json({
+    //         status:"success",
+    //         newPosition
+    //     })}
 })
