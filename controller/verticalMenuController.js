99353cfc97c748eaff122010112e1baa73d2482e
jayesh khuman <isquare.jayesh@gmail.com>
2023-06-30 12:45:46 +0530

    Commit

--- controller/verticalMenuController.js
+++ controller/verticalMenuController.js
@@ -4,9 +4,7 @@ const AppError = require("../utils/AppError");
 
 exports.createVerticalMenu = catchAsync(async(req, res, next) => {
     let allMenu = await verticalMenuModel.find()
-    if((req.body.num * 1) > (allMenu.length + 1) ){
-        req.body.num = (allMenu.length + 1)
-    }
+    req.body.num = (allMenu.length + 1)
     let newMenu
     try{
         newMenu = await verticalMenuModel.create(req.body);
