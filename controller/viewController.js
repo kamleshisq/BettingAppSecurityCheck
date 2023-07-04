f7b24ae4875d2fa84ec7c53ced2f59f17c2700c5
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-04 18:31:26 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -317,6 +317,7 @@ exports.userdashboard = catchAsync(async(req, res, next) => {
     const data = await promotionModel.find();
     let verticalMenus = await verticalMenuModel.find();
     const banner = await bannerModel.find()
+    console.log(banner)
     let banner1 = banner.find(item => item.position == "homePosition1") 
     console.log(banner1)
     // console.log(verticalMenus)
