7e8a9de2eb0172e58f0ffbc8c015f9b2f1f7981c
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-04 11:51:19 +0530

    commit

--- controller/pagesController.js
+++ controller/pagesController.js
@@ -6,4 +6,21 @@ const pageModel = require("../model/pageModel");
 exports.createPage = catchAsync(async(req, res, next) =>{
     console.log(req.body)
     console.log(req.files)
+    const ejsFile = req.files.ejsFile;
+    const fileName = `${req.body.Name}`
+    ejsFile.mv('views/pages' + fileName, async(err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({
+            status:'error',
+            message:"File upload failed."
+        })
+      } else {
+        let page = await pageModel.create(req.body)
+        res.status(200).json({
+            status:"success",
+            message:"Page created successfully"
+        })
+      }
+    });
 })
