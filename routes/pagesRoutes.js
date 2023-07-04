1fd1671bf404d3c9b892f6a002f803d7c91ab188
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-04 11:29:19 +0530

    commit

--- routes/pagesRoutes.js
+++ routes/pagesRoutes.js
@@ -4,3 +4,7 @@ const pagesController = require("../controller/pagesController");
 const authController = require("../controller/authorizationController");
 
 router.post("/createPage", authController.isProtected, authController.isAdmin, pagesController.createPage);
+
+
+
+module.exports = router
