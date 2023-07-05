f0d590b786b65bf7d3793fd23f2da13cc989cf75
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-05 10:14:07 +0530

    commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7035,7 +7035,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50283" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49925" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
