a067237debd016d7130fd95ee098bfd817822473
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-05 11:41:01 +0530

    commit

--- controller/sliderController.js
+++ controller/sliderController.js
@@ -3,5 +3,11 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require("../utils/AppError");
 
 exports.createNewSlider = catchAsync(async(req, res, next) => {
-    console.log(req.body)
+    let newSlider = await sliderModel.create(req.body)
+    if(newSlider){
+        res.status(200).json({
+            status:"success",
+            newSlider
+        })
+    }
 })
