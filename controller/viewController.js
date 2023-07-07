e534453b0f266e3283c13eec67ea994bc97b0650
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-07 14:31:06 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -1352,9 +1352,8 @@ exports.getUserExchangePage = catchAsync(async(req, res, next) => {
     let Tennis = sportListData[1].gameList.find(item => item.sport_name === "Tennis")
     footBall = footBall.eventList.sort((a, b) => a.eventData.time - b.eventData.time);
     Tennis = Tennis.eventList.sort((a, b) => a.eventData.time - b.eventData.time);
-    console.log(footBall)
-    let liveFootBall = footBall.eventList.filter(item => item.eventData.type === "IN_PLAY");
-    let liveTennis = Tennis.eventList.filter(item => item.eventData.type === "IN_PLAY")
+    let liveFootBall = eventList.filter(item => item.eventData.type === "IN_PLAY");
+    let liveTennis = eventList.filter(item => item.eventData.type === "IN_PLAY")
     let upcomintCricket = cricket.filter(item => item.eventData.type != "IN_PLAY")
     let upcomintFootball = cricket.filter(item => item.eventData.type != "IN_PLAY")
     let upcomintTennis = cricket.filter(item => item.eventData.type != "IN_PLAY")
