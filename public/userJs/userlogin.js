1e694dd8c1456d1488a4fa7045cd3b63b6af69bc
5815 <95901865+5815@users.noreply.github.com>
2023-07-10 17:53:42 +0530

    c

--- public/userJs/userlogin.js
+++ public/userJs/userlogin.js
@@ -1,26 +1,39 @@
 // console.log("js")
-// import axios from "axios";
-if(document.querySelector('.userLoginForm')){
-    document.querySelector('.userLoginForm').addEventListener('submit', e =>{
-    e.preventDefault();
-    const userName = document.getElementById('uname').value;
-    const password = document.getElementById('password').value;
+import axios from "axios";
+$(document).on('submit', ".myloginmodl-form-dv", function(e){
+    e.preventDefault()
+    let form = $(this)[0];
+    let fd = new FormData(form);
+    let data = Object.fromEntries(fd.entries());
     // console.log(userName, password)
-    const userLogin = async(userName, password) => {
-        $.ajax({
-            url:'/api/v1/auth/userLogin',
-            type:'post',
-            data:{userName, password},
-            success:function(data){
-                // console.log(data)
-                sessionStorage.setItem('loginUserDetails',JSON.stringify(data.data.user));
-                setTimeout(alert('login successfully'),window.location.href = '/')
-            },
-            error:function(error){
-                (error)
-                setTimeout(alert(error.responseJSON.message),3000)
+    const userLogin = async(data) => {
+        try{
+            const res = await axios({
+                method: 'POST',
+                url: '/api/v1/auth/userLogin',
+                data
+            });
+            if(res.data.status === 'success'){
+                alert('Logged in successfully!!!!');
+                sessionStorage.setItem('loginUserDetails',JSON.stringify(res.data.data.user));
+                sessionStorage.setItem('roles',JSON.stringify(res.data.data.roles))
+                // sessionStorage.setItem('grandParentDetails','{"parent_id":"0"}');
+                // console.log(res.data)
+                if(res.data.count){
+                    window.setTimeout(()=>{
+                        location.assign('/updatePassWord')
+                    }, 100)
+                }else{
+                    setTimeout(function() {
+                        location.reload();
+                      }, 300);
+                }
             }
-        })
+    
+        }catch(err){
+            console.log(err)
+        setTimeout(alert(err.response.data.message), 1500)
+        }
     }
-    userLogin(userName, password)
-})};
+    userLogin(data)
+});
