f5df6bc818652848949a2ae588b1d0275fb3a1a6
5815 <95901865+5815@users.noreply.github.com>
2023-07-11 11:39:23 +0530

    cmt

--- server.js
+++ server.js
@@ -1278,6 +1278,7 @@ io.on('connection', (socket) => {
         }).then(res => res.json())
         .then(Data =>{
             socket.emit('UserUpdatePass', Data)
+            console.log(Data)
         })
     })
 
