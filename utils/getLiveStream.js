137269e7373efc20e127f0bbbd8aae909199a7c2
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-14 15:50:25 +0530

    commit

--- utils/getLiveStream.js
+++ utils/getLiveStream.js
@@ -4,7 +4,7 @@ async function getLiveStream(id){
     let DATA
     // console.log(id)
     let body = {
-            "ipv4":"ollscores.com",
+            "ipv4":"172.105.58.243",
             "channel":`${id}`
         };
     var fullUrl = `https://score-session.dbm9.com/api/tv-stream-2`;
