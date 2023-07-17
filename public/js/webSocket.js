11fd019bf1559d413f36cd0e6d96b53155dec15b
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-17 16:44:11 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3459,6 +3459,7 @@ socket.on('connect', () => {
 
         socket.on("checkPage", async(data) => {
             let form = $(`#updatePages`)
+            console.log(data)
             form.attr('id', data._id);
             form.find('input[name = "heading"]').attr('value', data.heading)
             form.find('textarea[name = "details"]').html(data.details)
