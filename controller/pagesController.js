3706295cb94fa97e98aed1533e61c2a7b678c8d6
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-17 15:40:38 +0530

    commit

--- controller/pagesController.js
+++ controller/pagesController.js
@@ -4,28 +4,5 @@ const pageModel = require("../model/pageModel");
 
 
 exports.createPage = catchAsync(async(req, res, next) =>{
-    const ejsFile = req.files.ejsFile;
-    if(ejsFile.name.split('.')[1] == 'ejs'){
-        const fileName = `${req.body.Name}.ejs`
-        ejsFile.mv('views/pages/' + fileName, async(err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({
-                status:'error',
-                message:"File upload failed."
-            })
-          } else {
-            let page = await pageModel.create(req.body)
-            res.status(200).json({
-                status:"success",
-                message:"Page created successfully"
-            })
-          }
-        });
-    }else{
-        res.status(200).json({
-            status:'error',
-            message:"Please upload EJS file"
-        })
-    }
+    console.log(req.body)
 })
