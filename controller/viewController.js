2d08e3745a75a7e16dc2f0943ab92aecd75cec1b
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-17 18:42:38 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -1535,7 +1535,8 @@ exports.getExchangePageIn = catchAsync(async(req, res, next) => {
     }
     const liveStream = await liveStreameData(match.eventData.channelId, ipv4)
     const betLimit = await betLimitModel.find()
-    console.log(match.marketList)
+    console.log(match)
+    // let session = match.
     let SportLimits = betLimit.find(item => item.type === "Sport")
     res.status(200).render("./userSideEjs/userMatchDetails/main",{
         user: req.currentUser,
