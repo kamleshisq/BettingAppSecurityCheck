b805d40213987bd46eadb915dafe2cb9568f7009
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-18 18:34:40 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3492,7 +3492,7 @@ socket.on('connect', () => {
             data.title = title[0].innerText.trim();
             data.eventId = title[0].id
             data.odds = odd[0].innerText.trim();
-            data.secId = odd[0].id.slice(-1)
+            data.secId = odd[0].id.slice(0,-1)
             console.log(data)
         })
         
