01ce34d3c92fdfe243b7975c556229f572c02c18
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-19 13:06:43 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3388,6 +3388,7 @@ socket.on('connect', () => {
                 id = id.slice(0, -1);
                 let section = null;
                 data.items.some(item => {
+                    console.log(item.odds)
                     section = item.odds.find(odd => odd.selectionId == id);
                     return section !== undefined;
                 });
