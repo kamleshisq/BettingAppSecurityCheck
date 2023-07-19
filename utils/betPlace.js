69085c68f0a939ad2bb6dadc829672287c4db42b
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-19 11:40:11 +0530

    commit

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -28,11 +28,11 @@ async function placeBet(data){
     if(data.data.spoetId){
         betLimit = await betLimitModel.findOne({type:"Sport"})
     }
-    if(betLimit.min_stake > (data.data.stake * 1) ){
+    if(betLimit.min_stake > parseFloat(data.data.stake) ){
         return `Invalide stake, Please play with atleast minimum stake (${betLimit.min_stake})`
-    }else if(betLimit.max_stake < (data.data.stake * 1)){
+    }else if(betLimit.max_stake < parseFloat(data.data.stake)){
         return `Invalide stake, Please play with atmost maximum stake (${betLimit.max_stake})`
-    }else if(betLimit.max_odd < (data.data.odds) * 1 ){
+    }else if(betLimit.max_odd < parseFloat(data.data.odds)){
         return `Invalide odds valur, Please play with atmost maximum odds (${betLimit.max_odd})`
     }
 
@@ -70,67 +70,90 @@ async function placeBet(data){
               break;
             }
       }}
-let runnersData = JSON.parse(marketDetails.runners)
-let betOn = runnersData.find(item => item.secId == data.data.secId)
-    let betPlaceData = {
+let betPlaceData = {}
+if(!marketDetails.runners){
+    betPlaceData = {
         userId : data.LOGINDATA.LOGINUSER._id,
         userName : data.LOGINDATA.LOGINUSER.userName,
         transactionId : `${data.LOGINDATA.LOGINUSER.userName}${uniqueToken}`,
         date : Date.now(),
-        oddValue : data.data.odds * 1,
-        Stake : data.data.stake * 1,
+        oddValue : parseFloat(data.data.odds),
+        Stake : parseFloat(data.data.stake),
         status : "OPEN",
-        returns : -(data.data.stake),
+        returns : -parseFloat(data.data.stake),
         role_type : data.LOGINDATA.LOGINUSER.role_type,
         match : data.data.title,
         betType : bettype,
         event : liveBetGame.eventData.league,
         gameId : liveBetGame.eventData.sportId,
         marketName : marketDetails.title,
-        selectionName : betOn.runner,
+        selectionName : marketDetails.title,
         marketId : data.data.market,
         secId : data.data.secId
     }
+}else{
+    let runnersData = JSON.parse(marketDetails.runners)
+    let betOn = runnersData.find(item => item.secId == data.data.secId)
+        betPlaceData = {
+            userId : data.LOGINDATA.LOGINUSER._id,
+            userName : data.LOGINDATA.LOGINUSER.userName,
+            transactionId : `${data.LOGINDATA.LOGINUSER.userName}${uniqueToken}`,
+            date : Date.now(),
+            oddValue : parseFloat(data.data.odds),
+            Stake : parseFloat(data.data.stake),
+            status : "OPEN",
+            returns : -parseFloat(data.data.stake),
+            role_type : data.LOGINDATA.LOGINUSER.role_type,
+            match : data.data.title,
+            betType : bettype,
+            event : liveBetGame.eventData.league,
+            gameId : liveBetGame.eventData.sportId,
+            marketName : marketDetails.title,
+            selectionName : betOn.runner,
+            marketId : data.data.market,
+            secId : data.data.secId
+        }
+}
     let description = `Bet for ${data.data.title}/stake = ${data.data.stake}`
     let description2 = `Bet for ${data.data.title}/stake = ${data.data.stake}/user = ${data.LOGINDATA.LOGINUSER.userName} `
 
     let Acc = {
         "user_id":data.LOGINDATA.LOGINUSER._id,
         "description": description,
-        "creditDebitamount" : -data.data.stake,
-        "balance" : check.availableBalance - data.data.stake,
+        "creditDebitamount" : -parseFloat(data.data.stake),
+        "balance" : check.availableBalance - parseFloat(data.data.stake),
         "date" : Date.now(),
         "userName" : data.LOGINDATA.LOGINUSER.userName,
         "role_type" : data.LOGINDATA.LOGINUSER.role_type,
         "Remark":"-",
-        "stake": data.data.stake,
+        "stake": parseFloat(data.data.stake),
         "transactionId":`${data.LOGINDATA.LOGINUSER.userName}${uniqueToken}`
     }
     await betmodel.create(betPlaceData)
     await accountStatementByUserModel.create(Acc)
     let parentUser
-    let user = await userModel.findByIdAndUpdate(data.LOGINDATA.LOGINUSER._id, {$inc:{balance: -data.data.stake, availableBalance: -data.data.stake, myPL: -data.data.stake, Bets : 1, exposure:data.data.stake}})
+    let user = await userModel.findByIdAndUpdate(data.LOGINDATA.LOGINUSER._id, {$inc:{balance: -parseFloat(data.data.stake), availableBalance: -parseFloat(data.data.stake), myPL: -parseFloat(data.data.stake), Bets : 1, exposure:parseFloat(data.data.stake)}})
     if(!user){
         return "There is no user with that id"
     }
     if(user.parentUsers.length < 2){
         // await userModel.updateMany({ _id: { $in: user.parentUsers } }, {$inc:{balance: -data.data.stake, downlineBalance: -data.data.stake}})
         // parentUser = await userModel.findByIdAndUpdate(user.parentUsers[0], {$inc:{availableBalance:data.data.stake}})
-        parentUser = await userModel.findByIdAndUpdate(user.parentUsers[0],{$inc:{availableBalance:data.data.stake, downlineBalance: -data.data.stake}})
+        parentUser = await userModel.findByIdAndUpdate(user.parentUsers[0],{$inc:{availableBalance:parseFloat(data.data.stake), downlineBalance: -parseFloat(data.data.stake)}})
     }else{
-        await userModel.updateMany({ _id: { $in: user.parentUsers.slice(2) } }, {$inc:{balance: -data.data.stake, downlineBalance: -data.data.stake}})
-        parentUser = await userModel.findByIdAndUpdate(user.parentUsers[1], {$inc:{availableBalance:data.data.stake, downlineBalance: -data.data.stake}})
+        await userModel.updateMany({ _id: { $in: user.parentUsers.slice(2) } }, {$inc:{balance: -parseFloat(data.data.stake), downlineBalance: -parseFloat(data.data.stake)}})
+        parentUser = await userModel.findByIdAndUpdate(user.parentUsers[1], {$inc:{availableBalance:parseFloat(data.data.stake), downlineBalance: -parseFloat(data.data.stake)}})
     }
     let Acc2 = {
         "user_id":parentUser._id,
         "description": description2,
-        "creditDebitamount" : data.data.stake,
-        "balance" : parentUser.availableBalance + (data.data.stake * 1),
+        "creditDebitamount" : parseFloat(data.data.stake),
+        "balance" : parentUser.availableBalance + parseFloat(data.data.stake),
         "date" : Date.now(),
         "userName" : parentUser.userName,
         "role_type" : parentUser.role_type,
         "Remark":"-",
-        "stake": data.data.stake,
+        "stake": parseFloat(data.data.stake),
         "transactionId":`${data.LOGINDATA.LOGINUSER.userName}${uniqueToken}Parent`
     }
     await accountStatementByUserModel.create(Acc2)
