b7b641531b26b6c797694b4055bb3397ec6f60e0
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-19 16:50:13 +0530

    commit

--- middleWares/middleware.js
+++ middleWares/middleware.js
@@ -8,21 +8,21 @@ const User = require("../model/userModel");
 
 
 const LoginLogs = catchAsync(async(req, res, next) => {
-    console.log(req.originalUrl)
+    //console.log(req.originalUrl)
     if(!req.originalUrl.startsWith("/api/v1") && !req.originalUrl.startsWith("/wallet")){
         const clientIP1 = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         let clientIP2 = clientIP1.split(":")
         let clientIP = clientIP2[clientIP2.length - 1]
         global.ip = clientIP
     }
-    // console.log(global.ip)
+    // //console.log(global.ip)
     
     if(req.originalUrl == "/api/v1/auth/login" ){
-        // console.log("working")
+        // //console.log("working")
         const id = await User.findOne({userName:req.body.userName})
-        // console.log(id)
+        // //console.log(id)
         if(!id){
-            // console.log(req.body)
+            // //console.log(req.body)
             return res.status(404).json({
                 status:"error",
                 message:"please provide a valide user name"
@@ -31,11 +31,11 @@ const LoginLogs = catchAsync(async(req, res, next) => {
         const userLog = await loginLogs.find({user_id:id._id})
         global._count = userLog.length
         global._admin = true
-        // console.log(global._count, global._admin)
+        // //console.log(global._count, global._admin)
     }else if (req.originalUrl == "/api/v1/auth/userLogin"){
         const id = await User.findOne({userName:req.body.userName})
         if(!id){
-            // console.log(req.body)
+            // //console.log(req.body)
             return res.status(404).json({
                 status:"error",
                 message:"please provide a valide user name"
@@ -56,11 +56,11 @@ const LoginLogs = catchAsync(async(req, res, next) => {
     //         }
     // }
     else if(req.originalUrl != "/" && req.originalUrl != "/adminLogin" && req.originalUrl != "/userlogin"){
-        console.log(req.cookies.JWT, "MIDDLEWARES")
+        //console.log(req.cookies.JWT, "MIDDLEWARES")
         if(req.cookies.JWT && !req.originalUrl.startsWith("/wallet")){
-            // console.log(global._loggedInToken)
+            // //console.log(global._loggedInToken)
             const login = await loginLogs.findOne({session_id:req.cookies.JWT, isOnline:true})
-            // console.log(req.cookies.JWT)
+            // //console.log(req.cookies.JWT)
             if(login == null){
                 return next()
             }
