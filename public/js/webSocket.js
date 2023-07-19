83456be3b7af338a9cdb0a70a733cd105d9ef1c5
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-19 10:49:57 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3497,7 +3497,8 @@ socket.on('connect', () => {
             data.market = market[0].id
             data.stake = document.getElementById("stakeSpan").innerText.trim();
             data.spoetId = this.id
-            socket.emit("betDetails", {data, LOGINDATA})
+            console.log(data)
+            // socket.emit("betDetails", {data, LOGINDATA})
         })
 
         socket.on("betDetails" , (data) => {
