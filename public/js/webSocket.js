3716c3551cc8b8e80385877bee7908d0d649b6ca
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-24 16:31:37 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3322,7 +3322,6 @@ socket.on('connect', () => {
             if(data != ""){
                 let score = JSON.parse(data)
                 let element = document.getElementsByClassName("score")
-                console.log(element)
                 for(let i = 0; i < element.length; i++){
                     element[i].innerHTML = score[0].data
                 }
