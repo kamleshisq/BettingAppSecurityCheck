76340a061509e8a856caa4ecd2d2481afaf2c11e
jayesh khuman <isquare.jayesh@gmail.com>
2023-07-24 13:06:24 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3526,11 +3526,11 @@ socket.on('connect', () => {
                 console.log('Button ID:', buttonId);
                 console.log(this.id.slice(-1))
                 console.log('Span Inner Text:', spanInnerText);
-                let elements = document.getElementsByClassName(`betOn${this.id.slice(-1)}`);
+                let elements = document.getElementsByClassName(`betOn${marketId.slice(-1)}`);
                 console.log(elements)
                 elements[0].innerHTML  = `Bet on  :${teamName}@${spanInnerText}`
-                elements[0].id = marketId;
-                var elements2 = document.getElementsByClassName(`oddsvalue${this.id.slice(-1)}`);
+                elements[0].id = marketId.slice(0,-1);
+                var elements2 = document.getElementsByClassName(`oddsvalue${marketId.slice(-1)}`);
                 elements2[0].innerHTML = spanInnerText
                 elements2[0].id = this.id;
             });
