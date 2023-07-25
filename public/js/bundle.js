86c1d1a01c24cf3497697daca1cbf17910683228
5815 <95901865+5815@users.noreply.github.com>
2023-07-25 21:37:59 +0530

    commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6268,7 +6268,7 @@ var createHorizontalMenu = /*#__PURE__*/function () {
         case 3:
           res = _context.sent;
           if (res.data.status === 'success') {
-            alert('Men created successfully!!!!');
+            alert('Menu created successfully!!!!');
             // $(".popup_body").removeClass("popup_body_show");
 
             window.setTimeout(function () {
@@ -7296,7 +7296,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49974" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50550" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
