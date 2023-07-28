b68841a3fc4fc4aa624497d3df4f2e9c3a0ee3ed
jk21085815 <isquare.jayesh@gmail.com>
2023-07-28 18:08:19 +0530

    commit

--- utils/getSportAndCricketList.js
+++ utils/getSportAndCricketList.js
@@ -17,7 +17,7 @@ async function getSportDATA(){
     ]
     let requests = urls.map(item => fetch(item.url, {
         method: 'GET'
-    }).then(data => data.text()));
+    }).then(data => console.log(data.text())));
     const data = await Promise.all(requests)
     return data
 }
