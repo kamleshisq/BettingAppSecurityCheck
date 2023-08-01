5c7506fdb285d2bb3e6bf2ff84883770ae4055e0
jk21085815 <isquare.jayesh@gmail.com>
2023-08-01 12:30:57 +0530

    commit

--- utils/getmarketsbymarketId.js
+++ utils/getmarketsbymarketId.js
@@ -3,32 +3,36 @@ const fetch = require("node-fetch")
 async function getmarketdetails(array){
     let Data
     let body = JSON.stringify(array);
-    var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
-    await fetch(fullUrl, {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
-            },
-        body:body 
-    })
-    .then(res =>res.json())
-    .then(result => {
-        Data = result
-    }).catch(error => {
-        console.error("Error occurred while parsing JSON:", error);
-        return fetch(fullUrl, {
+    try{
+
+        var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
+        await fetch(fullUrl, {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json',
                 'accept': 'application/json'
-                },
+            },
             body:body 
-        });
-    }).then(res => res.text())
-    .then(result => {
-      console.log(result); // Handle the response text from the second fetch
-    });
+        })
+        .then(res =>res.json())
+        .then(result => {
+            Data = result
+        })
+    }catch(err){
+        var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
+        await fetch(fullUrl, {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body:body 
+        })
+        .then(res =>res.text())
+        .then(result => {
+           console.log(result)
+        })
+    }
     return Data
 }
 
