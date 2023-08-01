8dd2c8b48d8ab143d1d376d9b31863b6fdc3d7a4
jk21085815 <isquare.jayesh@gmail.com>
2023-08-01 12:20:18 +0530

    commit

--- utils/getmarketsbymarketId.js
+++ utils/getmarketsbymarketId.js
@@ -15,6 +15,21 @@ async function getmarketdetails(array){
     .then(res =>res.json())
     .then(result => {
         Data = result
+    }).catch(error => {
+        console.error("Error occurred while parsing JSON:", error);
+        let body = JSON.stringify(array);
+    var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
+    await fetch(fullUrl, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+            },
+        body:body 
+    })
+    .then(res =>res.text())
+    .then(result => {
+       console.log(result)
     })
     return Data
 }
