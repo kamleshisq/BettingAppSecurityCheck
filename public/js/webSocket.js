3da25ae6f52d48b9794425ffacd600c0f611cc72
jk21085815 <isquare.jayesh@gmail.com>
2023-08-01 15:31:00 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -5746,6 +5746,16 @@ socket.on('connect', () => {
             form.find('input[name = "name"]').attr('value', data.name)
             form.find('textarea[name = "description"]').html(data.description)
             form.find('input[name = "name"]').attr('id', data._id)
+
+
+            ClassicEditor
+            .create(document.getElementById('detailsTextArea1'))
+            .then(editor => {
+              console.log('ClassicEditor was initialized', editor);
+            })
+            .catch(error => {
+              console.error('Error initializing ClassicEditor', error);
+            });
         })
     }
    
