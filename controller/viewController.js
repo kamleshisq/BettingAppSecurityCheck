aeafd23e4e0a5effd735cb81ce5a60c49b4bce37
jk21085815 <isquare.jayesh@gmail.com>
2023-08-02 19:17:55 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -1673,7 +1673,7 @@ exports.getExchangePageIn = catchAsync(async(req, res, next) => {
     } else {
     console.log("No 'src' attribute found in the iframe tag.");
     }
-    console.log(liveStream)
+    console.log(src_attribute_value)
     const betLimit = await betLimitModel.find()
     // console.log(match.marketList.goals)
     // let session = match.marketList.session.filter(item => {
