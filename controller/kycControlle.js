1e66067e3c4e2dbd276b38d8050dd3976c0707b9
jk21085815 <isquare.jayesh@gmail.com>
2023-08-03 18:35:56 +0530

    commit

--- controller/kycControlle.js
+++ controller/kycControlle.js
@@ -8,4 +8,24 @@ exports.uploadphoto = catchAsynch(async(req, res, next) => {
     console.log(req.files)
     console.log(req.body)
     console.log("req.body")
+    console.log(req.currentUser)
+    // if(req.files != null){
+    //     if(req.files.file.mimetype.startsWith('application/pdf')){
+    //         const image = req.files.file
+    //         // console.log(logo)
+    //         // req.body.backGroundImage = req.body.name.split(' ')[0]
+    //         image.mv(`public/sliderBackgroundImages/${req.current.name.split(' ')[0]}.png`, (err)=>{
+    //             if(err) 
+    //             return next(new AppError("Something went wrong please try again later", 400))
+    //         })
+    //         let newSlider = await sliderModel.create(req.body)
+    //         if(newSlider){
+    //             res.status(200).json({
+    //                 status:"success"
+    //             })
+    //         }
+    //     }else{
+    //         return next(new AppError("Please Upload an image", 404))
+    //     }
+    // }
 })
