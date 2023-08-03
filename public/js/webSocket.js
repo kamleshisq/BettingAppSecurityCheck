9407eac5a6260866d39691aad9ad1b3f2d2afc75
jk21085815 <isquare.jayesh@gmail.com>
2023-08-03 15:54:49 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -6042,7 +6042,7 @@ socket.on('connect', () => {
             e.preventDefault()
             let files = $("#formFileSm")[0].files[0];
             let name = $("#cardName").val()
-            let idNum = $('exampleInputPassword1').val()
+            let idNum = $('#exampleInputPassword1').val()
             console.log(idNum)
         })
     }
