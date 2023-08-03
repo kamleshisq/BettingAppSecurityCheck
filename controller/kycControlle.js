e7642c613b4a3dec0f7789448c6dc6a8dcb393aa
jk21085815 <isquare.jayesh@gmail.com>
2023-08-03 18:40:39 +0530

    commit

--- controller/kycControlle.js
+++ controller/kycControlle.js
@@ -9,23 +9,18 @@ exports.uploadphoto = catchAsynch(async(req, res, next) => {
     console.log(req.body)
     console.log("req.body")
     console.log(req.currentUser)
-    // if(req.files != null){
-    //     if(req.files.file.mimetype.startsWith('application/pdf')){
-    //         const image = req.files.file
-    //         // console.log(logo)
-    //         // req.body.backGroundImage = req.body.name.split(' ')[0]
-    //         image.mv(`public/sliderBackgroundImages/${req.current.name.split(' ')[0]}.png`, (err)=>{
-    //             if(err) 
-    //             return next(new AppError("Something went wrong please try again later", 400))
-    //         })
-    //         let newSlider = await sliderModel.create(req.body)
-    //         if(newSlider){
-    //             res.status(200).json({
-    //                 status:"success"
-    //             })
-    //         }
-    //     }else{
-    //         return next(new AppError("Please Upload an image", 404))
-    //     }
-    // }
+    if(req.files != null){
+        let data = {}
+        if(req.files.file.mimetype.startsWith('application/pdf')){
+            const image = req.files.file
+            // console.log(logo)
+            data.kycDoc = `${path.join(__dirname, documents)}/${req.currentUser.userName}.pdf`
+            image.mv(`${data.kycDoc}`, (err)=>{
+                if(err) 
+                return next(new AppError("Something went wrong please try again later", 400))
+            })
+        }else{
+            return next(new AppError("Please Upload an image", 404))
+        }
+    }
 })
