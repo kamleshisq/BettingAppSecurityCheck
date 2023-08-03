629ac0d96e7a7320a46b25a5aaa5d14070de5e27
jk21085815 <isquare.jayesh@gmail.com>
2023-08-03 16:46:17 +0530

    commit

--- server.js
+++ server.js
@@ -1560,9 +1560,9 @@ io.on('connection', (socket) => {
           }
           const bufferData = Buffer.from(` files: ${data.data.files}`, 'hex');
           const filePath = path.join( __dirname, 'documents'); 
-        //   console.log(`${data.LOGINDATA.LOGINUSER.userName}`)
-          console.log(`${filePath}/${data.LOGINDATA.LOGINUSER.userName}`)
-        //   saveBufferToFile(bufferData, filePath);
+        // //   console.log(`${data.LOGINDATA.LOGINUSER.userName}`)
+        //   console.log(`${filePath}/${data.LOGINDATA.LOGINUSER.userName}`)
+          saveBufferToFile(bufferData, filePath);
     })
 
     
