4c766f1f1b11bab26856ff63277e42271e8a32c9
jk21085815 <isquare.jayesh@gmail.com>
2023-08-04 11:15:47 +0530

    commit

--- server.js
+++ server.js
@@ -1567,7 +1567,7 @@ io.on('connection', (socket) => {
 
     socket.on('getPdf', async(data) => {
         console.log(data)
-        const fileName = `${data.LOGINUSER.userName}`; 
+        const fileName = `${data.LOGINDATA.LOGINUSER.userName}`; 
         const filePath = `/var/www/bettingApp/documents/${fileName}`;
         socket.emit('getPdf', filePath)
     })
