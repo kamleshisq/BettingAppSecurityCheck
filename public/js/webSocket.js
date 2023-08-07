be06e417a93a3a97abb11bd397d38e5a928dd955
jk21085815 <isquare.jayesh@gmail.com>
2023-08-07 10:16:30 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4305,8 +4305,13 @@ socket.on('connect', () => {
                         }
                         html += `
                       </div>
-                    </div>
-                    <div class="liv-casino-games-cards-txt2">`
+                    </div>`
+                    if(fevGames.includes(games[i]._id)){
+                        html += `<i id="${games[i]._id}" class="fa-regular fa-heart my-heart-icon fevoriteHeart fa-solid liked-star"></i>`
+                    }else{
+                        html  += `<i id="${games[i]._id}" class="fa-regular fa-heart my-heart-icon fevoriteHeart"></i>`
+                    }
+                    // `<div class="liv-casino-games-cards-txt2">`
                     console.log(LOGINDATA)
                     if(LOGINDATA.LOGINUSER === ""){
                       html +=  `<a class="liv-casino-games-cards-txt2-btn" data-bs-toggle="modal" data-bs-target="#exampleModalToggle">LOGIN TO CONTINUE</a>`
