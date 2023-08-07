01ea8e4a98ebd1bef3f6b7cfcefbb2c88dafe1c9
jk21085815 <isquare.jayesh@gmail.com>
2023-08-07 11:35:54 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -43,7 +43,7 @@ socket.on('connect', () => {
     socket.on("UserSideSEarchLive", async(data) => {
         let html = ""
         for(let i = 0; i < data.length; i++){
-            html += `<li><a class="demoname" href="/exchange_inPlay/match?id=${data.eventData.eventId}">${data.eventData.name}</a></li>`
+            html += `<li><a class="demoname" href="/exchange_inPlay/match?id=${data[i].eventData.eventId}">${data[i].eventData.name}</a></li>`
         }
         document.getElementById("demonames1").innerHTML = html
     })
