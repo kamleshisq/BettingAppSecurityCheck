8fcf85477db77563fdbc96a28077193c3f840012
jk21085815 <isquare.jayesh@gmail.com>
2023-08-07 11:22:01 +0530

    commit

--- server.js
+++ server.js
@@ -1618,13 +1618,13 @@ io.on('connection', (socket) => {
         let allData =  await getCrkAndAllData()
         let searchdtaa = []
         const cricket = allData[0].gameList[0].eventList
-        let LiveCricket = cricket.filter(item => item.eventData.type === "IN_PLAY")
+        let LiveCricket = cricket.filter(item => item.eventData.type === "IN_PLAY" && item.eventData.name.includes(data))
         let footBall = allData[1].gameList.find(item => item.sport_name === "Football")
         let Tennis = allData[1].gameList.find(item => item.sport_name === "Tennis")
         footBall = footBall.eventList
         Tennis = Tennis.eventList
-        let liveFootBall = footBall.filter(item => item.eventData.type === "IN_PLAY");
-        let liveTennis = Tennis.filter(item => item.eventData.type === "IN_PLAY")
+        let liveFootBall = footBall.filter(item => item.eventData.type === "IN_PLAY" && item.eventData.name.includes(data));
+        let liveTennis = Tennis.filter(item => item.eventData.type === "IN_PLAY" && item.eventData.name.includes(data))
         console.log(LiveCricket, liveFootBall, liveTennis)
         console.log(data)
     })
