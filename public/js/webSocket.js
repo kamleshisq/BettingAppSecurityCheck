fc1f5d5ad579d4e8e0b5d0558b02edd54faa8bda
jk21085815 <isquare.jayesh@gmail.com>
2023-08-07 11:46:21 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -5539,11 +5539,11 @@ socket.on('connect', () => {
                                         <i class="fa-solid fa-lock"></i>
                                       </span>`
                     }else if(foundItem.odds[0].backPrice1=="1,000.00"){
-                        this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data ">
+                        this.innerHTML = `<span class="tbl-td-bg-blu-spn mylock-data ">
                         <i class="fa-solid fa-lock"></i>
                       </span>`
                     }else{
-                        this.innerHTML = `<span class="tbl-td-bg-pich-spn">${foundItem.odds[0].backPrice1}</span>`
+                        this.innerHTML = `<span class="tbl-td-bg-blu-spn">${foundItem.odds[0].backPrice1}</span>`
                     }
                 });
 
@@ -5579,11 +5579,11 @@ socket.on('connect', () => {
                                         <i class="fa-solid fa-lock"></i>
                                       </span>`
                     }else if(foundItem.odds[1].backPrice1=="1,000.00"){
-                        this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data ">
+                        this.innerHTML = `<span class="tbl-td-bg-blu-spn mylock-data ">
                         <i class="fa-solid fa-lock"></i>
                       </span>`
                     }else{
-                        this.innerHTML = `<span class="tbl-td-bg-pich-spn">${foundItem.odds[1].backPrice1}</span>`
+                        this.innerHTML = `<span class="tbl-td-bg-blu-spn">${foundItem.odds[1].backPrice1}</span>`
                     }
                 });
 
@@ -5599,11 +5599,11 @@ socket.on('connect', () => {
                                         <i class="fa-solid fa-lock"></i>
                                       </span>`
                         }else if(foundItem.odds[2].backPrice1=="1,000.00"){
-                            this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data ">
+                            this.innerHTML = `<span class="tbl-td-bg-blu-spn mylock-data ">
                             <i class="fa-solid fa-lock"></i>
                           </span>`
                         }else{
-                            this.innerHTML = `<span class="tbl-td-bg-pich-spn">${foundItem.odds[2].backPrice1}</span>`
+                            this.innerHTML = `<span class="tbl-td-bg-blu-spn">${foundItem.odds[2].backPrice1}</span>`
                         }
                 });
 
