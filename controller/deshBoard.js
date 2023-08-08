f37c97ff1c3b44e3c175a43eaa131153f8c88475
jk21085815 <isquare.jayesh@gmail.com>
2023-08-08 14:15:31 +0530

    commit

--- controller/deshBoard.js
+++ controller/deshBoard.js
@@ -161,7 +161,7 @@ exports.dashboardData = catchAsync(async(req, res, next) => {
     
 
     // console.log(betCount)
-    const topPlayers = await User.find({Bets:{ $nin : [0, null, undefined] }}).limit(5).sort({Bets:-1})
+    const topPlayers = await User.find({Bets:{ $nin : [0, null, undefined] }, parentUsers : { $in: [req.currentUser.id] }}).limit(5).sort({Bets:-1})
     const dashboard = {};
     dashboard.roles = roles
     dashboard.users = users
