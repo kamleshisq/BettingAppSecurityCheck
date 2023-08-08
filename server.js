845d13504c85827325f5dd7d0339b9203878cdad
jk21085815 <isquare.jayesh@gmail.com>
2023-08-08 19:06:58 +0530

    commit

--- server.js
+++ server.js
@@ -1668,43 +1668,43 @@ io.on('connection', (socket) => {
 
     socket.on("chartMain", async(data) => {
         console.log(data)
-        // const currentDate = new Date();
-        // const tenDaysAgo = new Date();
-        // tenDaysAgo.setDate(currentDate.getDate() - 10);
+        const currentDate = new Date();
+        const tenDaysAgo = new Date();
+        tenDaysAgo.setDate(currentDate.getDate() - 10);
     
-        // const dateSequence = [];
-        // for (let i = 0; i < 10; i++) {
-        // const currentDate = new Date(tenDaysAgo);
-        // currentDate.setDate(tenDaysAgo.getDate() + i);
-        // dateSequence.push(currentDate.toDateString());
-        // }
+        const dateSequence = [];
+        for (let i = 0; i < 10; i++) {
+        const currentDate = new Date(tenDaysAgo);
+        currentDate.setDate(tenDaysAgo.getDate() + i);
+        dateSequence.push(currentDate.toDateString());
+        }
 
-        // let accountForGraph = await AccModel.aggregate([
-        //     {
-        //         $match: {
-        //           userName: data.LOGINUSER.userName,
-        //           date: {
-        //             $gte: tenDaysAgo,
-        //           },
-        //         },
-        //       },
-        //       {
-        //         $group: {
-        //           _id: {
-        //             year: { $year: '$date' },
-        //             month: { $month: '$date' },
-        //             day: { $dayOfMonth: '$date' },
-        //           },
-        //           totalIncome: {
-        //             $sum: '$creditDebitamount',
-        //           },
-        //           totalIncome2: {
-        //             $sum: { $abs: '$creditDebitamount' }
-        //           }
-        //         },
-        //       },
-        //   ]);
-        //     console.log(accountForGraph)
+        let accountForGraph = await AccModel.aggregate([
+            {
+                $match: {
+                  userName: data.LOGINUSER.userName,
+                  date: {
+                    $gte: tenDaysAgo,
+                  },
+                },
+              },
+              {
+                $group: {
+                  _id: {
+                    year: { $year: '$date' },
+                    month: { $month: '$date' },
+                    day: { $dayOfMonth: '$date' },
+                  },
+                  totalIncome: {
+                    $sum: '$creditDebitamount',
+                  },
+                  totalIncome2: {
+                    $sum: { $abs: '$creditDebitamount' }
+                  }
+                },
+              },
+          ]);
+            console.log(accountForGraph)
         //     const dataArray = JSON.parse(accountForGraph);
         //     const dates = dataArray.map(item => new Date(item._id.year, item._id.month - 1, item._id.day));
         //     const startDate = new Date(Math.min(...dates));
