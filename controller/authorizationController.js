c66b821db8365c8ed375fe1746bafd39909d0632
jk21085815 <isquare.jayesh@gmail.com>
2023-08-09 12:21:13 +0530

    commit

--- controller/authorizationController.js
+++ controller/authorizationController.js
@@ -120,8 +120,8 @@ exports.login = catchAsync (async(req, res, next) => {
 exports.checkPass = catchAsync(async(req, res, next) => {
     const user = await User.findOne({userName:req.currentUser.userName}).select('+password');
     console.log(user)
-    console.log(req.body.password)
-    const passcheck = await user.correctPassword(req.body.password, user.password)
+    console.log(req.body.Password)
+    const passcheck = await user.correctPassword(req.body.Password, user.password)
     console.log(passcheck)
     if(!user || !(passcheck)){
         res.status(404).json({
