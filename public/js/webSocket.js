282c5a7b052c2bd983ce01aba6aff19e65b2b8f3
jk21085815 <isquare.jayesh@gmail.com>
2023-08-09 11:33:57 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -455,6 +455,7 @@ socket.on('connect', () => {
             e.preventDefault()
             let type = $(this).val()
             var row = this.closest('form');
+            console.log(row)
             var dataId = row.id;
             console.log(dataId)
             socket.emit("DepositW", {dataId, type})
