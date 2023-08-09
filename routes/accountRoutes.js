04d6b881d81f3696f00c88a4d0509e23d46d64a3
jk21085815 <isquare.jayesh@gmail.com>
2023-08-09 12:14:53 +0530

    commit

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -7,7 +7,7 @@ router.post("/getMyAccStatement",authController.isProtected, accountController.g
 
 router.use(authController.isProtected, authController.restrictTo('accountControl'))
 
-router.post("/deposit", accountController.deposit);
+router.post("/deposit", authController.checkPass ,accountController.deposit);
 router.get("/getAllStatement", accountController.getAllAccStatement);
 router.post("/withdrawl", accountController.withdrawl);
 router.post("/getUserAccStatement", accountController.getUserAccountStatement);
