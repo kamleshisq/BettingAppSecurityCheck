51905219775c94e0811866ec0f2e5fa43783b516
jk21085815 <isquare.jayesh@gmail.com>
2023-08-10 18:00:52 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -7138,6 +7138,20 @@ socket.on('connect', () => {
                 $(".userDetails").find('input[name="myShare"]').val(data.myShare)
             }
         })
+
+        const num1Input = document.getElementById('myShare');
+        const num2Input = document.getElementById('Share');
+        num1Input.addEventListener('input', () => {
+            const num1 = parseFloat(num1Input.value);
+            const num2 = 100 - num1;
+            num2Input.value = num2;
+        });
+
+        num2Input.addEventListener('input', () => {
+            const num2 = parseFloat(num2Input.value);
+            const num1 = 100 - num2;
+            num1Input.value = num1;
+        });
     }
     
     
