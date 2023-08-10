bc972203eff4857e1547dbff19b6467b063768e6
jk21085815 <isquare.jayesh@gmail.com>
2023-08-10 16:31:42 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -7,10 +7,11 @@ const socket = io();
 socket.on('disconnect', () => {
     console.log("WebSocket Disconnected");
     // Refresh the page when the WebSocket connection is lost
-    window.location.reload();
+    
 });
 socket.on('connect', () => {
     console.log("websocket Connected")
+    window.location.reload();
     let LOGINDATA = {}
     socket.on('loginUser',(data) => {
         // console.log('WORKING45654', data)
