1572d7eb6abe7ba43ba99929b8b997928a34bca4
jk21085815 <isquare.jayesh@gmail.com>
2023-08-10 11:40:22 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2751,7 +2751,9 @@ socket.on('connect', () => {
         if(data.status === "error"){
             alert("Please try again later")
         }else{
+            console.log(data.bet.id)
             const deleteButton = document.getElementById(data.bet.id);
+            console.log(deleteButton)
             const row = deleteButton.closest('tr'); 
             if (row) {
                 row.remove(); 
