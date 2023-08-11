a09ff7f2bb0c063f1e47d7daa2aeb1369ba84e47
jk21085815 <isquare.jayesh@gmail.com>
2023-08-11 18:44:08 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -2507,5 +2507,10 @@ exports.getSettlementPageIn = catchAsync(async(req, res, next) => {
             }
           }
     ])
-    console.log(betsEventWise)
+    res.status(200).render("./sattlementInPage/main",{
+        title:"SETTLEMENTS",
+        me,
+        currentUser:me,
+        betsEventWise
+    })
 } )
