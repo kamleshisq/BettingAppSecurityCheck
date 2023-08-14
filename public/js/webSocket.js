ab5ef55dc6b706db04792e98d2d3e062dc2f9d14
jk21085815 <isquare.jayesh@gmail.com>
2023-08-14 17:43:29 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -466,7 +466,7 @@ socket.on('connect', () => {
                 console.log(data)
                 let modleName = "#myModal2"
                 let form = $(modleName).find('.form-data')
-                console.log(data.commissionData.matchOdd.percentage)
+                console.log(data.commissionData.matchOdd)
                 form.attr('id', data.id);
                 form.find('input[name="matchOdds"]').attr('value', data.commissionData.matchOdd.percentage);
                 form.find('input[name="Bookmaker"]').attr('value', data.commissionData.Bookmaker.percentage);
