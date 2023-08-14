026006f48613e2f745221501a280d331387cc0f1
jk21085815 <isquare.jayesh@gmail.com>
2023-08-14 13:50:18 +0530

    commit

--- websocketController/mapBetsController.js
+++ websocketController/mapBetsController.js
@@ -4,6 +4,7 @@ const betModel = require("../model/betmodel");
 
 
 exports.mapbet = async(data) => {
+    console.log(data)
     let bets = await betModel.find({marketId:`${data.id}`})
     bets.forEach(bet => {
         console.log(bet, 121212)
