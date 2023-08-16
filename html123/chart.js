3cd4a050f78c8848df11d98912b5a2445dc77567
jk21085815 <isquare.jayesh@gmail.com>
2023-08-16 10:51:00 +0530

    commit

---
+++ html123/chart.js
@@ -0,0 +1,75 @@
+socket.on("chartMain", async(data) => {
+        console.log(data)
+        const currentDate = new Date();
+        const tenDaysAgo = new Date();
+        tenDaysAgo.setDate(currentDate.getDate() - 10);
+    
+        const dateSequence = [];
+        for (let i = 0; i < 10; i++) {
+        const currentDate = new Date(tenDaysAgo);
+        currentDate.setDate(tenDaysAgo.getDate() + i);
+        dateSequence.push(currentDate.toDateString());
+        }
+
+        let accountForGraph = await AccModel.aggregate([
+            {
+                $match: {
+                  userName: data.LOGINUSER.userName,
+                  date: {
+                    $gte: tenDaysAgo,
+                  },
+                },
+              },
+              {
+                $group: {
+                  _id: {
+                    year: { $year: '$date' },
+                    month: { $month: '$date' },
+                    day: { $dayOfMonth: '$date' },
+                  },
+                  totalIncome: {
+                    $sum: '$creditDebitamount',
+                  },
+                  totalIncome2: {
+                    $sum: { $abs: '$creditDebitamount' }
+                  }
+                },
+              },
+          ]);
+          const dataArray = accountForGraph;
+          const dates = dataArray.map(item => new Date(item._id.year, item._id.month - 1, item._id.day));
+          const startDate = new Date(Math.min(...dates));
+          const endDate = new Date(Math.max(...dates));
+          
+          // Step 3: Fill in missing dates with zero values
+          const newDataArray = [];
+          const currentDate1 = new Date(startDate);
+          while (currentDate1 <= endDate) {
+            const matchingData = dataArray.find(item =>
+              item._id.year === currentDate1.getFullYear() &&
+              item._id.month === currentDate1.getMonth() + 1 &&
+              item._id.day === currentDate1.getDate()
+            );
+          
+            if (matchingData) {
+              newDataArray.push(matchingData);
+            } else {
+              newDataArray.push({
+                _id: {
+                  year: currentDate1.getFullYear(),
+                  month: currentDate1.getMonth() + 1,
+                  day: currentDate1.getDate()
+                },
+                totalIncome: 0,
+                totalIncome2: 0
+              });
+            }
+          
+            currentDate1.setDate(currentDate1.getDate() + 1);
+          }
+          console.log(newDataArray);
+        const Income = newDataArray.map(item => item.totalIncome);
+        const Revanue = newDataArray.map(item => item.totalIncome2);
+        socket.emit("chartMain", {Income, Revanue})
+        
+    })
--- server.js
+++ server.js
@@ -1704,81 +1704,83 @@ io.on('connection', (socket) => {
         }
     })
 
-    socket.on("chartMain", async(data) => {
-        console.log(data)
+    socket.on("chartMain", async (data) => {
+        console.log(data);
+    
         const currentDate = new Date();
         const tenDaysAgo = new Date();
         tenDaysAgo.setDate(currentDate.getDate() - 10);
     
         const dateSequence = [];
         for (let i = 0; i < 10; i++) {
-        const currentDate = new Date(tenDaysAgo);
-        currentDate.setDate(tenDaysAgo.getDate() + i);
-        dateSequence.push(currentDate.toDateString());
+            const currentDate = new Date(tenDaysAgo);
+            currentDate.setDate(tenDaysAgo.getDate() + i);
+            dateSequence.push(currentDate.toDateString());
         }
-
+    
         let accountForGraph = await AccModel.aggregate([
             {
                 $match: {
-                  userName: data.LOGINUSER.userName,
-                  date: {
-                    $gte: tenDaysAgo,
-                  },
+                    userName: data.LOGINUSER.userName,
+                    date: {
+                        $gte: tenDaysAgo, // Change this to currentDate
+                        $lte: currentDate, // Add this line
+                    },
                 },
-              },
-              {
+            },
+            {
                 $group: {
-                  _id: {
-                    year: { $year: '$date' },
-                    month: { $month: '$date' },
-                    day: { $dayOfMonth: '$date' },
-                  },
-                  totalIncome: {
-                    $sum: '$creditDebitamount',
-                  },
-                  totalIncome2: {
-                    $sum: { $abs: '$creditDebitamount' }
-                  }
+                    _id: {
+                        year: { $year: '$date' },
+                        month: { $month: '$date' },
+                        day: { $dayOfMonth: '$date' },
+                    },
+                    totalIncome: {
+                        $sum: '$creditDebitamount',
+                    },
+                    totalIncome2: {
+                        $sum: { $abs: '$creditDebitamount' },
+                    },
                 },
-              },
-          ]);
-          const dataArray = accountForGraph;
-          const dates = dataArray.map(item => new Date(item._id.year, item._id.month - 1, item._id.day));
-          const startDate = new Date(Math.min(...dates));
-          const endDate = new Date(Math.max(...dates));
-          
-          // Step 3: Fill in missing dates with zero values
-          const newDataArray = [];
-          const currentDate1 = new Date(startDate);
-          while (currentDate1 <= endDate) {
-            const matchingData = dataArray.find(item =>
-              item._id.year === currentDate1.getFullYear() &&
-              item._id.month === currentDate1.getMonth() + 1 &&
-              item._id.day === currentDate1.getDate()
+            },
+            {
+                $sort: { '_id.year': 1, '_id.month': 1, '_id.day': 1 },
+            },
+        ]);
+    
+        const newDataArray = [];
+        const currentDate1 = new Date(tenDaysAgo); // Change this to currentDate
+        for (let i = 0; i < 10; i++) {
+            const matchingData = accountForGraph.find(item =>
+                item._id.year === currentDate1.getFullYear() &&
+                item._id.month === currentDate1.getMonth() + 1 &&
+                item._id.day === currentDate1.getDate()
             );
-          
+    
             if (matchingData) {
-              newDataArray.push(matchingData);
+                newDataArray.push(matchingData);
             } else {
-              newDataArray.push({
-                _id: {
-                  year: currentDate1.getFullYear(),
-                  month: currentDate1.getMonth() + 1,
-                  day: currentDate1.getDate()
-                },
-                totalIncome: 0,
-                totalIncome2: 0
-              });
+                newDataArray.push({
+                    _id: {
+                        year: currentDate1.getFullYear(),
+                        month: currentDate1.getMonth() + 1,
+                        day: currentDate1.getDate(),
+                    },
+                    totalIncome: 0,
+                    totalIncome2: 0,
+                });
             }
-          
+    
             currentDate1.setDate(currentDate1.getDate() + 1);
-          }
-          console.log(newDataArray);
+        }
+    
+        console.log(newDataArray);
+    
         const Income = newDataArray.map(item => item.totalIncome);
         const Revanue = newDataArray.map(item => item.totalIncome2);
         socket.emit("chartMain", {Income, Revanue})
-        
-    })
+        // Now you can use the Income and Revanue arrays as needed
+    });
 
     socket.on("getUserDetaisl", async(data) => {
         try{
