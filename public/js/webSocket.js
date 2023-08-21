ac5eb03614f56021d03b37f816a40c1d8011e926
jk21085815 <isquare.jayesh@gmail.com>
2023-08-21 17:30:50 +0530

    Commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4669,7 +4669,7 @@ socket.on('connect', () => {
                 let NewStake = spanId - 1;
                 let result = (NewStake * Odds) - NewStake;
                 console.log(result)
-                if(spanId){
+                if(spanId || newStake == -1){
                     $(this).closest("tr").find('.set-stake-form-input2').val(0)
                     $(this)
                     .closest("tr")
