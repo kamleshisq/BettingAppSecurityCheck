4111853223c005c1b3b8839b812713e2d0e0ea6c
jk21085815 <isquare.jayesh@gmail.com>
2023-08-21 17:45:28 +0530

    Commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4697,6 +4697,7 @@ socket.on('connect', () => {
                 let Odds = parseFloat($(this).closest('tr').find(".nww-bet-slip-wrp-col1-txt-num").text())
                 let NewStake = parseFloat(spanId) + 100;
                 let result = (NewStake * Odds) - NewStake;
+                console.log(result)
                 if(!spanId){
                     $(this).closest("tr").find('.set-stake-form-input2').val(0)
                     $(this)
@@ -4711,7 +4712,7 @@ socket.on('connect', () => {
                     .text(0);
                 }
                 else{
-                    console.log("WORKING")
+                    // console.log("WORKING")
                     $(this).closest("tr").find('.set-stake-form-input2').val(NewStake)
                     $(this)
                     .closest("tr")
