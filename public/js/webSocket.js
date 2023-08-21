1791d341929fb89107d31d3a375dfef1e541607f
jk21085815 <isquare.jayesh@gmail.com>
2023-08-21 18:45:08 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4427,7 +4427,6 @@ socket.on('connect', () => {
                         }
                     }
                 })
-                console.log(this.parentNode)
                 if(this.id == `${section.secId}1` ){
                     if(data.betLimits[0].max_odd < section.backPrice || section.backPrice == "-" || section.backPrice == "1,000.00" || section.backPrice == "0"){
                         this.innerHTML = `<span class="tbl-td-bg-blu-spn mylock-data">
