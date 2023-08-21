56511f94a27cf87d45fc03625288945f1ee3ae9f
jk21085815 <isquare.jayesh@gmail.com>
2023-08-21 13:10:42 +0530

    Commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5430,15 +5430,11 @@ var notificationsss = /*#__PURE__*/function () {
           if (data.status === "success") {
             document.getElementById("popup-1").classList.toggle("active");
             document.getElementById('redPopUP').innerText = data.message;
-            setTimeout(function () {
-              document.getElementById("popup-1").classList.toggle("active");
-            }, 5000);
+            // setTimeout(function(){document.getElementById("popup-1").classList.toggle("active")}, 5000);
           } else {
             document.getElementById("popup-2").classList.toggle("active");
             document.getElementById('redPopUP2').innerText = data.message;
-            setTimeout(function () {
-              document.getElementById("popup-2").classList.toggle("active");
-            }, 5000);
+            // setTimeout(function(){document.getElementById("popup-2").classList.toggle("active")}, 5000);
           }
         case 1:
         case "end":
--- public/js/notificationsss.js
+++ public/js/notificationsss.js
@@ -3,10 +3,10 @@ export const notificationsss = async(data) => {
     if(data.status === "success"){
         document.getElementById("popup-1").classList.toggle("active");
         document.getElementById('redPopUP').innerText  = data.message
-        setTimeout(function(){document.getElementById("popup-1").classList.toggle("active")}, 5000);
+        // setTimeout(function(){document.getElementById("popup-1").classList.toggle("active")}, 5000);
     }else{
         document.getElementById("popup-2").classList.toggle("active");
         document.getElementById('redPopUP2').innerText  = data.message
-        setTimeout(function(){document.getElementById("popup-2").classList.toggle("active")}, 5000);
+        // setTimeout(function(){document.getElementById("popup-2").classList.toggle("active")}, 5000);
     }
 }
