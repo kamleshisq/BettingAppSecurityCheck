57033354f84f0d4bb0c377c5bcb4bc8e94700b31
jk21085815 <isquare.jayesh@gmail.com>
2023-08-22 12:27:37 +0530

    Commit

--- controller/authorizationController.js
+++ controller/authorizationController.js
@@ -162,6 +162,7 @@ exports.isProtected = catchAsync( async (req, res, next) => {
     if(!token){
         return next(new AppError('Please log in to access', 404))
     }
+    console.log(token)
     const tokenId = await loginLogs.findOne({session_id:token})
     console.log(tokenId)
     if(!tokenId.isOnline){
