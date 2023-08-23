08eac97c8f244f020fd68d4041ade0e9a4178b47
jk21085815 <isquare.jayesh@gmail.com>
2023-08-23 18:44:06 +0530

    commit

--- controller/deshBoard.js
+++ controller/deshBoard.js
@@ -16,261 +16,261 @@ exports.dashboardData = catchAsync(async(req, res, next) => {
     let alertBet
     let betsEventWise
     let turnOver
-    if(req.currentUser.roleName === "Admin"){
-        roles = await User.aggregate([
-            {
-                $group:{
-                    _id:"$roleName",
-                    total:{$sum:1}
-                }
-            },
-            { 
-                $sort:{
-                    total:1
-                }
-            }
-        ]);
+    // if(req.currentUser.roleName === "Admin"){
+    //     roles = await User.aggregate([
+    //         {
+    //             $group:{
+    //                 _id:"$roleName",
+    //                 total:{$sum:1}
+    //             }
+    //         },
+    //         { 
+    //             $sort:{
+    //                 total:1
+    //             }
+    //         }
+    //     ]);
 
-        users = await User.aggregate([
-            {
-                $group:{
-                    _id:{
-                        whiteLabel:"$whiteLabel",
-                        roleType:"$roleName"
-                    },
-                    total:{$sum:1}
-                }
-            },
-            { $group : { 
-                _id :  "$_id.whiteLabel",
-                terms: { 
-                    $push: { 
-                        roleType:"$_id.roleType",
-                        total:"$total"
-                    }
-                }
-             }
-            },    { 
-                $sort:{
-                    _id:1
-                }
-            }
-        ]);
+    //     users = await User.aggregate([
+    //         {
+    //             $group:{
+    //                 _id:{
+    //                     whiteLabel:"$whiteLabel",
+    //                     roleType:"$roleName"
+    //                 },
+    //                 total:{$sum:1}
+    //             }
+    //         },
+    //         { $group : { 
+    //             _id :  "$_id.whiteLabel",
+    //             terms: { 
+    //                 $push: { 
+    //                     roleType:"$_id.roleType",
+    //                     total:"$total"
+    //                 }
+    //             }
+    //          }
+    //         },    { 
+    //             $sort:{
+    //                 _id:1
+    //             }
+    //         }
+    //     ]);
 
-        topGames = await betModel.aggregate([
-            {
-                $match: {
-                    status: { $ne: "OPEN" }
-                }
-            },
-            {
-                $lookup: {
-                  from: "users",
-                  localField: "userName",
-                  foreignField: "userName",
-                  as: "user"
-                }
-              },
-              {
-                $unwind: "$user"
-              },
-            {
-                $group: {
-                    _id: "$event",
-                    totalCount: { $sum: 1 },
-                    uniqueUsers: { $addToSet: "$userId" },
-                    totalReturns: { $sum: "$Stake" }
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    event: "$_id",
-                    totalCount: 1,
-                    noOfUniqueUsers: { $size: "$uniqueUsers" },
-                    totalReturns: 1
-                }
-            },
-            {
-                $sort: {
-                    totalCount: -1
-                }
-            },
-            {
-                $limit: 5
-            }
-        ])
+    //     topGames = await betModel.aggregate([
+    //         {
+    //             $match: {
+    //                 status: { $ne: "OPEN" }
+    //             }
+    //         },
+    //         {
+    //             $lookup: {
+    //               from: "users",
+    //               localField: "userName",
+    //               foreignField: "userName",
+    //               as: "user"
+    //             }
+    //           },
+    //           {
+    //             $unwind: "$user"
+    //           },
+    //         {
+    //             $group: {
+    //                 _id: "$event",
+    //                 totalCount: { $sum: 1 },
+    //                 uniqueUsers: { $addToSet: "$userId" },
+    //                 totalReturns: { $sum: "$Stake" }
+    //             }
+    //         },
+    //         {
+    //             $project: {
+    //                 _id: 0,
+    //                 event: "$_id",
+    //                 totalCount: 1,
+    //                 noOfUniqueUsers: { $size: "$uniqueUsers" },
+    //                 totalReturns: 1
+    //             }
+    //         },
+    //         {
+    //             $sort: {
+    //                 totalCount: -1
+    //             }
+    //         },
+    //         {
+    //             $limit: 5
+    //         }
+    //     ])
 
-        Categories = await betModel.aggregate([
-            {
-                $match: {
-                    status: { $ne: "OPEN" }
-                }
-            },
-            {
-                $lookup: {
-                  from: "users",
-                  localField: "userName",
-                  foreignField: "userName",
-                  as: "user"
-                }
-              },
-              {
-                $unwind: "$user"
-              },
-            {
-                $group: {
-                    _id: "$betType",
-                    totalBets: { $sum: 1 },
-                    totalReturns: { $sum: "$Stake" },
-                    uniqueEvent: { $addToSet: "$event" }
-                }
-            },
-            {
-                $sort: {
-                    totalBets: -1
-                }
-            }
-        ])
+    //     Categories = await betModel.aggregate([
+    //         {
+    //             $match: {
+    //                 status: { $ne: "OPEN" }
+    //             }
+    //         },
+    //         {
+    //             $lookup: {
+    //               from: "users",
+    //               localField: "userName",
+    //               foreignField: "userName",
+    //               as: "user"
+    //             }
+    //           },
+    //           {
+    //             $unwind: "$user"
+    //           },
+    //         {
+    //             $group: {
+    //                 _id: "$betType",
+    //                 totalBets: { $sum: 1 },
+    //                 totalReturns: { $sum: "$Stake" },
+    //                 uniqueEvent: { $addToSet: "$event" }
+    //             }
+    //         },
+    //         {
+    //             $sort: {
+    //                 totalBets: -1
+    //             }
+    //         }
+    //     ])
 
-        userCount = await User.countDocuments({
-            roleName: 'user',
-            is_Online: true,
-        });
+    //     userCount = await User.countDocuments({
+    //         roleName: 'user',
+    //         is_Online: true,
+    //     });
 
-        adminCount = await User.countDocuments({
-            roleName: { $ne: 'user' },
-            is_Online: true,
-        });
+    //     adminCount = await User.countDocuments({
+    //         roleName: { $ne: 'user' },
+    //         is_Online: true,
+    //     });
 
-        betCount = await betModel.aggregate([
-            {
-                $lookup: {
-                  from: "users",
-                  localField: "userName",
-                  foreignField: "userName",
-                  as: "user"
-                }
-              },
-              {
-                $unwind: "$user"
-              },
-            {
-                $count: "totalBets"
-              }
-          ])
+    //     betCount = await betModel.aggregate([
+    //         {
+    //             $lookup: {
+    //               from: "users",
+    //               localField: "userName",
+    //               foreignField: "userName",
+    //               as: "user"
+    //             }
+    //           },
+    //           {
+    //             $unwind: "$user"
+    //           },
+    //         {
+    //             $count: "totalBets"
+    //           }
+    //       ])
 
-          alertBet = await betModel.aggregate([
-            {
-                $lookup: {
-                    from: "users",
-                    localField: "userName",
-                    foreignField: "userName",
-                    as: "user"
-                }
-            },
-            {
-                $unwind: "$user"
-            },
-            // {
-            //     $match: {
-            //         "user.parentUsers": { $in: [req.currentUser.id] }
-            //     }
-            // },
-            {
-                $match: {
-                    "status": "Alert"
-                }
-            },
-            {
-                $sort: {
-                    Stake: -1
-                }
-            },
-            {
-                $limit: 5
-            }
-        ]);
+    //       alertBet = await betModel.aggregate([
+    //         {
+    //             $lookup: {
+    //                 from: "users",
+    //                 localField: "userName",
+    //                 foreignField: "userName",
+    //                 as: "user"
+    //             }
+    //         },
+    //         {
+    //             $unwind: "$user"
+    //         },
+    //         // {
+    //         //     $match: {
+    //         //         "user.parentUsers": { $in: [req.currentUser.id] }
+    //         //     }
+    //         // },
+    //         {
+    //             $match: {
+    //                 "status": "Alert"
+    //             }
+    //         },
+    //         {
+    //             $sort: {
+    //                 Stake: -1
+    //             }
+    //         },
+    //         {
+    //             $limit: 5
+    //         }
+    //     ]);
     
     
-        betsEventWise = await betModel.aggregate([
-            {
-                $match: {
-                    status:"OPEN" 
-                }
-            },
-            {
-                $lookup: {
-                  from: "users",
-                  localField: "userName",
-                  foreignField: "userName",
-                  as: "user"
-                }
-              },
-              {
-                $unwind: "$user"
-              },
-            //   {
-            //     $match: {
-            //       "user.parentUsers": { $in: [req.currentUser.id] }
-            //     }
-            //   },
-            //   {
-            //     $group: {
-            //       _id: "$match",
-            //       count: { $sum: 1 }
-            //     }
-            //   },
-            //   {
-            //     $project: {
-            //       _id: 0,
-            //       eventname: "$_id",
-            //       count: 1
-            //     }
-            //   }
-            {
-                $group: {
-                  _id: "$match",
-                  count: { $sum: 1 },
-                  eventdate: { $first: "$eventDate" }, 
-                  eventid: { $first: "$eventId" },
-                  series: {$first: "$event"},
-                  sport: {$first : "$betType"}
-                }
-              },
-              {
-                $project: {
-                  _id: 0,
-                  matchName: "$_id",
-                  eventdate: 1,
-                  eventid: 1,
-                  series:1,
-                  count: 1,
-                  sport:1
-                }
-              },{
-                $sort:{count : -1}
-              },
-              {
-                $limit:5
-              }
-        ])
+    //     betsEventWise = await betModel.aggregate([
+    //         {
+    //             $match: {
+    //                 status:"OPEN" 
+    //             }
+    //         },
+    //         {
+    //             $lookup: {
+    //               from: "users",
+    //               localField: "userName",
+    //               foreignField: "userName",
+    //               as: "user"
+    //             }
+    //           },
+    //           {
+    //             $unwind: "$user"
+    //           },
+    //         //   {
+    //         //     $match: {
+    //         //       "user.parentUsers": { $in: [req.currentUser.id] }
+    //         //     }
+    //         //   },
+    //         //   {
+    //         //     $group: {
+    //         //       _id: "$match",
+    //         //       count: { $sum: 1 }
+    //         //     }
+    //         //   },
+    //         //   {
+    //         //     $project: {
+    //         //       _id: 0,
+    //         //       eventname: "$_id",
+    //         //       count: 1
+    //         //     }
+    //         //   }
+    //         {
+    //             $group: {
+    //               _id: "$match",
+    //               count: { $sum: 1 },
+    //               eventdate: { $first: "$eventDate" }, 
+    //               eventid: { $first: "$eventId" },
+    //               series: {$first: "$event"},
+    //               sport: {$first : "$betType"}
+    //             }
+    //           },
+    //           {
+    //             $project: {
+    //               _id: 0,
+    //               matchName: "$_id",
+    //               eventdate: 1,
+    //               eventid: 1,
+    //               series:1,
+    //               count: 1,
+    //               sport:1
+    //             }
+    //           },{
+    //             $sort:{count : -1}
+    //           },
+    //           {
+    //             $limit:5
+    //           }
+    //     ])
     
-        turnOver = await accountModel.aggregate([
-            // {
-            //     $match:{
-            //         user_id:req.currentUser._id
-            //     }
-            // },
-            {
-                $group: {
-                    _id: null,
-                    totalAmount: { $sum: { $abs: "$creditDebitamount" } }
-                }
-            }
-        ])
+    //     turnOver = await accountModel.aggregate([
+    //         // {
+    //         //     $match:{
+    //         //         user_id:req.currentUser._id
+    //         //     }
+    //         // },
+    //         {
+    //             $group: {
+    //                 _id: null,
+    //                 totalAmount: { $sum: { $abs: "$creditDebitamount" } }
+    //             }
+    //         }
+    //     ])
 
-    }else{
+    // }else{
         roles = await User.aggregate([
             {
                 $match:{
@@ -552,7 +552,7 @@ exports.dashboardData = catchAsync(async(req, res, next) => {
                 }
             }
         ])
-    }
+    // }
     
     
 
