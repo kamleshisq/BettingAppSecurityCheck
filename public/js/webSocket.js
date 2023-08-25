1891b8a6ac4b8056045dfc8735312358b4b79f7b
jk21085815 <isquare.jayesh@gmail.com>
2023-08-25 14:14:18 +0530

    Commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -8261,27 +8261,27 @@ socket.on('connect', () => {
             let status = $(this).prop('checked') ? true : false;
             let id = $(this).data('id')
             console.log(id)
-            // if(id){
-            //     if(confirm('do you want to change status')){
-            //         socket.emit('sportStatusChange2',{status,id})
-            //     }else{
-            //         if(status){
-            //             $(this).prop('checked','')
-            //         }else{
-            //             $(this).prop('checked','checked')
-            //         }
-            //     }
-            // }else if(id == 0) {
-            //     if(confirm('do you want to change status')){
-            //         socket.emit('sportStatusChange2',{status,id})
-            //     }else{
-            //         if(status){
-            //             $(this).prop('checked','')
-            //         }else{
-            //             $(this).prop('checked','checked')
-            //         }
-            //     }
-            // }
+            if(id){
+                if(confirm('do you want to change status')){
+                    socket.emit('sportStatusChange2',{status,id})
+                }else{
+                    if(status){
+                        $(this).prop('checked','')
+                    }else{
+                        $(this).prop('checked','checked')
+                    }
+                }
+            }else if(id == 0) {
+                if(confirm('do you want to change status')){
+                    socket.emit('sportStatusChange2',{status,id})
+                }else{
+                    if(status){
+                        $(this).prop('checked','')
+                    }else{
+                        $(this).prop('checked','checked')
+                    }
+                }
+            }
         })
         socket.on('sportStatusChange2',async(data)=>{
             if(data.status == 'success'){
