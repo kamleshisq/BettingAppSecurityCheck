733f892f8e6c06b6af91164aaa58b38cc3334556
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2023-08-31 19:40:35 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3003,22 +3003,22 @@ socket.on('connect', () => {
         socket.on("SearchOnlineUser", (data) =>{
             console.log(data, 565464)
             let html = ``
-    if(data.page === 1){
-        for(let i = 0; i < data.onlineUsers.length; i++){
-            html += `<li class="searchList" id="${data.onlineUsers[i]._id}">${data.onlineUsers[i].userName}</li>`
-        }
-        document.getElementById('search').innerHTML = html
-        document.getElementById("button").innerHTML = `<button id="${data.page}" class="next">Show More</button>`
-    }else if(data.page === null){
-        document.getElementById("button").innerHTML = ``
-    }else{
-        html = document.getElementById('search').innerHTML
-        for(let i = 0; i < data.onlineUsers.length; i++){
-            html += `<li class="searchList" id="${data.onlineUsers[i]._id}">${data.onlineUsers[i].userName}</li>`
-        }
-        document.getElementById('search').innerHTML = html
-        document.getElementById("button").innerHTML = `<button id="${data.page}" class="next">Show More</button>`
-    }
+            if(data.page === 1){
+                for(let i = 0; i < data.onlineUsers.length; i++){
+                    html += `<li class="searchList" id="${data.onlineUsers[i]._id}">${data.onlineUsers[i].userName}</li>`
+                }
+                document.getElementById('search').innerHTML = html
+                document.getElementById("button").innerHTML = `<button id="${data.page}" class="next">Show More</button>`
+            }else if(data.page === null){
+                document.getElementById("button").innerHTML = ``
+            }else{
+                html = document.getElementById('search').innerHTML
+                for(let i = 0; i < data.onlineUsers.length; i++){
+                    html += `<li class="searchList" id="${data.onlineUsers[i]._id}">${data.onlineUsers[i].userName}</li>`
+                }
+                document.getElementById('search').innerHTML = html
+                document.getElementById("button").innerHTML = `<button id="${data.page}" class="next">Show More</button>`
+            }
         })
 
         $(".logout").click(function(){
@@ -3041,6 +3041,7 @@ socket.on('connect', () => {
             filterData = {}
             filterData.userName = this.textContent
             $('.pageId').attr('data-pageid','1')
+            $('.wrapper').hide()
             socket.emit('OnlineUser',{filterData,LOGINDATA,page:0})
             
         })
