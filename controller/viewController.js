02427322489b934fb5ba7863944034ff033714ab
jk21085815 <isquare.jayesh@gmail.com>
2023-08-31 12:36:00 +0530

    Commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -1672,6 +1672,7 @@ exports.getLiveMarketsPage = catchAsync(async(req, res, next) => {
     let liveFootBall = footBall.eventList.filter(item => item.eventData.type === "IN_PLAY");
     let liveTennis = Tennis.eventList.filter(item => item.eventData.type === "IN_PLAY")
     let currentUser =  req.currentUser
+    console.log(req.currentUser)
     // console.log(liveFootBall)
     // console.log(liveTennis)
     // console.log(liveCricket)
