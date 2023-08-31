4276c8d802d0badf2659b87aca884d21388ec258
jk21085815 <isquare.jayesh@gmail.com>
2023-08-31 17:17:07 +0530

    Commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -3218,3 +3218,96 @@ exports.getCommissionReporMatch = catchAsync(async(req, res, next) => {
         data
     })
 })
+
+
+exports.RiskAnalysis = catchAsync(async(req, res, next) => {
+    let ip = req.ip
+    let ipv4
+    if (ip.indexOf('::ffff:') === 0) {
+        // Extract the IPv4 portion from the IPv6 address
+        ipv4 = ip.split('::ffff:')[1];
+    }else{
+        ipv4 = ip
+    }
+    let verticalMenus = await verticalMenuModel.find().sort({num:1});
+    const sportData = await getCrkAndAllData()
+    const cricket = sportData[0].gameList[0].eventList
+    let match = cricket.find(item => item.eventData.eventId == req.query.id);
+    if(match === undefined){
+        let data1liveCricket = sportData[1].gameList.map(item => item.eventList.find(item1 => item1.eventData.eventId == req.query.id))
+        match = data1liveCricket.find(item => item != undefined)
+    }
+    if(match == undefined){
+        res.status(404).json({
+            status:"Success",
+            message:"This match is no more live"
+        })
+    }
+    const liveStream = await liveStreameData(match.eventData.channelId, ipv4)
+    const src_regex = /src='([^']+)'/;
+    let match1
+    let src
+    if(liveStream.data){
+
+        match1 = liveStream.data.match(src_regex);
+        if (match1) {
+            src = match1[1];
+        } else {
+            console.log("No 'src' attribute found in the iframe tag.");
+        }
+        // console.log(src, 123)
+    }
+    const betLimit = await betLimitModel.find()
+    // console.log(match.marketList.goals)
+    // let session = match.marketList.session.filter(item => {
+        //     let date = new Date(item.updated_on);
+        //     return date < Date.now() - 1000 * 60 * 60;
+        // });
+        let SportLimits = betLimit.find(item => item.type === "Sport")
+        let min 
+        let max 
+        if (SportLimits.min_stake >= 1000) {
+            min = (SportLimits.min_stake / 1000) + 'K';
+        } else {
+            min = SportLimits.min_stake.toString();
+        }
+        if (SportLimits.max_stake >= 1000) {
+            max = (SportLimits.max_stake / 1000) + 'K';
+          } else {
+            max = SportLimits.max_stake.toString();
+        }
+        console.log(SportLimits, min , max)
+        let userLog
+        let stakeLabledata
+        let userMultimarkets
+        let betsOnthisMatch = []
+        let rules = await gamrRuleModel.find()
+        if(req.currentUser){
+            userLog = await loginLogs.find({user_id:req.currentUser._id})
+            userMultimarkets = await multimarkets.findOne({userId:req.currentUser._id})
+            stakeLabledata = await stakeLable.findOne({userId:req.currentUser._id})
+            if(stakeLabledata === null){
+                stakeLabledata = await stakeLable.findOne({userId:"6492fd6cd09db28e00761691"})
+            }
+            betsOnthisMatch = await betModel.find({userId:req.currentUser._id, match:match.eventData.name, status: 'OPEN'})
+        }else{
+            stakeLabledata = await stakeLable.findOne({userId:"6492fd6cd09db28e00761691"})
+        }
+        res.status(200).render("./userSideEjs/mainRiskAnalysis/main",{
+            user: req.currentUser,
+            verticalMenus,
+            check:"ExchangeIn",
+            match,
+            SportLimits,
+            liveStream,
+            userLog,
+            notifications:req.notifications,
+            stakeLabledata,
+            betsOnthisMatch,
+            rules,
+            src,
+            userMultimarkets,
+            min,
+            max
+    })
+});
