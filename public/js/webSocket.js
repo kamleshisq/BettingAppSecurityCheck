85ae2495779fc0219ee8602ef8d503c24d72c313
5815 <95901865+5815@users.noreply.github.com>
2023-09-05 16:11:25 +0530

    commiy

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3339,13 +3339,13 @@ socket.on('connect', () => {
                     html += `<td>${i + count}</td>
                     <td>${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</td>
                     <td>${bets[i].userName}</td>
-                    <td>${bets[i].event}</td>
+                    <td class="text-nowrap">${bets[i].event}</td>
                     `
                     if(bets[i].match){
                         html += `
-                        <td>${bets[i].marketName}</td>
+                        <td class="text-nowrap">${bets[i].marketName}</td>
                         <td>${bets[i].oddValue}</td>
-                        <td>${bets[i].match}</td>
+                        <td class="text-nowrap">${bets[i].match}</td>
                         <td>${bets[i].selectionName}</td>`
                     }else{
                         html += `<td>-</td>
