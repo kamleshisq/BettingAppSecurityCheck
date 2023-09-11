92a1d2d2359dc4c20a00d638ac59b399faa223e0
jk21085815 <isquare.jayesh@gmail.com>
2023-09-11 15:58:23 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -3521,7 +3521,7 @@ socket.on('connect', () => {
             if(userName != ''){
                 filterData.userName = userName
             }else{
-                filterData.userName = LOGINDATA.LOGINUSER.userName
+                // filterData.userName = LOGINDATA.LOGINUSER.userName
             }
             data.filterData = filterData
             data.LOGINDATA = LOGINDATA
@@ -3571,7 +3571,7 @@ socket.on('connect', () => {
                 let data = {}
                 let userName = $('.searchUser').val()
                 if(userName == ''){
-                    filterData.userName = LOGINDATA.LOGINUSER.userName
+                    // filterData.userName = LOGINDATA.LOGINUSER.userName
                 }else{
                     filterData.userName = userName
                 }
