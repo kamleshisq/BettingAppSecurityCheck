66aa04a63d762ea23aa76442ed8969009694ee24
jk21085815 <isquare.jayesh@gmail.com>
2023-09-11 11:45:44 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -2900,7 +2900,7 @@ exports.getSettlementHistoryPage = catchAsync(async(req, res, next) => {
 
 exports.getCommissionReport = catchAsync(async(req, res, next) => {
     let me = req.currentUser
-    let data = await accountStatement.find({user_id:me._id,description: { $regex: /^commission for/ } }).sort({date:-1}).limit(20)
+    let data = await accountStatement.find({user_id:me._id,description: { $regex: /^commission for/ } }).sort({date:-1}).limit(10)
     // console.log(data)
     res.status(200).render("./commissionPage/commissionPage",{
         title:"Commission",
