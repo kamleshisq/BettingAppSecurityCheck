536d2b950fba5ed8990fac3240603ad3b05a4979
jk21085815 <isquare.jayesh@gmail.com>
2023-09-11 11:05:50 +0530

    commit

--- model/userModel.js
+++ model/userModel.js
@@ -273,6 +273,7 @@ function roundToTwoDecimals(value) {
 }
 
 userSchema.pre(/^find/, function (next) {
+    console.log(this.myPL, this.uplinePL,  this.lifetimePL, this.pointsWL)
     this.myPL = roundToTwoDecimals(this.myPL);
     this.uplinePL = roundToTwoDecimals(this.uplinePL);
     this.lifetimePL = roundToTwoDecimals(this.lifetimePL);
