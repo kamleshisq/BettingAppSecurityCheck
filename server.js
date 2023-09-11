4743b96e41f1f8aa7e833e627ec051378f09bd2a
jk21085815 <isquare.jayesh@gmail.com>
2023-09-11 14:23:22 +0530

    commit

--- server.js
+++ server.js
@@ -2997,66 +2997,66 @@ io.on('connection', (socket) => {
             {
                 $match:filter
             },
-            {
-                $lookup:{
-                    from:'users',
-                    localField:'userName',
-                    foreignField:'userName',
-                    as:'userDetails'
-                }
-            },
-            {
-                $unwind:'$userDetails'
-            },
-            {
-                $match:{
-                    'userDetails.isActive':true,
-                    'userDetails.roleName':{$ne:'Admin'},
-                    'userDetails.role_type':{$in:role_type},
-                    'userDetails.parentUsers':{$elemMatch:{$eq:me._id}},
-                    'userDetails.whiteLabel':fWhitlabel
-                }
-            },
-            {
-                $group:{
-                    _id:{
-                        event:'$event',
-                        userName:'$userName'
-                    },
-                    betCount:{$sum:1},
-                    loss:{$sum:{$cond:[{$eq:['$status','LOSS']},1,0]}},
-                    won:{$sum:{$cond:[{$eq:['$status','WON']},1,0]}},
-                    open:{$sum:{$cond:[{$eq:['$status','OPEN']},1,0]}},
-                    returns:{$sum:{$cond:[{$in:['$status',['LOSS','OPEN']]},'$returns',{ "$subtract": [ "$returns", "$Stake" ] }]}}
+            // {
+            //     $lookup:{
+            //         from:'users',
+            //         localField:'userName',
+            //         foreignField:'userName',
+            //         as:'userDetails'
+            //     }
+            // },
+            // {
+            //     $unwind:'$userDetails'
+            // },
+            // {
+            //     $match:{
+            //         'userDetails.isActive':true,
+            //         'userDetails.roleName':{$ne:'Admin'},
+            //         'userDetails.role_type':{$in:role_type},
+            //         'userDetails.parentUsers':{$elemMatch:{$eq:me._id}},
+            //         'userDetails.whiteLabel':fWhitlabel
+            //     }
+            // },
+            // {
+            //     $group:{
+            //         _id:{
+            //             event:'$event',
+            //             userName:'$userName'
+            //         },
+            //         betCount:{$sum:1},
+            //         loss:{$sum:{$cond:[{$eq:['$status','LOSS']},1,0]}},
+            //         won:{$sum:{$cond:[{$eq:['$status','WON']},1,0]}},
+            //         open:{$sum:{$cond:[{$eq:['$status','OPEN']},1,0]}},
+            //         returns:{$sum:{$cond:[{$in:['$status',['LOSS','OPEN']]},'$returns',{ "$subtract": [ "$returns", "$Stake" ] }]}}
                     
-                }
-            },
-            {
-                $group:{
-                    _id:'$_id.event',
-                    Total_User:{$sum:1},
-                    betcount:{$sum:'$betCount'},
-                    loss:{$sum:'$loss'},
-                    won:{$sum:'$won'},
-                    open:{$sum:'$open'},
-                    returns:{$sum:'$returns'}
-                }
-            },
-            {
-                $sort: {
-                    betcount: -1 ,
-                    open : -1,
-                    won : -1,
-                    loss : -1,
-                    Total_User:-1
-                }
-            },
-            {
-                $skip: page * 10
-            },
-            {
-                $limit: 10 
-            }
+            //     }
+            // },
+            // {
+            //     $group:{
+            //         _id:'$_id.event',
+            //         Total_User:{$sum:1},
+            //         betcount:{$sum:'$betCount'},
+            //         loss:{$sum:'$loss'},
+            //         won:{$sum:'$won'},
+            //         open:{$sum:'$open'},
+            //         returns:{$sum:'$returns'}
+            //     }
+            // },
+            // {
+            //     $sort: {
+            //         betcount: -1 ,
+            //         open : -1,
+            //         won : -1,
+            //         loss : -1,
+            //         Total_User:-1
+            //     }
+            // },
+            // {
+            //     $skip: page * 10
+            // },
+            // {
+            //     $limit: 10 
+            // }
         ])
         console.log(gameAnalist)
         socket.emit('gameAnalysis', {gameAnalist, page})
