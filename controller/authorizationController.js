be9125243b4d8137fed823f14ed44a09c173bed3
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2023-09-13 15:12:03 +0530

    commit

--- controller/authorizationController.js
+++ controller/authorizationController.js
@@ -266,8 +266,8 @@ exports.isProtected_User = catchAsync( async (req, res, next) => {
     let token 
     // console.log(req.headers.authorization, 456)
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        console.log("WORKING")
-        console.log(req.headers.authorization)
+        // console.log("WORKING")
+        // console.log(req.headers.authorization)
         // console.log(req.headers.authorization.split(' ')[1].split("=")[1])
         token = req.headers.authorization.split(' ')[1].split("=")[1];
         if(!token){
