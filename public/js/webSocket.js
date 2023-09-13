ba4e437f9ab5fceb9a1df16249804f5261fa781d
jk21085815 <isquare.jayesh@gmail.com>
2023-09-13 16:38:32 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -1278,13 +1278,13 @@ socket.on('connect', () => {
                             html += `<button data-bs-toggle="modal" data-bs-target="#myModal3" class="PasswordChange" title="Change Password">P</button>`
                         }
                         html += `<button  data-bs-toggle="modal" data-bs-target="#myModal" class="Settlement" title="Settlement">S</button>`
-                        if(data.currentUser.role.authorization.includes('betLockAndUnloack')){
-                            if(response[i].betLock){
-                                html += `<button type="button" class="betLockStatus Locked" title="Bet Unlock">B</button>`
-                            }else{ 
-                                html += `<button type="button" class="betLockStatus" title="Bet Lock">B</button>`
-                            } 
-                        }
+                        // if(data.currentUser.role.authorization.includes('betLockAndUnloack')){
+                        //     if(response[i].betLock){
+                        //         html += `<button type="button" class="betLockStatus Locked" title="Bet Unlock">B</button>`
+                        //     }else{ 
+                        //         html += `<button type="button" class="betLockStatus" title="Bet Lock">B</button>`
+                        //     } 
+                        // }
                         if(data.currentUser.role.authorization.includes('userStatus')){
                             html += `<button data-bs-toggle="modal" data-bs-target="#myModal4" class="StatusChange" title="Change Status">CS</button>
                             `
