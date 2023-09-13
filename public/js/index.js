4b6684075d048a6c890738d04f7a59fdbac27ed3
jk21085815 <isquare.jayesh@gmail.com>
2023-09-13 18:53:39 +0530

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -396,11 +396,11 @@ $(document).on('submit','.userStatus',function(e) {
     let formDataObj = Object.fromEntries(fd.entries());
     formDataObj.id = id
     // let rowId = $('.rowId').attr('data-rowid')
-    console.log(formDataObj)
-    var trElement = document.querySelector(`tr[data-id="${id}"]`);
-    let rowId = trElement.id
-    // console.log(rowId)
-    // console.log(formDataObj)
+    console.log(formDataObj, "WORKING1212121")
+    // var trElement = document.querySelector(`tr[data-id="${id}"]`);
+    // let rowId = trElement.id
+    // // console.log(rowId)
+    // // console.log(formDataObj)
     // userStatus(formDataObj, rowId)
 });
 
