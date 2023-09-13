f3ae25dda4e5b20bfa9883b4eccd4bf7cbae05c5
jk21085815 <isquare.jayesh@gmail.com>
2023-09-13 18:51:19 +0530

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -401,7 +401,7 @@ $(document).on('submit','.userStatus',function(e) {
     let rowId = trElement.id
     // console.log(rowId)
     // console.log(formDataObj)
-    userStatus(formDataObj, rowId)
+    // userStatus(formDataObj, rowId)
 });
 
    
