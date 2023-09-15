d92132c824f56f085d07a7b883bf0f6823fb6ba2
jk21085815 <isquare.jayesh@gmail.com>
2023-09-15 11:54:29 +0530

    commit

--- websocketController/checkPassUser.js
+++ websocketController/checkPassUser.js
@@ -2,13 +2,18 @@ const userModel = require("../model/userModel");
 const catchAsync = require('../utils/catchAsync');
 
 exports.checkPass = catchAsync(async(data , pass) => {
-    const user = await userModel.findOne({userName:data.userName}).select('+password');
-            console.log(user)
-            if(!user || !(await user.correctPassword(pass, user.password))){
-                console.log("WRONG")
-                return false
-            }else{
-                console.log("RIght")
-                return true
-            }
+    try{
+        const user = await userModel.findOne({userName:data.userName}).select('+password');
+                console.log(user)
+                if(!user || !(await user.correctPassword(pass, user.password))){
+                    console.log("WRONG")
+                    return false;
+                }else{
+                    console.log("RIght")
+                    return true;
+                }
+    }catch(err){
+        console.log(err)
+        return false;
+    }
 })
