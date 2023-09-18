24852fcc898b3cb6bd0e7e4f74270b1bae47f6b4
jk21085815 <isquare.jayesh@gmail.com>
2023-09-18 19:08:14 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9208,8 +9208,8 @@ socket.on('connect', () => {
             let form = $(this)[0];
             let fd = new FormData(form);
             let data = Object.fromEntries(fd.entries());
-            console.log(data, id)
-            // socket.emit("VoidBetIn", {LOGINDATA, id, data})
+            // console.log(data, id)
+            socket.emit("VoidBetIn2", {LOGINDATA, id, data})
         })
        
 
---
+++ utils/voideBetAfterPlace.js
@@ -0,0 +1,12 @@
+const userModel = require('../model/userModel');
+const accountStatementModel = require("../model/accountStatementByUserModel");
+const betmodel = require('../model/betmodel');
+
+
+async function voidBET(data){
+ console.log(data, 444)   
+ return "WORKING"
+}
+
+
+module.exports = voidBET
