d7a7d3b1338aa0f09d68b803556e8341427ee760
jk21085815 <isquare.jayesh@gmail.com>
2023-09-19 17:38:49 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -10802,7 +10802,7 @@ socket.on('connect', () => {
             form.find('input[name = "max_odd"]').val(100)
             form.find('input[name = "delay"]').val(5)
             form.find('input[name = "type"]').val(data.data)
-            form.attr(data.id)
+            form.attr('id', data.id)
             }else if (data.status == "errr"){
                 alert('please try again leter')
             }else{
