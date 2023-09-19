2dce8a2e6a1d48cd94347ee2637a65908301409e
jk21085815 <isquare.jayesh@gmail.com>
2023-09-19 18:56:16 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -3314,7 +3314,7 @@ exports.getSettlementHistoryPage = catchAsync(async(req, res, next) => {
             $lookup: {
                 from: "users",
                 localField: "userId",
-                foreignField: { $toString: "$_id" },
+                // foreignField: { $toString: "$_id" },
                 as: "user"
               }
         }
