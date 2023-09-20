2c99c24b465aed4b4502e568847b325c7d3a1cd4
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2023-09-20 10:51:02 +0530

    commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7696,7 +7696,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49861" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49804" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
