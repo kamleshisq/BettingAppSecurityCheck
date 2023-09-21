5dc30a355aa0c95454355faa3b184a8a30641b13
jk21085815 <isquare.jayesh@gmail.com>
2023-09-21 11:05:21 +0530

    commit

--- server.js
+++ server.js
@@ -2671,11 +2671,11 @@ io.on('connection', (socket) => {
         let History
         let filter = {}
         if(data.from_date && data.to_date){
-            filter.date = {$gte:data.from_date,$lte:data.to_date}
+            filter.date = {$gte:new Date(data.from_date),$lte:new Date(data.to_date)}
         }else if(data.from_date && !data.to_date){
-            filter.date = {$gte:data.from_date}
+            filter.date = {$gte:new Date(data.from_date)}
         }else if(data.to_date && !data.from_date){
-            filter.date = {$lte:data.to_date}
+            filter.date = {$lte:new Date(data.to_date)}
         }
         console.log(filter)
         if(me.roleName === "Admin"){
