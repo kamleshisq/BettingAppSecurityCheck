33168ead938a405fd598a21b68bf2aaaa349ebd1
Jignesh <isquare.jigneshkumar@gmail.com>
2023-09-26 17:41:29 +0530

    commite

--- server.js
+++ server.js
@@ -454,7 +454,7 @@ io.on('connection', (socket) => {
             ])
             console.log(Logs)
 
-            socket.emit('Acc2', {json,page:data.page,Logs})
+            socket.emit('Acc2', {json,page:data.page,Logs,data})
             // account  = await AccModel.find({user_id:data.id})
             
             // fullUrl = 'http://127.0.0.1/api/v1/Account/getUserAccStatement1?id=' + data.id + "&page=" + data.page + "&from=" + data.Fdate + "&to=" + data.Tdate  
