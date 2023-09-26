c56b759f785fcf0ae2c95c92249a232d422f6cd1
jk21085815 <isquare.jayesh@gmail.com>
2023-09-26 16:45:02 +0530

    commit

--- NewCroneForUserAndBets/betPlaceCrone.js
+++ NewCroneForUserAndBets/betPlaceCrone.js
@@ -16,7 +16,8 @@ module.exports = () => {
             console.log('betCrone')
             let sportData1 = await sportData()
             const cricket = sportData1[0].gameList[0].eventList.sort((a, b) => a.eventData.time - b.eventData.time);
-            console.log(cricket)
+            let LiveCricket = cricket.filter(item => item.eventData.type === "IN_PLAY")
+            console.log(LiveCricket)
 
         }catch(err){
             console.log(err)
