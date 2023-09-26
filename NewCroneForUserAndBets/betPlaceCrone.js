811fc214f65f13eefa08b5b468bd530b890e5a76
jk21085815 <isquare.jayesh@gmail.com>
2023-09-26 16:28:24 +0530

    commit

--- NewCroneForUserAndBets/betPlaceCrone.js
+++ NewCroneForUserAndBets/betPlaceCrone.js
@@ -14,8 +14,8 @@ module.exports = () => {
     cron.schedule('*/5 * * * * *', async() => { 
         try{
             console.log('betCrone')
-            let sportData = await sportData()
-            const cricket = sportData[0].gameList[0].eventList.sort((a, b) => a.eventData.time - b.eventData.time);
+            let sportData1 = await sportData()
+            const cricket = sportData1[0].gameList[0].eventList.sort((a, b) => a.eventData.time - b.eventData.time);
             console.log(cricket)
 
         }catch(err){
