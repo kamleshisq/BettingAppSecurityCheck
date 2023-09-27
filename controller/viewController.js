f71ae7889b18aef2194ce5dcc7f07d355f3f5ef3
jk21085815 <isquare.jayesh@gmail.com>
2023-09-27 16:48:42 +0530

    cmt

--- controller/viewController.js
+++ controller/viewController.js
@@ -1698,7 +1698,7 @@ exports.getCricketData = catchAsync(async(req, res, next) => {
 // });
 
 exports.getmarketDetailsByMarketId = catchAsync(async(req, res, next) => {
-    let body = JSON.stringify(["1.218726263"]);
+    let body = JSON.stringify(["1.218726263", "4.335835341-F2"]);
     // console.log(body)
     var fullUrl = 'https://oddsserver.dbm9.com/dream/get_odds';
     fetch(fullUrl, {
