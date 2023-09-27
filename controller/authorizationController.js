9284d5f35a0fa3623c2ad23d2122d891dafbee8c
jk21085815 <isquare.jayesh@gmail.com>
2023-09-27 10:32:47 +0530

    cmt

--- controller/authorizationController.js
+++ controller/authorizationController.js
@@ -198,12 +198,9 @@ exports.checkPass = catchAsync(async(req, res, next) => {
 })
 
 exports.isProtected = catchAsync( async (req, res, next) => {
-    console.log('WORKING ADMIN')
     let token 
     // console.log(req.headers.authorization, 456)
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        console.log("WORKING")
-        console.log(req.headers.authorization)
         // console.log(req.headers.authorization.split(' ')[1].split("=")[1])
         token = req.headers.authorization.split(' ')[1].split("=")[1];
         if(!token){
@@ -214,7 +211,6 @@ exports.isProtected = catchAsync( async (req, res, next) => {
         }
     }else if(req.headers.cookie){
         token = parseCookies(req.headers.cookie).ADMIN_JWT;
-        console.log(token)
     }
     if(!token){
         return next(new AppError('Please log in to access', 404))
