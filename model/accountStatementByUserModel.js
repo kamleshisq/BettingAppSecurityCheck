53d0f523844ce0d9bdc8602b2c804b45096d459d
Jignesh <isquare.jigneshkumar@gmail.com>
2023-09-27 12:55:33 +0530

    commit

--- model/accountStatementByUserModel.js
+++ model/accountStatementByUserModel.js
@@ -63,6 +63,16 @@ accountStatementByUser.pre(/^find/, function(next){
     next()
 })
 
+function roundToTwoDecimals(value) {
+    if (typeof value === 'number' && !isNaN(value)) {
+        return parseFloat(value.toFixed(2));
+    } else {
+        // Handle cases where value is not a number or is undefined
+        // You can return an error message or a default value here.
+        return NaN; // or any other appropriate handling
+    }
+}
+
 accountStatementByUser.post(/^find/, function (docs) {
     // console.log(docs)
     // if(docs){
