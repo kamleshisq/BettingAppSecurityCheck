c18c8a2b990534e97f99e9057f1e62717cd53775
Jignesh <isquare.jigneshkumar@gmail.com>
2023-09-28 16:44:23 +0530

    commit

--- server.js
+++ server.js
@@ -138,9 +138,15 @@ io.on('connection', (socket) => {
             if(user){
                 const passcheck = await user.correctPassword(data.data.oldpassword, user.password)
                 if(passcheck){
-                    user.password = data.data.password
-                    user.passwordConfirm = data.data.passwordConfirm
-                    socket.emit('editMyPassword',{status:'success',msg:'Password Updated Successfully'})
+                    if(data.data.password == data.data.passwordConfirm){
+                        user.password = data.data.password
+                        user.passwordConfirm = data.data.passwordConfirm
+                        user.save()
+                        socket.emit('editMyPassword',{status:'success',msg:'Password Updated Successfully'})
+                    }else{
+                        socket.emit('editMyPassword',{status:'fail',msg:'Your new password and confirm new password are not match'})
+                    }
+                        
                 }else{
                     socket.emit('editMyPassword',{status:'fail',msg:'Your old password is wrong'})
                 }
