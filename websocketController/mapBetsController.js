8026148d211ff8e95c22b537c9fe0f23704602ea
jk21085815 <isquare.jayesh@gmail.com>
2023-09-28 14:48:02 +0530

    commit

--- websocketController/mapBetsController.js
+++ websocketController/mapBetsController.js
@@ -542,7 +542,7 @@ exports.mapbet = async(data) => {
           let checkDelete = await InprogressModel.findOneAndUpdate({marketId : bet.marketId}, {$inc:{settledBet:1}})
           console.log(checkDelete, '<======== checkDelete')
           if((checkDelete.settledBet + 1) == checkDelete.length){
-            await InprogressModel.findOneAndDelete({eventId : bet.marketId})
+            await InprogressModel.findOneAndDelete({marketId : bet.marketId})
           }
       });
 
