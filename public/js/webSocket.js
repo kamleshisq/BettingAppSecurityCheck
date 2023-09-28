37ed7dad5aa3307adfef11908e94bb9e1b75e320
Jignesh <isquare.jigneshkumar@gmail.com>
2023-09-28 11:37:53 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -11889,12 +11889,8 @@ socket.on('connect', () => {
                 const table = row.parentNode;
                 const rowIndex = Array.from(table.rows).indexOf(row);
                 row.remove(); 
-                const rowsToUpdate = Array.from(table.rows).slice(rowIndex);
-                rowsToUpdate.forEach((row, index) => {
-                    const srNoCell = row.cells[0]; 
-                    srNoCell.textContent = index + rowIndex + 1;
-                  });
-              }
+               
+            }
         }
     })
     }
