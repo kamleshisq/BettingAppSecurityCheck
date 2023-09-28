0048943f0fd226e7b0b135f1ca4761736e5472b8
Jignesh <isquare.jigneshkumar@gmail.com>
2023-09-28 11:50:08 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -6570,17 +6570,17 @@ socket.on('connect', () => {
         // });
       
         // jQuery approach
-        $(document).ready(function () {
-            $(".button").click(function () {
-              let odds = $(this).children("span:first-child").text();
-              let beton = $(this).closest("tr").find("td:first-child").text();
-              let secondPTag = $(this).closest("tr").next().find(".beton");
-              let numSpan = $(this).closest("tr").next().find(".nww-bet-slip-wrp-col1-txt-num");
-              let secId = this.id
-              secondPTag.text(`Bet on :${beton}@${odds}`).attr("id", `${secId}1`);;
-              numSpan.text(odds);
-            });
-          });
+        // $(document).ready(function () {
+        //     $(".button").click(function () {
+        //       let odds = $(this).children("span:first-child").text();
+        //       let beton = $(this).closest("tr").find("td:first-child").text();
+        //       let secondPTag = $(this).closest("tr").next().find(".beton");
+        //       let numSpan = $(this).closest("tr").next().find(".nww-bet-slip-wrp-col1-txt-num");
+        //       let secId = this.id
+        //       secondPTag.text(`Bet on :${beton}@${odds}`).attr("id", `${secId}1`);;
+        //       numSpan.text(odds);
+        //     });
+        //   });
 
           $(document).ready(function () {
             $(".nww-bet-slip-wrp-col2-inn span").click(function () {
