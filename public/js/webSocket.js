08e39ff12aaaf3ab4e3dec714445a2d609fe6f4f
jk21085815 <isquare.jayesh@gmail.com>
2023-10-02 10:38:27 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4830,10 +4830,10 @@ socket.on('connect', () => {
                 let html = '';
                 for(let i = 0; i < bets.length; i++){
                     let date = new Date(bets[i].date)
-                    if((i%2)==0){
-                        html += `<tr style="text-align: center;" class="blue">`
+                    if(bets[i].bettype2 === 'BACK'){
+                        html += `<tr class="back">`
                     }else{
-                        html += `<tr style="text-align: center;" >`
+                        html += `<tr class="lay">`
                     }
                     html += `<td>${i + count}</td>
                     <td class="date-time">${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</td>
