ba8ea8f1df7bb38cb71da0162ea8926f1d3448ec
Jignesh <isquare.jigneshkumar@gmail.com>
2023-10-03 10:40:15 +0530

    commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7724,7 +7724,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52010" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53478" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
