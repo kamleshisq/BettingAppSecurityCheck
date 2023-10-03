675eeaa0031cc68bcb54add570b2e466624a28b3
jk21085815 <isquare.jayesh@gmail.com>
2023-10-03 10:25:37 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -12955,6 +12955,7 @@ socket.on('connect', () => {
             if(data.status === "noFound"){
                 let form = $('#myModaNotification').find('.add-event-notification')
                 form.attr('id', data.id)
+                form.find('input[name = "status"]').classList.add('hide')
                 // form.find('input[name = "status"]').
             }else{
                 let form = $('#myModaNotification').find('.add-event-notification')
