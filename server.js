92856afb5f93e666d601f505498275737dd17f51
Jignesh <isquare.jigneshkumar@gmail.com>
2023-10-03 19:32:22 +0530

    commit

--- server.js
+++ server.js
@@ -3559,7 +3559,7 @@ io.on('connection', (socket) => {
             childrenUsername.push(ele.userName) 
         })
         let role_type = []
-        let roles = await Role.find({role_level: {$gt:ele.role.role_level}});
+        let roles = await Role.find({role_level: {$gt:me.role.role_level}});
         for(let i = 0; i < roles.length; i++){
             role_type.push(roles[i].role_type)
         }
