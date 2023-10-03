708baee56b7f3429afe6668b6a087f6d90aa7b81
jk21085815 <isquare.jayesh@gmail.com>
2023-10-03 17:38:09 +0530

    commit

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -723,7 +723,7 @@ socket.on('connect', () => {
         let form = $(this)[0];
         let fd = new FormData(form);
         let data = Object.fromEntries(fd.entries());
-        console.log(data)
+        console.log(data, "BETLIMIT")
         // socket.emit('UpdateBetLimit', {data, LOGINDATA})
     })
 
