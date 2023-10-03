ff3a8b720864e8097f4ce41d0bf1821fb11cecc0
jk21085815 <isquare.jayesh@gmail.com>
2023-10-03 19:05:04 +0530

    commit

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -201,7 +201,7 @@ if(marketDetails.title.toLowerCase().startsWith('match')){
 if(data.data.bettype2 === 'BACK'){
     let OddChake = data.data.oldOdds + betLimit.max_odd 
     if(OddChake <= data.data.odds || data.data.odds < data.data.oldOdds){
-        return 'Odds out of range'
+        return 'Odds out of range back'
     }
 }else{
     let OddChake = data.data.oldOdds - betLimit.max_odd  
