c191560c1dc3c18a56c360db884adcf863f1987e
jk21085815 <isquare.jayesh@gmail.com>
2023-10-05 12:07:31 +0530

    COMMIT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -6628,8 +6628,8 @@ socket.on('connect', () => {
                         <i class="fa-solid fa-lock"></i>
                       </span>`
                       this.removeAttribute("data-bs-toggle");
-                    //   parentElement.classList.add("suspended");
-                    //   $(this).parent().find(".match-status-message").text("Suspended")
+                      parentElement.classList.add("suspended");
+                      $(this).parent().find(".match-status-message").text("Suspended")
                     }else if(sectionData.win_result != 'undefined' && sectionData.win_result != " " && sectionData.win_result != ""){
                         this.removeAttribute("data-bs-toggle");
                       parentElement.classList.add("suspended");
