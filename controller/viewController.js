29ad2e31f495d886d36bddd21817024ab7e0aa63
jk21085815 <isquare.jayesh@gmail.com>
2023-10-06 12:01:59 +0530

    COMMIT

--- controller/viewController.js
+++ controller/viewController.js
@@ -3980,6 +3980,7 @@ exports.getBetLimitMatch = catchAsync(async(req, res, next) => {
 
 exports.getcommissionMarketWise1 = catchAsync(async(req, res, next) => {
     const me = req.currentUser
+    console.log(req.query.event)
     res.status(200).render('./commissionMarketWise/commissionMarketWise1/commissionMarketWise1.ejs', {
         title:"Commission Report",
         me,
