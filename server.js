1a1f8851559b6f5bb8959e693ac1873b76c104ed
jk21085815 <isquare.jayesh@gmail.com>
2023-10-09 19:34:05 +0530

    Commit

--- server.js
+++ server.js
@@ -4822,7 +4822,73 @@ io.on('connection', (socket) => {
             const passcheck = await user.correctPassword(data.data.password, user.password)
             // console.log(passcheck, "PASSWORD CHECK")
             if(passcheck){
-                
+            let bet = await Bet.findByIdAndUpdate(data, {status:"CANCEL"});
+            let DebitCreditAmount 
+            if(bet.bettype2 === "Back"){
+                if(bet.marketName.toLowerCase().startsWith('match')){
+                    DebitCreditAmount = bet.Stake
+                }else if(bet.marketName.toLowerCase().startsWith('book')){
+                    DebitCreditAmount = bet.Stake
+                }else{
+                    DebitCreditAmount = bet.Stake
+                }
+            }else{
+                if(bet.marketName.toLowerCase().startsWith('match')){
+                    DebitCreditAmount = ((bet.Stake * bet.oddValue) - bet.Stake).toFixed(2)
+                }else if(bet.marketName.toLowerCase().startsWith('book')){
+                    DebitCreditAmount = ((bet.Stake * bet.oddValue)/100).toFixed(2)
+                }else{
+                    DebitCreditAmount = ((bet.Stake * bet.oddValue)/100).toFixed(2)
+                }
+            }
+            let user = await User.findByIdAndUpdate(bet.userId, {$inc:{availableBalance: DebitCreditAmount, myPL: DebitCreditAmount, exposure:-DebitCreditAmount}})
+            let description = `Bet for ${bet.match}/stake = ${bet.Stake}/CANCEL`
+            let userAcc = {
+                "user_id":user._id,
+                "description": description,
+                "creditDebitamount" : DebitCreditAmount,
+                "balance" : user.availableBalance + DebitCreditAmount,
+                "date" : Date.now(),
+                "userName" : user.userName,
+                "role_type" : user.role_type,
+                "Remark":"-",
+                "stake": DebitCreditAmount,
+                "transactionId":`${bet.transactionId}`
+            }
+            
+            let debitAmountForP = DebitCreditAmount
+              for(let i = user.parentUsers.length - 1; i >= 1; i--){
+                  let parentUser1 = await userModel.findById(user.parentUsers[i])
+                  let parentUser2 = await userModel.findById(user.parentUsers[i - 1])
+                  let parentUser1Amount = new Decimal(parentUser1.myShare).times(debitAmountForP).dividedBy(100)
+                  let parentUser2Amount = new Decimal(parentUser1.Share).times(debitAmountForP).dividedBy(100);
+                  parentUser1Amount = parentUser1Amount.toDecimalPlaces(4);
+                  parentUser2Amount =  parentUser2Amount.toDecimalPlaces(4);
+                  await userModel.findByIdAndUpdate(user.parentUsers[i], {
+                    $inc: {
+                        downlineBalance: DebitCreditAmount,
+                        myPL: -parentUser1Amount,
+                        uplinePL: -parentUser2Amount,
+                        lifetimePL: -parentUser1Amount,
+                        pointsWL: DebitCreditAmount
+                    }
+                });
+            
+                if (i === 1) {
+                    await userModel.findByIdAndUpdate(user.parentUsers[i - 1], {
+                        $inc: {
+                            downlineBalance: DebitCreditAmount,
+                            myPL: -parentUser2Amount,
+                            lifetimePL: -parentUser2Amount,
+                            pointsWL: DebitCreditAmount
+                        }
+                    });
+                }
+                  debitAmountForP = parentUser2Amount
+              }
+            
+            await AccModel.create(userAcc);
+            socket.emit('timelyVoideBEt', {bet, status:"success"})
             }else{
                 socket.emit('timelyVoideBEt', {status:'err', message:'Please Provide valide password'})
             }
