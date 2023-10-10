baede2688c1b38cb2146a1c01a23c2a64ea85687
jk21085815 <isquare.jayesh@gmail.com>
2023-10-10 18:53:14 +0530

    commit

--- server.js
+++ server.js
@@ -3764,6 +3764,7 @@ io.on('connection', (socket) => {
 
                     if(Bets.length > 0){
                         console.log(Bets, 'BETS')
+                        console.log(Bets[0].selections, Bets[1].selections)
                     }
                 }
                 // role_type = []
