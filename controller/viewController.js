9674b7ff077951ea3909207bba6eacaad938a5ea
jk21085815 <isquare.jayesh@gmail.com>
2023-10-10 13:30:10 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -1817,7 +1817,7 @@ exports.getLiveTv = catchAsync(async(req, res, next) => {
 
 
 exports.getMarketResult = catchAsync(async(req, res, next) => {
-    let body = JSON.stringify(["4.1696779664635-OE"]);
+    let body = JSON.stringify(["4.1696779664635-OE", "4.1696912456680-F2"]);
     // console.log(body)
     let fullUrl = "https://admin-api.dreamexch9.com/api/dream/markets/result";
     fetch(fullUrl, {
