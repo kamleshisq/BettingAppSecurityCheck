a8c0aa4b413582d0720116e5b1802348ae0a8ff6
jk21085815 <isquare.jayesh@gmail.com>
2023-10-12 10:05:25 +0530

    COMIT

---
+++ Untitled-1.js
@@ -0,0 +1,53 @@
+[
+  {
+    selectionName: 'India',
+    totalAmount: 23.000000000000003,
+    matchName: 'India v Afghanistan',
+    Stake: -200
+  },
+  {
+    selectionName: 'Afghanistan',
+    totalAmount: 780.0000000000001,
+    matchName: 'India v Afghanistan',
+    Stake: -100
+  }
+]
+
+
+[
+  {
+    selectionName: 'Afghanistan',
+    totalAmount: 780.0000000000001,
+    matchName: 'India v Afghanistan',
+    Stake: -100,
+    winAmount: 580.0000000000001,
+    lossAmount: -76.99999999999997
+  },
+  {
+    selectionName: 'India',
+    totalAmount: 23.00000000000003,
+    matchName: 'India v Afghanistan',
+    Stake: -200,
+    winAmount: -76.99999999999997,
+    lossAmount: 580.0000000000001
+  }
+]
+
+[
+  {
+    selectionName: 'India',
+    totalAmount: 107.00000000000003,
+    matchName: 'India v Afghanistan',
+    Stake: -500,
+    winAmount: 207.00000000000003,
+    lossAmount: -1260
+  },
+  {
+    selectionName: 'Afghanistan',
+    totalAmount: -760,
+    matchName: 'India v Afghanistan',
+    Stake: 100,
+    winAmount: -1260,
+    lossAmount: 207.00000000000003
+  }
+]
