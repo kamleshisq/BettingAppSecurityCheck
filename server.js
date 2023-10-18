05f62fc373f5fab2eadf2948de91da493d235b46
jk21085815 <isquare.jayesh@gmail.com>
2023-10-18 16:44:33 +0530

    commit

--- server.js
+++ server.js
@@ -2495,7 +2495,9 @@ io.on('connection', (socket) => {
 
 
     socket.on('socketStakeLABLEDATA', async(data) => {
+        console.log(data)
         let check = await stakeLabelModel.find({userId:data.LOGINUSER._id})
+        console.log(check)
         if(check.length > 0){
             socket.emit('socketStakeLABLEDATA', {status:"notFound"})
         }else{
