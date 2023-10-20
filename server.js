d39e59ad9e9238588ebdab9e5ff8468f61c1b3f0
jk21085815 <isquare.jayesh@gmail.com>
2023-10-20 11:43:56 +0530

    commit

--- server.js
+++ server.js
@@ -5598,57 +5598,57 @@ io.on('connection', (socket) => {
 
                     }
                 },
-                // {
-                //     $group: {
-                //       _id: null,
-                //       data: {
-                //         $push: {
-                //           _id: "$_id",
-                //           totalAmount: {
-                //             $multiply:["$totalAmount", -1]
-                //           },
-                //           totalWinAmount: {
-                //             $multiply:["$totalWinAmount", -1]
-                //           }
-                //         }
-                //       }
-                //     }
-                //   },
-                //   {
-                //     $project: {
-                //       _id: 0,
-                //       data: {
-                //         $map: {
-                //           input: "$data",
-                //           as: "item",
-                //           in: {
-                //             _id: "$$item._id",
-                //             totalAmount: "$$item.totalAmount",
-                //             totalWinAmount: "$$item.totalWinAmount",
-                //             totalWinAmount2: {
-                //               $add: ["$$item.totalWinAmount", {
-                //                 $reduce: { 
-                //                     input: "$data",
-                //                     initialValue: 0,
-                //                     in: {
-                //                         $cond: {
-                //                             if: {
-                //                                 $ne: ["$$this._id", "$$item._id"] 
-                //                             },
-                //                             then: { $add: ["$$value", "$$this.totalAmount"] },
-                //                             else: {
-                //                                 $add: ["$$value", 0] 
-                //                             }
-                //                         }
-                //                     }
-                //                 }
-                //               }]
-                //             }
-                //           }
-                //         }
-                //       }
-                //     }
-                //   }
+                {
+                    $group: {
+                      _id: null,
+                      data: {
+                        $push: {
+                          _id: "$_id",
+                          totalAmount: {
+                            $multiply:["$totalAmount", -1]
+                          },
+                          totalWinAmount: {
+                            $multiply:["$totalWinAmount", -1]
+                          }
+                        }
+                      }
+                    }
+                  },
+                  {
+                    $project: {
+                      _id: 0,
+                      data: {
+                        $map: {
+                          input: "$data",
+                          as: "item",
+                          in: {
+                            _id: "$$item._id",
+                            totalAmount: "$$item.totalAmount",
+                            totalWinAmount: "$$item.totalWinAmount",
+                            totalWinAmount2: {
+                              $add: ["$$item.totalWinAmount", {
+                                $reduce: { 
+                                    input: "$data",
+                                    initialValue: 0,
+                                    in: {
+                                        $cond: {
+                                            if: {
+                                                $ne: ["$$this._id", "$$item._id"] 
+                                            },
+                                            then: { $add: ["$$value", "$$this.totalAmount"] },
+                                            else: {
+                                                $add: ["$$value", 0] 
+                                            }
+                                        }
+                                    }
+                                }
+                              }]
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
             ])
 
             console.log(betData, "betData")
