6c88731382c77a93994ae24e8a8478f1ffa9905c
5815 <95901865+5815@users.noreply.github.com>
2023-10-23 17:33:06 +0530

    COMIT

--- server.js
+++ server.js
@@ -5464,11 +5464,25 @@ io.on('connection', (socket) => {
                         }
                     },
                     {
+                        $addFields: {
+                          runs: {
+                            $toInt: {
+                              $arrayElemAt: [
+                                { $split: ["$selectionName", "@"] },
+                                1 
+                              ]
+                            }
+                          }
+                        }
+                    },
+                    {
                         $group: { 
                             _id: {
                                 "secId":"$secId",
-                                "userName":"$userName"
+                                "userName":"$userName",
+                                "runs":"$runs"
                             },
+                            uniqueRuns: { $addToSet: "$runs" },
                             parentArray: { $first: "$parentArray" },
                             totalAmount: { 
                                 $sum: '$returns'
@@ -5491,9 +5505,11 @@ io.on('connection', (socket) => {
                             _id:0,
                             userName: "$_id.userName",
                             secId: "$_id.secId",
+                            runs: "$_id.runs",
                             parentArray: "$parentArray",
                             totalAmount1: "$totalAmount",
                             totalWinAmount1: "$totalWinAmount",
+                            uniqueRuns:"$uniqueRuns",
                             totalAmount:{
                                 $reduce:{
                                     input:'$parentArray',
@@ -5584,146 +5600,51 @@ io.on('connection', (socket) => {
                     },
                     {
                         $project:{
-                            _id:"$secId",
-                            totalAmount:{
-                                $sum: '$totalAmount.value'
-                            },
-                            totalWinAmount:{
-                                $sum: '$totalWinAmount.value'
-                            },
-    
+                            _id:0,
+                            secId: "$secId",
+                            runs: "$runs",
+                            totalAmount:"$totalAmount.value",
+                            totalWinAmount:"$totalWinAmount.value",
+                            uniqueRuns:"$uniqueRuns",
                         }
                     },
                     {
-                        $group: {
-                          _id: null,
-                          data: {
-                            $push: {
-                              _id: "$_id",
-                              totalAmount: {
-                                $multiply:["$totalAmount", -1]
-                              },
-                              totalWinAmount: {
-                                $multiply:["$totalWinAmount", -1]
-                              }
+                        $addFields: {
+                          dataTOShow: {
+                            $cond: {
+                              if: { $eq: ["$secId", "odd_Even_Yes"] },
+                              then: { $concat: [ { $toString: "$runs" }, " or more" ] },
+                              else: { $concat: [ { $toString: "$runs" }, " less than" ] }
                             }
                           }
                         }
-                      },
-                      {
-                        $project: {
-                          _id: 0,
-                          data: {
-                            $map: {
-                              input: "$data",
-                              as: "item",
-                              in: {
-                                _id: "$$item._id",
-                                totalAmount: "$$item.totalAmount",
-                                totalWinAmount: "$$item.totalWinAmount",
-                                totalWinAmount2: {
-                                  $add: ["$$item.totalWinAmount", {
-                                    $reduce: { 
-                                        input: "$data",
-                                        initialValue: 0,
-                                        in: {
-                                            $cond: {
-                                                if: {
-                                                    $ne: ["$$this._id", "$$item._id"] 
-                                                },
-                                                then: { $add: ["$$value", "$$this.totalAmount"] },
-                                                else: {
-                                                    $add: ["$$value", 0] 
-                                                }
-                                            }
-                                        }
-                                    }
-                                  }]
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                ])
-    
-                // console.log(betData, "betData")
-                // console.log(betData[0].data, "betData[0].databetData[0].databetData[0].data")
-                socket.emit('FANCYBOOK', {betData:betData[0].data, type:'ODD'})
-            }else{
-                console.log('WORKING123', data)
-                let betData = await Bet.aggregate([
+                    },
                     {
-                        $match: {
-                          status: "OPEN",
-                          marketId: data.marketId,
-                          userName: { $in: childrenUsername1 }
-                        }
-                      },
-                      {
-                        $addFields: {
-                          runs: {
-                            $toInt: {
-                              $arrayElemAt: [
-                                { $split: ["$selectionName", "@"] },
-                                1
-                              ]
-                            }
-                          }
-                        }
-                      },
-                      {
-                        $group: {
-                          _id: {
+                        $project:{
+                            _id:"$dataTOShow",
                             secId: "$secId",
-                            userName: "$userName",
-                            runs: "$runs"
-                          },
-                          uniqueRuns: { $addToSet: "$runs" },
-                          parentArray: { $first: "$parentArray" },
-                          totalAmount: {
-                            $sum: "$returns"
-                          },
-                          totalWinAmount: {
-                            $sum: {
-                              $cond: {
-                                if: { $eq: ["$secId", "odd_Even_Yes"] },
-                                then: {
-                                  $divide: [{ $multiply: ["$oddValue", "$Stake"] }, 100]
-                                },
-                                else: "$Stake"
-                              }
-                            }
-                          }
+                            runs: "$runs",
+                            totalAmount:"$totalAmount",
+                            totalWinAmount:"$totalWinAmount",
+                            uniqueRuns:"$uniqueRuns",
                         }
-                      },
-                      {
-                        $addFields: {
-                          uniqueRuns: {
-                            $reduce: {
-                              input: "$uniqueRuns",
-                              initialValue: [],
-                              in: { $concatArrays: ["$$value", ["$$this"]] }
-                            }
-                          }
-                        }
-                      },
-                      {
-                        $sort: { "uniqueRuns": 1 } // Sort uniqueRuns in ascending order
-                      },
-                      {
+                    },
+                    {
                         $project: {
-                          _id: 0,
-                          secId: "$_id.secId",
-                          runs: "$_id.runs",
+                          _id: "$_id",
+                          secId: "$secId",
+                          runs: "$runs",
                           totalAmount: "$totalAmount",
                           totalWinAmount: "$totalWinAmount",
-                          uniqueRuns: "$uniqueRuns"
+                          uniqueRuns: {
+                            $setUnion: ["$uniqueRuns"] 
+                          }
                         }
                       }
                     
                     
-                ])
+                    
+                  ])
                 console.log(betData, "betData")
                 socket.emit('FANCYBOOK', {betData, type:'Fancy'})
             }
