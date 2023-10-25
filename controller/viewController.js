fa361b83ab96952bf92d215c9bc4b4c62a0d082e
jk21085815 <isquare.jayesh@gmail.com>
2023-10-25 17:05:14 +0530

    commit

--- controller/viewController.js
+++ controller/viewController.js
@@ -5143,6 +5143,7 @@ exports.getFancyBookDATA = catchAsync(async(req, res, next) => {
                 let dataToshow = []
                 for(let i = 0; i < betData.uniqueRuns.length; i++){ 
                     console.log(betData.uniqueRuns[i])
+                    let data = []
                     if(betData.uniqueRuns.length === 1){
                         data.message = `${betData[0].uniqueRuns[i] - 1} or less`
                         let sum = 0
