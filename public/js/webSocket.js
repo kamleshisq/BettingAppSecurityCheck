080e9abed1e91bfa83982ff935df088f803d2318
jk21085815 <isquare.jayesh@gmail.com>
2023-10-26 11:21:26 +0530

    COMMIT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -14130,7 +14130,19 @@ socket.on('connect', () => {
                     <!-- <td>${data.data[i].userName}</td> -->
                     <td>${data.data[i].oddValue}</td>
                     <td>${data.data[i].Stake}</td>
-                    <td><div class="btn-group"><button class="btn alert-btn" id="${data.data[i]._id}">Alert</button></div></td>
+                    <td><div class="btn-group"><button data-bs-toggle="modal" data-bs-target="#myModal2" class="btn alert flag-button" id="${data.data[i]._id}"><svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 489 489" xml:space="preserve">
+                    <g>
+                      <g>
+                        <path d="M454.3,31.6c-28.5-15.3-59.1-23.4-93.7-23.4c-40.7,0-81.5,11.2-120.2,21.4S166,50,130.4,50c-23.8,0-45.3-4.6-65.2-13.7
+                          V20.4C65.2,9.2,56,0,44.8,0S24.4,9.2,24.4,20.4v448.2c0,11.2,9.2,20.4,20.4,20.4s20.4-9.2,20.4-20.4v-148
+                          c20,6.9,41.2,10.5,64.2,10.5c40.7,0,81.5-11.2,120.2-20.4c38.7-10.2,74.4-20.4,110-20.4c27.5,0,52,6.1,74.4,18.3
+                          c12.7,8.7,30.6-2.1,30.6-17.3V49.9C464.4,41.8,460.4,35.7,454.3,31.6z M423.7,258.8c-20.4-7.1-41.8-10.2-64.2-10.2
+                          c-40.7,0-81.5,11.2-120.2,21.4s-74.4,20.4-110,20.4c-23.4,0-44.8-4.1-64.2-13.2V79.5c20.4,7.1,41.8,10.2,64.2,10.2
+                          c40.7,0,81.5-11.2,120.2-21.4s74.4-20.4,110-20.4c23.4,0,44.8,4.1,64.2,13.2V258.8z"></path>
+                      </g>
+                    </g>
+                    </svg></button></div></td>
+                    
                 </tr>`
                 if(!data.data[i].marketName.toLowerCase().startsWith('match') || !data.data[i].marketName.toLowerCase().startsWith('book')  || !data.data[i].marketName.toLowerCase().startsWith('winne')){
                     let string = `.fancy.disable[data-id="${data.data[i].marketId}"]`;
@@ -14169,7 +14181,7 @@ socket.on('connect', () => {
             //     e.preventDefault()
             //     socket.emit("alertBet", this.id)
             // })
-            
+
 
             socket.on("alertBet", async(data) => {
                 if(data.status === "error"){
