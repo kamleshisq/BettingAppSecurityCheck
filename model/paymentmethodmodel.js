2716f5504b5d35742ccffe76021638070cac99c2
Jignesh <isquare.jigneshkumar@gmail.com>
2023-10-27 12:33:25 +0530

    newCommte

---
+++ model/paymentmethodmodel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+
+const paymentMethodSchem = mongoose.Schema({
+    accountholdername:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    accountnumber:{
+        type:String,
+        required:true,
+        unique
+    },
+    ifsccode:{
+        type:String,
+        required:true,
+        unique
+    },
+    bankname:{
+        type:String,
+        required:true
+    },
+    branchname:{
+        type:String,
+        required:true
+    },
+    displayname:{
+        type:String,
+        required:true
+    },
+    pmethod:{
+        type:String,
+        required:true,
+        enum:['banktransfer','upi','paytm']
+    },
+    status:{
+        type:String,
+        required:true
+    }
+})
+
+const payment = mongoose.model('paymentmethodmodel', paymentMethodSchem)
+
+module.exports = payment
