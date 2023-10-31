fa12e9909c12013946af56ab7ec4dde2bf4dff29
jk21085815 <isquare.jayesh@gmail.com>
2023-10-31 16:43:25 +0530

    cmt

--- server.js
+++ server.js
@@ -2090,9 +2090,9 @@ io.on('connection', (socket) => {
             }
           ])
             .then((userResult) => {
-                console.log(userResult, "userResultuserResultuserResultuserResult")
+                // console.log(userResult, "userResultuserResultuserResultuserResult")
               const userIds = userResult.length > 0 ? userResult[0].userIds.map(id => id.toString()) : [];
-              console.log(userIds, "userIds")
+            //   console.log(userIds, "userIds")
               Bet.aggregate([
                 {
                   $match: {
