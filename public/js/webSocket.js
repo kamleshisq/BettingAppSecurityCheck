8ebc621b5a376f2160b7ca88e6a50b11abe1768b
Jignesh <isquare.jigneshkumar@gmail.com>
2023-10-31 15:57:13 +0530

    newCommte

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -965,13 +965,13 @@ socket.on('connect', () => {
     
     if(pathname.startsWith('/admin') && pathname != '/admin/gameanalysis' && pathname != '/admin/useraccount' && !pathname.startsWith('/admin/userManagement') && !pathname.startsWith('/admin/betlimit')){
         setInterval(()=>{
-            console.log("loginuserbalance")
+            // console.log("loginuserbalance")
             socket.emit('loginuserbalance',LOGINDATA)
         },1000)
         
     }
     socket.on('loginuserbalance',async(data)=>{
-        console.log('refresh login data')
+        // console.log('refresh login data')
         html1 = `
         <div class="skin-data green">
             
