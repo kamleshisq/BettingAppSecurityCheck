203ba3b4c0f892813f20aad2355287313cfbdefa
jk21085815 <isquare.jayesh@gmail.com>
2023-11-01 16:35:37 +0530

    cmt

--- controller/viewController.js
+++ controller/viewController.js
@@ -79,7 +79,7 @@ const paymentReportModel = require('../model/paymentreport');
 // });
 
 exports.userTable = catchAsync(async(req, res, next) => {
-    console.log(process.memoryUsage(), "MEMORY DATA")
+    // console.log(process.memoryUsage(), "MEMORY DATA")
     var WhiteLabel = await whiteLabel.find()
     let id = req.query.id;
     // console.log(id, "111111111111111111")
