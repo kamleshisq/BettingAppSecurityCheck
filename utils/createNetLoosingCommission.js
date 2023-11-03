9584d0f4dc9ccb985729a86054247815eeb4725b
jk21085815 <isquare.jayesh@gmail.com>
2023-11-03 15:25:14 +0530

    CMT

---
+++ utils/createNetLoosingCommission.js
@@ -0,0 +1,15 @@
+const userModel = require('../model/userModel');
+const accModel = require('../model/accountStatementByUserModel');
+const betModel = require('../model/betmodel');
+const commissionModel = require('../model/CommissionModel');
+const commissionMarketModel = require('../model/CommissionMarketsModel');
+const settlementHistory = require('../model/settelementHistory');
+const InprogressModel = require('../model/InprogressModel');
+const newCommissionModel = require('../model/commissioNNModel');
+const Decimal = require('decimal.js');
+const runnerDataModel = require("../model/runnersData");
+
+
+async function commisiion(data){
+    
+}
