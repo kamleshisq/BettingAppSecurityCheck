adf7ee65850c41f06c1fd0851945143ddfe2eef2
jk21085815 <isquare.jayesh@gmail.com>
2023-11-08 17:39:12 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -325,6 +325,7 @@ socket.on('connect', () => {
         }else{
             document.getElementById("loader3-overlay").style.display = "none";
             if(data.data.length > 0){
+                console.log('WORKING')
                 $('#navmod4').find('form').addClass('hide')
 
             }else{
