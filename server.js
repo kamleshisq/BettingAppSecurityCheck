500e91f82f21185413c05849737cea5c0c1d2ac9
jk21085815 <isquare.jayesh@gmail.com>
2023-11-10 15:14:22 +0530

    CMT

--- server.js
+++ server.js
@@ -8295,7 +8295,7 @@ io.on('connection', (socket) => {
             filterData.reqDate = {$lte : new Date(new Date(data.filterData.toDate))}
         }
         console.log(filterData)
-        let reqData = await withdowReqModel.find(filterData)
+        let reqData = await withdowReqModel.find(filterData).sort({date:-1}).skip(skip).limit(limit)
         socket.emit('WithdrawLoadMoreAdmin', {reqData, page})
 
     })
