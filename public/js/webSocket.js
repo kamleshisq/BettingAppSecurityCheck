aa51a41693a69ad9a3118a8e81cd8fe275f13584
jk21085815 <isquare.jayesh@gmail.com>
2023-11-21 17:49:04 +0530

    COMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -10456,6 +10456,16 @@ socket.on('connect', () => {
             });
           });
 
+          $(document).ready(function(){
+            $("table .button").click(function(){
+              $('tr:not(.tbl-data-href) .my-exc-inn-colaps-txt-dv').removeClass('open');
+              $(this).parents('tr').next().find('.my-exc-inn-colaps-txt-dv').addClass('open');
+            });
+            $(".my-exc-inn-colaps-txt-dv .close-btn").click(function(){
+              $('tr:not(.tbl-data-href) .my-exc-inn-colaps-txt-dv').removeClass('open');
+            });
+        });
+
         function showLoader() {
             document.getElementById("loader-overlay").style.display = "flex";
           }
