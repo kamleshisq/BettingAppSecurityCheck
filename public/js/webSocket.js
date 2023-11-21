658d5edf32eada8597c3b81623ff460d11132846
jk21085815 <isquare.jayesh@gmail.com>
2023-11-21 17:11:54 +0530

    COMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -10618,16 +10618,16 @@ socket.on('connect', () => {
                     }
                 }
                 else if (this.id == `${section.selectionId}6`){
-                    console.log(data)
-                    if(!data.status){
-                        this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data">
-                        <i class="fa-solid fa-lock"></i>
-                        </span>`
-                        this.removeAttribute("data-bs-toggle");
-                        parentElement.classList.add("suspended");
-                        $(this).parent().find(".match-status-message").text("Suspended")
-                    }
-                   else if( section.layPrice3 == "-" || section.layPrice3 == "1,000.00" || section.layPrice3 == "0"){
+                    // console.log(data)
+                    // if(!data.status){
+                    //     this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data">
+                    //     <i class="fa-solid fa-lock"></i>
+                    //     </span>`
+                    //     this.removeAttribute("data-bs-toggle");
+                    //     parentElement.classList.add("suspended");
+                    //     $(this).parent().find(".match-status-message").text("Suspended")
+                    // }else 
+                    if( section.layPrice3 == "-" || section.layPrice3 == "1,000.00" || section.layPrice3 == "0"){
                     //     this.innerHTML = `<span class="tbl-td-bg-pich-spn mylock-data">
                     //     <i class="fa-solid fa-lock"></i>
                     //   </span>`
