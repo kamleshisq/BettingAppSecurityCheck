e3562ca915178a7cb82a4194a322cf47d81239f7
jk21085815 <isquare.jayesh@gmail.com>
2023-11-24 17:26:10 +0530

    CMT

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(express.static(path.join( __dirname, 'public')));
 // app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.json({limit:"5mb"}));
+app.use(express.json());
 app.use(fileUpload());
 app.use(express.urlencoded({ extended:true}));
 app.use(cookieParser());
