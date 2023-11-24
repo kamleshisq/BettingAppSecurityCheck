45df4f00f62d8afbfc9ede3e10a76c6c96bbad03
jk21085815 <isquare.jayesh@gmail.com>
2023-11-24 11:27:58 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -7411,10 +7411,11 @@ socket.on('connect', () => {
                 $(thatSpan).closest("tr").next().removeClass('back-inplaymatch')
                 $(thatSpan).closest("tr").next().addClass('lay-inplaymatch')
             }
-            $(thatSpan).closest("tr").find(".set-stake-form-input2").val(parseFloat(data.stake))
+            $(thatSpan).closest("tr").next().find(".set-stake-form-input2").val(parseFloat(data.stake))
             var result = (parseFloat(data.stake) * parseFloat(data.odds)) - parseFloat(data.stake);
             $(thatSpan)
                   .closest("tr")
+                  .next()
                   .find(".c-gren")
                   .text(result.toFixed(2));
         })
