549bb8ff5a3d0aad71acaf66355788d7f5324762
jk21085815 <isquare.jayesh@gmail.com>
2023-11-28 15:50:27 +0530

    CMT

--- controller/globalSettingsController.js
+++ controller/globalSettingsController.js
@@ -1,1 +1,17 @@
 const globlalSettingsModel = require('../model/globalSetting');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+const whiteLabel = require('../model/whitelableModel');
+
+exports.createData = catchAsync(async(req, res, next) => {
+    const whiteLabels = await whiteLabel.find()
+    if(whiteLabels.length > 0){
+        for(let i = 0; i < whiteLabels.length; i++){
+            let data = {
+                name:'photo',
+                whiteLabel : whiteLabels[i].whiteLabelName,
+                
+            }
+        }
+    }
+})
