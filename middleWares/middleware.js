f26c8ccbc400972e1638e20c5aee4dde9f0efdfb
jk21085815 <isquare.jayesh@gmail.com>
2023-11-29 18:24:04 +0530

    CMT

--- middleWares/middleware.js
+++ middleWares/middleware.js
@@ -90,7 +90,8 @@ const LoginLogs = catchAsync(async(req, res, next) => {
         if(req.headers.cookie && !req.originalUrl.startsWith("/wallet")){
             // //console.log(global._loggedInToken)
             const login = await loginLogs.findOne({session_id:parseCookies(req.headers.cookie).ADMIN_JWT, isOnline:true})
-            // //console.log(req.headers.cookie)
+            console.log(req.headers.cookie)
+            console.log(login, 1212)
             if(login === null){
                 return next()
             }
