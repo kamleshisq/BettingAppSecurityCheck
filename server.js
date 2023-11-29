59572911eaf95c011487dee73d38ba6e41a2e571
Jignesh <jigneshkumar142@gmail.com>
2023-11-29 10:45:57 +0530

    commite

--- server.js
+++ server.js
@@ -7390,7 +7390,7 @@ io.on('connection', (socket) => {
                 exposer3Amount = exposure3[0].amount
             }
         
-            // console.log(exposure3, exposure2, exposure1)
+            console.log(exposure3, exposure2, exposure1,'==>exposures')
 
             function getExposure(runs,obj){
                 runs.sort((a, b) => a - b)
