80a0ce13b2a8fe735ee7336ef74a0dbd07f69122
jk21085815 <isquare.jayesh@gmail.com>
2023-12-07 13:16:59 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -13799,6 +13799,7 @@ socket.on('connect', () => {
         socket.on('getRefresh', async(data) => {
             data.getMapBetData.map(data1 => {
                 console.log('working')
+                console.log($('#mapMarket').find(`.Unmap #${data1._id}`).closest('tr'))
                 $('#mapMarket').find(`.Unmap #${data1._id}`).closest('tr').find('td:eq(2)').text(`${data1.count}`);
             })
         })
