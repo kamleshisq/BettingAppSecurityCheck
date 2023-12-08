aef15ebc4e5612470d093dbf0f48800737d38105
jk21085815 <isquare.jayesh@gmail.com>
2023-12-08 15:02:27 +0530

    COMT

--- utils/mapBetController.js
+++ utils/mapBetController.js
@@ -565,7 +565,7 @@ async function mapBet(data){
                 let checkDelete = await InprogressModel.findOneAndUpdate({marketId : bets[bet].marketId, progressType:'SettleMent'}, {$inc:{settledBet:1}})
                 if((checkDelete.settledBet + 1) == checkDelete.length){
                 await InprogressModel.findOneAndDelete({marketId : bets[bet].marketId, progressType:'SettleMent'})
-                await runnerDataModel.findOneAndDelete({marketId:bets[bet].marketId})
+                // await runnerDataModel.findOneAndDelete({marketId:bets[bet].marketId})
                 }
             }catch(err){
                 console.log(err)
