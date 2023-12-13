69cded3e57a0eaf1afba84f6b521842bb9852c90
jk21085815 <isquare.jayesh@gmail.com>
2023-12-13 17:17:43 +0530

    COMIT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9153,7 +9153,9 @@ socket.on('connect', () => {
                                 table.find("tr:eq(1)").find("td:eq(0)").after(newTd);
                                 table.find("tr:eq(3)").find("td:eq(0)").after(newTd2);
                             }
-                            
+                            table.find('tr.back-inplaymatch').each(function(){
+                                this.find('td:eq(0)').attr('colspan', 9)
+                            })
                         }
                     }
                 })
