ceff26bec533752e9c218b4714d32f5ba674a528
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 17:52:58 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9644,7 +9644,7 @@ socket.on('connect', () => {
                 $("td.market").each(function() {
                     if(data.betDetails.some(item => item == this.id)){
                         let text = $(this).text()
-                        text += '<button class="site-button fancy-book">book</button>'
+                        text += '<button class="site-button fancy-book" data-bs-toggle="modal" data-bs-target="#gameroulspop">book</button>'
                         $(this).html(text)
                     } 
 
