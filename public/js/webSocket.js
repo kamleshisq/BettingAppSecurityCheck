4996b18a9519752f9cc2491b6fa2baf8bdcc222d
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 14:04:11 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9391,7 +9391,7 @@ socket.on('connect', () => {
            
       })
         socket.on('marketIdbookDetails', data => {
-            console.log('WORKING', console.log(data))
+            console.log('WORKING', console.log(data, "datadatadatda"))
             for(let i = 0; i < data.betsMarketIdWise.length; i++){
                 // console.log(data.betsMarketIdWise[i]._id, data.betsMarketIdWise[i].selections, "selections")
                 let team1Data
