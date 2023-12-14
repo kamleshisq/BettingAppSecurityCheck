f3ba70045c8f9f54a37a3f0a8a5ca55249302b74
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 10:52:04 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -8498,6 +8498,7 @@ socket.on('connect', () => {
                     data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
                         let oldValue = $(this).find('td:eq(1)').find('span').text()
                         let newvalue = (oldValue * 1) + (data.result * 1)
+                        console.log(newvalue , "newvaluenewvaluenewvalue")
                         $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
                         if(newvalue > 0){
                             $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
