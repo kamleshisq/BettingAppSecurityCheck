c2b1807fd26ff10992f57696e8686eae3932445a
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 13:10:54 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -8572,89 +8572,53 @@ socket.on('connect', () => {
             }
           }
 
-
+          function marketIdbookDetails( status ){
+            let eventId = search.split('=')[1]
+            socket.emit("marketIdbookDetails", {LOGINDATA, eventId, status})
+    }
+    marketIdbookDetails( false )
 
           function Onlyminus ( data ){
             console.log(data)
-            let table = data.element.closest('table')
-            if(table.hasClass("market")){
-                let trLength = table.find("tr:eq(1)").find('td').length
-                if(trLength === 4 || trLength === 8){
-                    if(data.status){
-                        // console.log(data.plusMinus, "newvaluenewvaluenewvalue")
-                        let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
-                        let newvale = (beforevalue * 1) + (data.result * 1) + (data.plusMinus * 1)
-                        data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
-                        data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
-                            let oldValue = $(this).find('td:eq(1)').find('span').text()
-                            let newvalue = (oldValue * 1) - (data.result * 1)
-                            console.log(newvalue , "newvaluenewvaluenewvalue")
-                            $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
-                            if(newvalue > 0){
-                                $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
-                            }else{
-                                $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
-                            }
-                        })
-                    }else{
-                        let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
-                        let newvale = (beforevalue * 1) - (data.result * 1) - (data.NewStake * 1)
-                        data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
-                        data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
-                            let oldValue = $(this).find('td:eq(1)').find('span').text()
-                            let newvalue = (oldValue * 1) + (data.NewStake * 1)
-                            $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
-                            // console.log(newvalue)
-                            if(newvalue > 0){
-                                $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
-                            }else{
-                                $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
-                            }
-                        })
-                    }
-                }else{
-                    table.find('th:eq(0)').after('<th></th>')
-                    if(data.status){
-                        // let thathtml = ''
-                        if((data.plusMinus * -1) > 0){
-                            var newTd = $("<td class='tbl-td-with5'>").html(`<span class="c-gren" >+${(data.plusMinus * -1).toFixed(2)}</span>`);
+            if(data.NewStake == 0){
+                marketIdbookDetails( false )
+            }else{
+                let table = data.element.closest('table')
+                if(table.hasClass("market")){
+                    let trLength = table.find("tr:eq(1)").find('td').length
+                    if(trLength === 4 || trLength === 8){
+                        if(data.status){
+                            // console.log(data.plusMinus, "newvaluenewvaluenewvalue")
+                            let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
+                            let newvale = (beforevalue * 1) + (data.result * 1) + (data.plusMinus * 1)
+                            data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
+                            data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
+                                let oldValue = $(this).find('td:eq(1)').find('span').text()
+                                let newvalue = (oldValue * 1) - (data.result * 1)
+                                console.log(newvalue , "newvaluenewvaluenewvalue")
+                                $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
+                                if(newvalue > 0){
+                                    $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
+                                }else{
+                                    $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
+                                }
+                            })
                         }else{
-                            var newTd = $("<td class='tbl-td-with5'>").html(`<span class="c-reed" >${(data.plusMinus * -1).toFixed(2)}</span>`);
+                            let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
+                            let newvale = (beforevalue * 1) - (data.result * 1) - (data.NewStake * 1)
+                            data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
+                            data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
+                                let oldValue = $(this).find('td:eq(1)').find('span').text()
+                                let newvalue = (oldValue * 1) + (data.NewStake * 1)
+                                $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
+                                // console.log(newvalue)
+                                if(newvalue > 0){
+                                    $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
+                                }else{
+                                    $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
+                                }
+                            })
                         }
-                        if((data.result * 1) > 0){
-                            var newTd2 = $("<td class='tbl-td-with5'>").html(`<span class="c-gren" >+${(data.result * 1).toFixed(2)}</span>`);
-                        }else{
-                            var newTd2 = $("<td class='tbl-td-with5'>").html(`<span class="c-reed" >${(data.result * 1).toFixed(2)}</span>`);
-                        }
-                        data.element.closest('tr').prev().find("td:eq(0)").after(newTd)
-                        table.find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function () {
-                            var firstTd = $(this).find('td:first-child');
-                        
-                            if (firstTd.length === 1 && (firstTd.siblings().length === 7 || firstTd.siblings().length === 2)) {
-                                // console.log('Working:', firstTd);
-                                firstTd.after(newTd2.clone()); 
-                            }
-                        });
-                    }else{
-                        if((data.result * 1) > 0){
-                            var newTd = $("<td class='tbl-td-with5'>").html(`<span class="c-gren" >+${(data.result * 1).toFixed(2)}</span>`);
-                        }else{
-                            var newTd = $("<td class='tbl-td-with5'>").html(`<span class="c-reed" >${(data.result * 1).toFixed(2)}</span>`);
-                        }
-                        if((data.NewStake * -1) > 0){
-                            var newTd2 = $("<td class='tbl-td-with5'>").html(`<span class="c-gren" >+${(data.NewStake * -1).toFixed(2)}</span>`);
-                        }else{
-                            var newTd2 = $("<td class='tbl-td-with5'>").html(`<span class="c-reed" >${(data.NewStake * -1).toFixed(2)}</span>`);
-                        }
-                        data.element.closest('tr').prev().find("td:eq(0)").after(newTd)
-                        table.find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function () {
-                            var firstTd = $(this).find('td:first-child');
-                        
-                            if (firstTd.length === 1 && (firstTd.siblings().length === 7 || firstTd.siblings().length === 2)) {
-                                // console.log('Working:', firstTd);
-                                firstTd.after(newTd2.clone()); 
-                            }
-                        });
                     }
                 }
             }
@@ -9288,11 +9252,7 @@ socket.on('connect', () => {
         //     link.addEventListener("click", handlePlaceBetClick);
         //   });
 
-        function marketIdbookDetails( status ){
-                let eventId = search.split('=')[1]
-                socket.emit("marketIdbookDetails", {LOGINDATA, eventId, status})
-        }
-        marketIdbookDetails( false )
+        
         $(document).on('click', ".close-btn", function(){
             marketIdbookDetails( false )
         })
