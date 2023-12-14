4d39ad3c8be1541ce5d2693589f2041e9ad1230a
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 10:39:08 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -8488,30 +8488,36 @@ socket.on('connect', () => {
 
           function marketplusminus ( data ){
             console.log(data)
-            if(data.status){
-                let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
-                let newvale = (beforevalue * 1) - (data.result * 2)
-                data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
-                data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
-                    let oldValue = $(this).find('td:eq(1)').find('span').text()
-                    let newvalue = (oldValue * 1) + (data.result * 1)
-                    $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
-                })
+            let table = data.element.closest('table')
+            let trLength = table.find("tr:eq(1)").find('td').length
+            if(trLength === 4 || trLength === 8){
+                if(data.status){
+                    let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
+                    let newvale = (beforevalue * 1) - (data.result * 2)
+                    data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
+                    data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
+                        let oldValue = $(this).find('td:eq(1)').find('span').text()
+                        let newvalue = (oldValue * 1) + (data.result * 1)
+                        $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
+                    })
+                }else{
+                    let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
+                    let newvale = (beforevalue * 1) + (data.result * 1) + (data.NewStake * 1)
+                    data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
+                    data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
+                        let oldValue = $(this).find('td:eq(1)').find('span').text()
+                        let newvalue = (oldValue * 1) - (data.NewStake * 1)
+                        $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
+                        // console.log(newvalue)
+                        if(newvalue > 0){
+                            $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
+                        }else{
+                            $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
+                        }
+                    })
+                }
             }else{
-                let beforevalue  = data.element.closest('tr').prev().find('td:eq(1)').find('span').text()
-                let newvale = (beforevalue * 1) + (data.result * 1) + (data.NewStake * 1)
-                data.element.closest('tr').prev().find('td:eq(1)').find('span').text(newvale.toFixed(2))
-                data.element.closest('table').find('tr:eq(1), tr:eq(3), tr:eq(5)').each(function(){
-                    let oldValue = $(this).find('td:eq(1)').find('span').text()
-                    let newvalue = (oldValue * 1) - (data.NewStake * 1)
-                    $(this).find('td:eq(1)').find('span').text(newvalue.toFixed(2))
-                    console.log(newvalue)
-                    if(newvalue > 0){
-                        $(this).find('td:eq(1)').find('span').attr('class', 'c-gren');
-                    }else{
-                        $(this).find('td:eq(1)').find('span').attr('class', 'c-reed');
-                    }
-                })
+                
             }
           }
 
