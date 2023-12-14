8bebdcbe3fa7ca7e12147bc3c80c0bb5aa230f09
jk21085815 <isquare.jayesh@gmail.com>
2023-12-14 18:59:58 +0530

    CMT

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -156,6 +156,7 @@ if(data.data.spoetId == 1){
 
 
 // FOR STAKE RANGE
+console.log(marketDetails.title.toLowerCase(), "marketDetails.title.toLowerCase()marketDetails.title.toLowerCase()")
     if(marketDetails.title.toLowerCase().startsWith('match') && marketDetails.title.toLowerCase().split(' ').startsWith('odd')){
 
         let MATCHODDDATA = await betLimitModel.findOne({type:`${sportName}/matchOdds`})
