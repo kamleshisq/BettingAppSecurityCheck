c9859def28fcccd901784cbcd647eb1519a1cf87
jk21085815 <isquare.jayesh@gmail.com>
2023-12-15 10:39:44 +0530

    CMT

--- controller/walletController.js
+++ controller/walletController.js
@@ -24,6 +24,7 @@ exports.consoleBodyAndURL = catchAsync(async(req, res, next) => {
     // console.log(req.ip)
     let x  = req.body
     let publicKey
+    console.log(req.ip, "ipip")
     if(req.ip == "::ffff:3.9.120.247"){
         publicKey = readPem("publicSport.pem")
     }else{
