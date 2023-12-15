48f3efd3db26b7181d8c399fd3cb08bb4f849b66
jk21085815 <isquare.jayesh@gmail.com>
2023-12-15 15:13:07 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -2250,7 +2250,7 @@ exports.getLiveTv = catchAsync(async(req, res, next) => {
 
 
 exports.getMarketResult = catchAsync(async(req, res, next) => {
-    let body = JSON.stringify([ "4.1701832199070-F2", "1.222032054", "4.1701498444440-BM"]);
+    let body = JSON.stringify([ "4.1701832199070-F2", "1.222032054", "4.1701498444440-BM", "4.1702616096118-OE"]);
     // console.log(body)
     let fullUrl = "https://admin-api.dreamexch9.com/api/dream/markets/result";
     fetch(fullUrl, {
