518c35f26001368b2f8fd84345a16c30a5fe7275
jk21085815 <isquare.jayesh@gmail.com>
2023-12-15 11:15:01 +0530

    CMT

--- crones/cancelCrone.js
+++ crones/cancelCrone.js
@@ -8,5 +8,9 @@ const Decimal = require('decimal.js');
 module.exports = () => { 
     cron.schedule('*/5 * * * * *', async() => { 
         console.log('WORKING 123456879')
+        let currentDate = new Date();
+        let oneDayAgo = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000);
+        console.log("Current Date:", currentDate);
+        console.log("One Day Ago:", oneDayAgo);
     })
 }
