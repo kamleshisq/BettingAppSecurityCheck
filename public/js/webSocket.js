4fa7be0a10fdca7a803b218f29e0077ae9c713ca
jk21085815 <isquare.jayesh@gmail.com>
2023-12-21 17:51:17 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -16350,6 +16350,7 @@ socket.on('connect', () => {
 
                         }else{
                             let html = `<tr class="headDetail"><th>User name</th>`
+                            let status = true
                             for(let i = 0; i < data.Bets.length; i++){
                                 for (let j = 0; j < data.runn.length; j++) {
                                     let currentTeamData = data.Bets[i].Bets[0].selections.find(item => item.selectionName.toLowerCase().includes(data.runn[j].runner.toLowerCase()));
@@ -16364,8 +16365,11 @@ socket.on('connect', () => {
                                         winAmountForThatRUn = amount
                                     }
                                     data.runn[j].showAmount = winAmountForThatRUn
-                                    html += `<th>${data.runn[j].runner}</th>`
+                                    if(status){
+                                        html += `<th>${data.runn[j].runner}</th>`
+                                    }
                                 }
+                                status = false
                                 if(data.Bets[i].User.roleName == 'user'){
                                     html += ` <tr class="tabelBodyTr children pr${data.Id}"><td data-usename="${data.Bets[i].User.userName}">${data.Bets[i].User.userName}</td>`
                                 }else{
