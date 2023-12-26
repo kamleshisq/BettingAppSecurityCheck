5a1ecc0781664430c396380edd36b3facf483746
jk21085815 <isquare.jayesh@gmail.com>
2023-12-26 11:51:27 +0530

    CMT

--- server.js
+++ server.js
@@ -2209,51 +2209,7 @@ io.on('connection', (socket) => {
                 }
               }
         ])
-        // console.log(bets, "userIdsuserIdsuserIds")
         socket.emit("aggreat", bets)
-        // User.aggregate([
-        //     {
-        //       $match: {
-        //         parentUsers: { $elemMatch: { $eq: data.LOGINUSER._id.toString() } }
-        //       }
-        //     },
-        //     {
-        //       $group: {
-        //         _id: null,
-        //         userIds: { $push: '$_id' } 
-        //       }
-        //     }
-        //   ])
-        //     .then((userResult) => {
-        //         // console.log(userResult, "userResultuserResultuserResultuserResult")
-        //       const userIds = userResult.length > 0 ? userResult[0].userIds.map(id => id.toString()) : [];
-        //     //   console.log(userIds, "aggreataggreataggreataggreataggreataggreat")
-        //       Bet.aggregate([
-        //         {
-        //           $match: {
-        //             userId: { $in: userIds },
-        //             status: 'OPEN'
-        //           }
-        //         },
-        //         {
-        //             $group:{
-        //                 _id: '$secId',
-        //                 totalStake: { $sum: '$Stake' },
-        //                 count: { $sum: 1 }
-        //             }
-        //         }
-        //       ])
-        //         .then((betResult) => {
-        //           socket.emit("aggreat", betResult)
-        //         })
-        //         .catch((error) => {
-        //           console.error(error);
-        //         });
-        //     })
-        //     .catch((error) => {
-        //       console.error(error);
-        //     });
-
     })
 
 
