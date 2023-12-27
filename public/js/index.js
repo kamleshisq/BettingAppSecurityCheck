0de360fb89f166ef4379198926b14dc2325828cb
Jignesh <jigneshkumar142@gmail.com>
2023-12-27 11:06:39 +0530

    commite

--- public/js/index.js
+++ public/js/index.js
@@ -150,18 +150,18 @@ let sentinterval1 = setInterval(()=>{
         }
     }
     
-    if(sessionStorage.getItem('logintime')){
-        console.log(Date.now()-parseInt(sessionStorage.getItem('logintime')))
-        if(Date.now()-parseInt(sessionStorage.getItem('logintime')) >= 1000  * 30){
-            // if(pathname.startsWith('/admin')){
-            //     logout()
-            // }else{
-            // }
-            logout()
-            clearInterval(sentinterval1)
-            sessionStorage.removeItem('logintime')
-        }
-    }
+    // if(sessionStorage.getItem('logintime')){
+    //     console.log(Date.now()-parseInt(sessionStorage.getItem('logintime')))
+    //     if(Date.now()-parseInt(sessionStorage.getItem('logintime')) >= 1000  * 30){
+    //         // if(pathname.startsWith('/admin')){
+    //         //     logout()
+    //         // }else{
+    //         // }
+    //         logout()
+    //         clearInterval(sentinterval1)
+    //         sessionStorage.removeItem('logintime')
+    //     }
+    // }
 },1000)
 
 
