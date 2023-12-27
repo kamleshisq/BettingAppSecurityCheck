dfcc062e9f067303c5205f8dfece2166c9be88d6
jk21085815 <isquare.jayesh@gmail.com>
2023-12-27 09:39:10 +0530

    CMT

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7293,7 +7293,7 @@ $(document).on('click', ".logOut", function (e) {
 });
 var sentinterval1 = setInterval(function () {
   if (sessionStorage.getItem('logintime')) {
-    // console.log(Date.now()-parseInt(sessionStorage.getItem('logintime')))
+    console.log(Date.now() - parseInt(sessionStorage.getItem('logintime')));
     if (Date.now() - parseInt(sessionStorage.getItem('logintime')) >= 1000 * 60 * 30) {
       if (pathname.startsWith('/admin')) {
         (0, _logOut.logout)();
