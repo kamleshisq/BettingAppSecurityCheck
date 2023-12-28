4a5b5119b367e6e521c58e8c9dd08867a3333cd9
jk21085815 <isquare.jayesh@gmail.com>
2023-12-28 12:53:01 +0530

    CMT

--- utils/checkOddsLimit.js
+++ utils/checkOddsLimit.js
@@ -3,7 +3,15 @@ const cricketAndOtherSport = require('../utils/getSportAndCricketList');
 
 
 async function checkLimit(data){
-    console.log(data, "GOtHERE")
+    let Sports = cricketAndOtherSport()
+    let cricketList = Sports[0].gameList[0].eventList
+    let footballList = Sports[1].gameList.find(item => item.sportId == 1)
+    footballList = footballList.eventList
+    let tennisList = Sports[1].gameList.find(item => item.sportId == 2)
+    tennisList = tennisList.eventList
+    let allData = cricketList.concat(footballList, tennisList)
+    let thatMatch = allData.filter(item => item.eventData.eventId == data.eventId)
+    console.log(data,thatMatch, "GOtHERE")
 }
 
 module.exports = checkLimit
