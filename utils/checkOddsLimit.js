94ba5c2e992f81c75e5eaf946264cf75a02e3355
jk21085815 <isquare.jayesh@gmail.com>
2023-12-28 13:08:36 +0530

    CMT

--- utils/checkOddsLimit.js
+++ utils/checkOddsLimit.js
@@ -14,7 +14,16 @@ async function checkLimit(data){
         let thatMatch = allData.find(item => item.eventData.eventId == data.eventId)
         if(thatMatch){
             let IDS = [...new Set(data.ids)];
-            console.log("GOtHERE",IDS)
+            let sport_name
+            if(thatMatch.eventData.sportId == 4){
+                sport_name = 'Cricket'
+            }else if(thatMatch.eventData.sportId == 1){
+                sport_name = "Football"
+            }else if (thatMatch.eventData.sportId == 2){
+                sport_name = "Tennis"
+            }
+        
+            console.log("GOtHERE",sport_name,IDS)
 
         }else{
             return 'ERR'
