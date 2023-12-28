3204fc6805babf53c468b585d97da613d68ea4fa
jk21085815 <isquare.jayesh@gmail.com>
2023-12-28 13:00:21 +0530

    CMT

--- utils/checkOddsLimit.js
+++ utils/checkOddsLimit.js
@@ -3,15 +3,25 @@ const cricketAndOtherSport = require('../utils/getSportAndCricketList');
 
 
 async function checkLimit(data){
-    let Sports = await cricketAndOtherSport()
-    let cricketList = Sports[0].gameList[0].eventList
-    let footballList = Sports[1].gameList.find(item => item.sportId == 1)
-    footballList = footballList.eventList
-    let tennisList = Sports[1].gameList.find(item => item.sportId == 2)
-    tennisList = tennisList.eventList
-    let allData = cricketList.concat(footballList, tennisList)
-    let thatMatch = allData.find(item => item.eventData.eventId == data.eventId)
-    console.log(data,thatMatch, "GOtHERE")
+    if(data.eventId){
+        let Sports = await cricketAndOtherSport()
+        let cricketList = Sports[0].gameList[0].eventList
+        let footballList = Sports[1].gameList.find(item => item.sportId == 1)
+        footballList = footballList.eventList
+        let tennisList = Sports[1].gameList.find(item => item.sportId == 2)
+        tennisList = tennisList.eventList
+        let allData = cricketList.concat(footballList, tennisList)
+        let thatMatch = allData.find(item => item.eventData.eventId == data.eventId)
+        if(thatMatch){
+            let IDS = [...new Set(data.ids)];
+            console.log("GOtHERE",IDS)
+
+        }else{
+            return 'ERR'
+        }
+    }else{
+        return 'ERR'
+    }
 }
 
 module.exports = checkLimit
