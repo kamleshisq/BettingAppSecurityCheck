6f957265eb112578a7d0ec1f3c323f43457898a1
jk21085815 <isquare.jayesh@gmail.com>
2023-12-28 18:28:26 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9600,9 +9600,9 @@ socket.on('connect', () => {
           socket.on('checkDelay', data => {
             console.log(data, clickTime)
             if(data.delay && data.delay != 0){
-                console.log(checkTime)
+                console.log(checkTime, "123654")
                 checkTime = clickTime + data.delay * 1000
-                console.log(checkTime)
+                console.log(checkTime, "jkjkkj")
             }
 
           })
