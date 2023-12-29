b6f4b0e7271f4202e92bbb4c039afefe79f4797d
jk21085815 <isquare.jayesh@gmail.com>
2023-12-29 12:25:53 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9027,7 +9027,7 @@ socket.on('connect', () => {
                     diffStake = parseFloat(spanId)
                     var escapedId = buttonId.replace(/\./g, '\\.');
                     let IdButton = $(this).closest("tr").find(`#${escapedId}`)
-                    console.log(IdButton, buttonId)
+                    console.log(IdButton, escapedId)
                     var betValue
                     if(IdButton.hasClass('only_over_blue')|| IdButton.hasClass('odd_even_blue')){
                         betValue = parseFloat(
