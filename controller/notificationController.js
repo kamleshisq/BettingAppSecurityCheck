d343dc9805ba2fef8d9fbf63cab26f59710282f9
jk21085815 <isquare.jayesh@gmail.com>
2024-01-01 10:27:31 +0530

    CMT

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -1,18 +1,25 @@
 const notificationModel = require("../model/notificationModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const userModel = require('../model/userModel');
 
 exports.createNewNotification = catchAsync(async(req, res, next) => {
     // console.log(req.body)
     if(req.body.Sdate === req.body.Edate){
         return next(new AppError("Both starting and ending date are same that is not acceptable", 404))
     }
+
+    let ID = req.body.userId
+    let currentUser = await userModel.findById(ID)
+    if(currentUser.roleName === "Operator"){
+        ID = currentUser.parent_id
+    }
     let data = {
         title : req.body.title,
         message : req.body.message,
         startDate : new Date(req.body.Sdate),
         endDate : new Date(req.body.Edate),
-        userId : req.body.userId
+        userId : ID
     }
     // console.log(data)
     let newNotification = await notificationModel.create(data)
