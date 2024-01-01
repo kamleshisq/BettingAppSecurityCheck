6e16bc33d12611fda74799ae753fbaf3832f1523
jk21085815 <isquare.jayesh@gmail.com>
2024-01-01 14:21:59 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -9680,7 +9680,7 @@ socket.on('connect', () => {
             }
             let specificSpan 
             if(data.secId.startsWith('odd_Even_')){
-                console.log($(`#${secforFency}`))
+                console.log($(`#${secforFency}`).children("span").eq(0))
                 specificSpan = $(`#${secforFency}`).children("span").eq(0).text();
                 // console.log(`#${secforFency}`)
             }else{
