694a1c19abccaf3c17365f0d61c86c178bdc7d82
jk21085815 <isquare.jayesh@gmail.com>
2024-01-01 11:29:07 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -2419,6 +2419,9 @@ exports.getMatchDetailsPage = catchAsync(async(req, res, next) => {
 exports.getLiveMarketsPage = catchAsync(async(req, res, next) => {
     const runners = await runnerData.find()
     let currentUser =  req.currentUser
+    if(currentUser.roleName === "Operator"){
+        currentUser = await User.findById(currentUser.parent_id)
+    }
     let childrenUsername = await User.distinct('userName', { parentUsers : req.currentUser.id, role_type: 5 });
     let bets = await betModel.distinct('marketId', {userName : {$in:childrenUsername}, status: 'OPEN'})
     res.status(200).render("./liveMarket/liveMarket", {
