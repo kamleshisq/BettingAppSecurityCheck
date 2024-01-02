f9c373fa04ae18b7dbb51a9c6709c650e7971685
Jignesh <jigneshkumar142@gmail.com>
2024-01-02 15:58:34 +0530

    commote

--- controller/viewController.js
+++ controller/viewController.js
@@ -4445,11 +4445,11 @@ exports.CommissionMarkets = catchAsync(async(req, res, next) => {
     let cricketData = await getLiveGameData()
     // const cricket = cricketData[0].gameList[0].eventList
     // console.log(cricket, "cricketcricketcricket")
-    let cricket = cricketData.filter( sprtID == "4")
+    // let cricket = cricketData.filter( sprtID == "4")
     const me = req.currentUser
     res.status(200).json({
         status:'success',
-        cricketData:cricket
+        cricketData
     })
     // res.status(200).render("./commissionMarket/main",{
     //     title:"Commission Markets",
