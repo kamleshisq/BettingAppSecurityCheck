d4dbdf9fb4f3097a60870d3fb8187a2a7e423213
Jignesh <jigneshkumar142@gmail.com>
2024-01-02 15:06:45 +0530

    commote

--- controller/viewController.js
+++ controller/viewController.js
@@ -2215,7 +2215,7 @@ exports.getCricketData1 = catchAsync(async(req, res, next) => {
     fetch(fullUrl, {
         method: 'GET'
     })
-    .then(res =>res)
+    .then(res =>res.text())
     .then(result => {
         // console.log(result)
         res.status(200).json({
