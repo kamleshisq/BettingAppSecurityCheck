49f9d67cd74359ee841e5b25d7b6f8345629b3ac
jk21085815 <isquare.jayesh@gmail.com>
2024-01-02 19:38:09 +0530

    CMT

--- server.js
+++ server.js
@@ -10239,7 +10239,7 @@ io.on('connection', (socket) => {
             }else{  
                 let betId = await commissionNewModel.distinct('betId', {userName:{$in:usernameArr}, sportId:data.data.sportId, marketName:data.data.marketName, eventName:data.data.eventName})
                 console.log(betId)
-                betId.map(id => mongoose.Types.ObjectId(id))
+                betId.map(id =>new ObjectId(id))
                 filter._id = {
                     $in:betId
                 }
