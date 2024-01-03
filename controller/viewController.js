92220821e21a9202802fab63f443babe2cf1fa66
Jignesh <jigneshkumar142@gmail.com>
2024-01-03 11:28:10 +0530

    commote

--- controller/viewController.js
+++ controller/viewController.js
@@ -4518,7 +4518,7 @@ exports.getEventControllerPage = catchAsync(async(req,res,next)=>{
     footballEvents = await Promise.all(newfootballEvents);
     tennisEvents = await Promise.all(newtennisEvents);
     data = {cricketEvents,footballEvents,tennisEvents}
-    console.log(data.footballEvents, "fhdhhfdhfd")
+    console.log(data.cricketEvents, "fhdhhfdhfd")
     // data = {}
 
     return res.status(200).render("./eventController/eventController", {
