e523bc02a50b0eb354c09624c28bc3967ace2432
Jignesh <jigneshkumar142@gmail.com>
2024-01-03 19:06:17 +0530

    commite

--- controller/viewController.js
+++ controller/viewController.js
@@ -5334,7 +5334,7 @@ exports.getcommissionUser = catchAsync(async(req, res, next) => {
 })
 
 exports.getSportwisedownlinecommreport = catchAsync(async(req, res, next)=>{
-    if(!req.query.id){
+    if(Object.keys(req.query).length == 0){
         let loginuserid1 = await User.distinct("_id",{parent_id:req.currentUser._id})
         loginuserid1 = loginuserid1.toString()
         loginuserid1 = loginuserid1.split(',')
