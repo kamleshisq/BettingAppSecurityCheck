da95c0aed90c2ed303da5598a4dec4d529f445aa
jk21085815 <isquare.jayesh@gmail.com>
2024-01-05 16:18:19 +0530

    CMT

--- utils/checkOddsLimit.js
+++ utils/checkOddsLimit.js
@@ -78,7 +78,7 @@ async function checkLimit(data){
                     pushData.Limits = thatMarketLimit
                     sendData.push(pushData)
                 }
-                console.log(sendData, "hghg")
+                // console.log(sendData, "hghg")
                 return sendData
             }else{
                 return 'ERR'
