a70686c0ebc0524f11158ca5869d14b38b0c9fd9
jk21085815 <isquare.jayesh@gmail.com>
2024-01-08 20:02:47 +0530

    CMT

--- server.js
+++ server.js
@@ -9832,6 +9832,7 @@ io.on('connection', (socket) => {
 
 
     socket.on('getRefresh', async(data) => {
+        console.log(data, "datadatadata")
         let getMapBetData = await Bet.aggregate([
             {
                 $match: {
