9f9d66444e5df3200b10f3ed287ca73b103da5fb
jk21085815 <isquare.jayesh@gmail.com>
2024-01-08 12:05:16 +0530

    CMt

--- server.js
+++ server.js
@@ -77,17 +77,7 @@ const commissionNewModel = require('./model/commissioNNModel');
 
 // const { date } = require('joi');
 // const { Linter } = require('eslint');
-io.use((socket, next) => {
-    // Access the request object from the socket handshake
-    const req = socket.handshake.auth.req;
-  
-    console.log(req, "retdyfyghf")
-    // Set values on the socket
-    // socket.currentUserUnique = req.currentUserUnique; // Use the same variable name as in the middleware
-  
-    // Continue with the connection
-    next();
-  });
+
 io.on('connection', (socket) => {
     // console.log('connected to client')
     // console.log(socket.request, socket.request.app,"21212")
@@ -98,7 +88,7 @@ io.on('connection', (socket) => {
         const myVariable = socket.request.app.get('User');
         const myVariable2 = socket.request.app.get('token');
         let user = socket.request.currentUserUnique
-        console.log(user, myVariable, "tyttftftf", socket.request, socket.handshake.currentUserUnique)
+        console.log(user, myVariable, "tyttftftf")
         const ip = socket.request.app.get('Ip');
         socket.emit("loginUser", {
             loginData:myVariable,
