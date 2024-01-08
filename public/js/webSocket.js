a7b475b6169c766c9354339472f4da414e4c2788
jk21085815 <isquare.jayesh@gmail.com>
2024-01-08 14:18:01 +0530

    CMt

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -2341,9 +2341,11 @@ socket.on('connect', () => {
 
                         }
 
-                        html += `<td> ${response[i].uplinePL}</td>
-                        <td> 0</td>
-                        <td>
+                        html += `<td> ${response[i].uplinePL}</td>`
+                        // <td> 0</td>
+                        let trtdexp = $(`.trtable[data-id="${response[i]._id}"]`).find('td:eq(9)').text()
+                        console.log( trtdexp)
+                        html += `<td>
                         <div class="btn-group">
                         `
                             // if(data.currentUser.role.authorization.includes('accountControl')){
