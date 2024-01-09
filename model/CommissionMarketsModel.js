1f222f6c7702ffdad93f1dd6ea653c6f100c95f6
Jignesh <jigneshkumar142@gmail.com>
2024-01-09 17:21:50 +0530

    commit

--- model/CommissionMarketsModel.js
+++ model/CommissionMarketsModel.js
@@ -14,6 +14,10 @@ const commissionMarket = mongoose.Schema({
     date:{
         type:Date,
         required:true
+    },
+    commionstatus:{
+        type:Boolean,
+        required:true
     }
 })
 
