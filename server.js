214d0047ff9bcd0834b08a23c3896e0b04fec85a
Jignesh <jigneshkumar142@gmail.com>
2024-01-09 11:20:56 +0530

    commit

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ io.on('connection', (socket) => {
             let loginuser = await loginuserdata.findOne({userId:data.id})
             let loginstatus = true
             console.log(Date.now() , new Date(loginuser.date).getTime(),'Date.now() - new Date(loginuser.date).getTime())/(1000 * 60)')
-            if((Date.now() - new Date(loginuser.date).getTime())/(1000 * 60) >= 10){
+            if((Date.now() - new Date(loginuser.date).getTime())/(1000 * 60) >= 2){
                 loginstatus = false
                 let fullUrl =  `http://127.0.0.1:${process.env.port}/api/v1/auth/logOutSelectedUser
                 `
