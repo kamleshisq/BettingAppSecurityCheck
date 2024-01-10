c0686fbeba145bf57d4187f8a047c0d8ee610142
jk21085815 <isquare.jayesh@gmail.com>
2024-01-10 13:27:33 +0530

    CMT

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -401,6 +401,9 @@ if(await commissionMarketModel.findOne({marketId:data.data.market})){
 
     let data23 = await checkMarketWinAmount(betPlaceData)
     console.log(data23, "data23data23data23data23")
+    if(data23 > limitData.max_profit){
+        return 'Win Amount out of range'
+    }
     return 'Please try again leter market SUSPENDED'
     // console.log(betPlaceData, data, marketDetails, "betPlaceDatabetPlaceDatabetPlaceDatabetPlaceDatabetPlaceDatabetPlaceDatabetPlaceData")
 // FOR ACC STATEMENTS DATA 
