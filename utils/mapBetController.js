b92645b757f936ae892eba7b51f8f949b8ddbc0d
jk21085815 <isquare.jayesh@gmail.com>
2024-01-10 20:34:34 +0530

    CMT

--- utils/mapBetController.js
+++ utils/mapBetController.js
@@ -137,7 +137,7 @@ async function mapBet(data){
                             
                             let commissionCoin = ((commissionPer * bets[bet].Stake)/100).toFixed(4)
                             // console.log(commissionCoin, commissionPer, "commissionPercommissionPercommissionPercommissionPer")
-                            if(commissionPer > 0 && bets[bet].commionstatus){
+                            if(commissionPer > 0 ){
                                 let commissiondata = {
                                     userName : childUser.userName,
                                     userId : user.parentUsers[i],
