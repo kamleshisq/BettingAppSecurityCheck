ea6f46dc7c006b9ff861e23976be91c1525f0a5c
jk21085815 <isquare.jayesh@gmail.com>
2024-01-17 12:39:18 +0530

    CMT

--- controller/walletController.js
+++ controller/walletController.js
@@ -312,9 +312,63 @@ exports.betResult = catchAsync(async(req, res, next) =>{
         let user;
         let balance;
         if(req.body.creditAmount === 0){
-            let betforStake = await betModel.findOneAndUpdate({transactionId:req.body.transactionId},{status:"LOSS"})
-            user = await userModel.findByIdAndUpdate(req.body.userId,{$inc:{Loss:1, exposure: -parseFloat(betforStake.Stake)}})
-            balance = user.balance
+            if(req.body.gameId){
+                let betforStake = await betModel.findOneAndUpdate({transactionId:req.body.transactionId},{status:"LOSS"})
+                user = await userModel.findByIdAndUpdate(req.body.userId,{$inc:{Loss:1, exposure: -parseFloat(betforStake.Stake)}})
+                balance = user.balance
+            }else{
+                let betforStake = await betModel.findOneAndUpdate({transactionId:req.body.transactionId},{status:"LOSS",result:req.body.marketWinner})
+                let exposure = thatBet.returns
+                user = await userModel.findByIdAndUpdate(thatBet.userId, {$inc:{Loss:1, exposure:exposure, availableBalance: exposure, myPL:exposure, uplinePL: -parseFloat(exposure), pointsWL:exposure}})
+                let description = `Bet for ${bets[bet].match}/Result = ${req.body.marketWinner}/LOSS`
+                let debitAmountForP =  - exposure
+                for(let i = user.parentUsers.length - 1; i >= 1; i--){
+                    let parentUser1 = await userModel.findById(user.parentUsers[i])
+                    let parentUser2 = await userModel.findById(user.parentUsers[i - 1])
+                    let parentUser1Amount = new Decimal(parentUser1.myShare).times(debitAmountForP).dividedBy(100)
+                    let parentUser2Amount = new Decimal(parentUser1.Share).times(debitAmountForP).dividedBy(100);
+                    parentUser1Amount = parentUser1Amount.toDecimalPlaces(4);
+                    parentUser2Amount =  parentUser2Amount.toDecimalPlaces(4);
+                    await userModel.findByIdAndUpdate(user.parentUsers[i], {
+                        $inc: {
+                            downlineBalance: -exposure,
+                            myPL: parentUser1Amount,
+                            uplinePL: parentUser2Amount,
+                            lifetimePL: parentUser1Amount,
+                            pointsWL: -exposure
+                        }
+                    });
+                        
+                    if (i === 1) {
+                        await userModel.findByIdAndUpdate(user.parentUsers[i - 1], {
+                            $inc: {
+                                downlineBalance: -exposure,
+                                myPL: parentUser2Amount,
+                                lifetimePL: parentUser2Amount,
+                                pointsWL: -exposure
+                            }
+                        });
+                    }
+                    debitAmountForP = parentUser2Amount
+                }
+
+                let Acc = {
+                    "user_id":req.body.userId,
+                    "description": description,
+                    "creditDebitamount" : -req.body.debitAmount,
+                    "balance" : user.availableBalance - req.body.debitAmount,
+                    "date" : Date.now(),
+                    "userName" : user.userName,
+                    "role_type" : user.role_type,
+                    "Remark":"-",
+                    "stake": req.body.debitAmount,
+                    "transactionId":req.body.transactionId,
+                    "marketId":`${thatBet.marketId}`
+                }
+
+                accountStatement.create(Acc)
+
+            }
         }else{
             let bet = await betModel.findOneAndUpdate({transactionId:req.body.transactionId},{status:"WON", returns:req.body.creditAmount});
             user = await userModel.findByIdAndUpdate(req.body.userId,{$inc:{availableBalance: req.body.creditAmount, myPL: req.body.creditAmount, Won:1, exposure:-bet.Stake, uplinePL:-req.body.creditAmount, pointsWL:req.body.creditAmount}});
