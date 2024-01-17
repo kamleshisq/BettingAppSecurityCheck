d502aa5507fab544728dd7aa688677b2ee448351
Jignesh <jigneshkumar142@gmail.com>
2024-01-17 18:33:28 +0530

    commit

--- server.js
+++ server.js
@@ -2700,38 +2700,20 @@ io.on('connection', (socket) => {
             $lt : new Date(data.filterData.toDate)
         }
     }
-    if(data.Transaction_type === "2"){
-        filter.stake = {
-            $ne:undefined
-        }
+    let filterstatus = true
+    if(data.filterData.type === "bsettlement"){
+        filter.$expr = {
+            $eq: [{ $strLenCP: "$transactionId" }, 16]
+          }
+        filter.marketId = {$exists:true}
     }else if (data.filterData.type === "deposit"){
-        filter.stake = undefined
-        filter.accStype = undefined
-        filter.creditDebitamount = {
-            $gt: 0
-        }
+        filterstatus = false
     }else if(data.filterData.type === "withdraw"){
-        filter.stake = undefined
-        filter.accStype = undefined
-        filter.creditDebitamount = {
-            $lt: 0
-        }
+        filterstatus = false
     }else if (data.filterData.type === "sdeposit"){
-        filter.stake = undefined
-        filter.accStype = {
-            $ne:undefined
-        }
-        filter.creditDebitamount = {
-            $gt: 0
-        }
+        filterstatus = false
     }else if(data.filterData.type === "swithdraw"){
-        filter.stake = undefined
-        filter.accStype = {
-            $ne:undefined
-        }
-        filter.creditDebitamount = {
-            $lt: 0
-        }
+        filterstatus = false
     }
     // console.log(filter)
     let finalresult = []
@@ -2885,16 +2867,18 @@ io.on('connection', (socket) => {
     }
     let j = 0
     let skipvalue
-    while(finalresult.length != 20){
-        skip = data.skipid
-        skipvalue = await getmarketwiseaccdata(limit,skip)
-        skipvalue += skip
-        console.log(skipvalue,j,'skipvalue')
-        console.log(finalresult.length,'finalresult.length')
-        if(!userAccflage){
-            break
+    if(filterstatus){
+        while(finalresult.length != 20){
+            skip = data.skipid
+            skipvalue = await getmarketwiseaccdata(limit,skip)
+            skipvalue += skip
+            console.log(skipvalue,j,'skipvalue')
+            console.log(finalresult.length,'finalresult.length')
+            if(!userAccflage){
+                break
+            }
+            j++
         }
-        j++
     }
     console.log(finalresult, 'finalresult')
     socket.emit("ACCSTATEMENTUSERSIDE", {userAcc:finalresult, page,skipvalue})
