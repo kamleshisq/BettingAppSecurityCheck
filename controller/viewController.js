72932d8b3125627d70721c8bf8867392685dbf3f
jk21085815 <isquare.jayesh@gmail.com>
2024-01-18 17:34:34 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -805,22 +805,22 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                             $limit:(20 - finalresult.length)
                          }
                      ])
-
-                     let accounts = await accountStatement.aggregate([
-                        {
-                            $match:{
-                                userId:req.currentUser._id.toString(),
-                                $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId}],
-                            }
-                        },
-                        {
-                            $group:{
-                                _id:null,
-                                marketId:"$marketId",
-                                creditDebitamount:{$sum:'$creditDebitamount'},
-                            }
-                        }
-                     ])
+                     let accounts = []
+                    // accounts = await accountStatement.aggregate([
+                    //     {
+                    //         $match:{
+                    //             userId:req.currentUser._id.toString(),
+                    //             $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId}],
+                    //         }
+                    //     },
+                    //     {
+                    //         $group:{
+                    //             _id:null,
+                    //             marketId:"$marketId",
+                    //             creditDebitamount:{$sum:'$creditDebitamount'},
+                    //         }
+                    //     }
+                    //  ])
 
                      console.log('inuseracc sport book',bet,accounts)
                      if(!marketidarray.includes(bet[0]._id.marketId)){
