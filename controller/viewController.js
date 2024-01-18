c9acdd525e7b668d703eca261d7ed6ff3d4ba3a7
jk21085815 <isquare.jayesh@gmail.com>
2024-01-18 10:17:20 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -2459,8 +2459,8 @@ exports.getLiveTv = catchAsync(async(req, res, next) => {
         headers: { 
             'Content-Type': 'application/json',
             'accept': 'application/json' ,
-            "Origin":"http://app.ollscores.com/",
-            "Referer":"http://app.ollscores.com/"},
+            "Origin":"http://dev.ollscores.com/",
+            "Referer":"http://dev.ollscores.com/"},
         body:JSON.stringify(body) 
     })
     .then(res =>res.json())
