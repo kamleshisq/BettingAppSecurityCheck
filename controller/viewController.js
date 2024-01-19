7ff8febd8e6b5f48a605f49531a33548de6ab25e
jk21085815 <isquare.jayesh@gmail.com>
2024-01-19 13:53:10 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -875,7 +875,7 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
     }
     async function getmarketwiseaccdata (limit,skip){
         console.log('in getmarketwiseaccdata function')
-         let userAcc = await accountStatement.find({user_id:req.currentUser._id,date:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))},$or:[{marketId:{$exists:true}},{gameId:{$exists:true}},{eventId:{$exists:true}}],}).sort({date: -1}).skip(skip).limit(limit)
+         let userAcc = await accountStatement.find({user_id:req.currentUser._id,$or:[{marketId:{$exists:true}},{gameId:{$exists:true}},{eventId:{$exists:true}}],}).sort({date: -1}).skip(skip).limit(limit)
          let c = 0
          if(userAcc.length == 0){
             userAccflage = false
@@ -1057,6 +1057,7 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                          }
                      }
                  }else{
+                    console.log(userAcc[i], "userAcc[i]userAcc[i]userAcc[i]userAcc[i]")
                      finalresult.push(userAcc[i])
                      if(finalresult.length >= 20){
                              break
