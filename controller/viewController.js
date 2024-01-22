181a14b221248bf7112c744b3ca2fe9388bb6300
jk21085815 <isquare.jayesh@gmail.com>
2024-01-22 15:20:00 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -891,57 +891,59 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
              for(let i = 0;i<userAcc.length;i++){
                 c++
                  if(userAcc[i].gameId){
-                    if(marketidarray.includes(userAcc[i].gameId)){
-                        console.log('WORKING')
-                        continue;
-                    }
-                     let bet = await betModel.aggregate([
-                         {
-                             $match:{
-                                 userId:req.currentUser._id.toString(),
-                                 $and:[{gameId:{$exists:true}},{gameId:userAcc[i].gameId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
-                                 closingBalance:{$exists:true}
+                    // if(marketidarray.includes(userAcc[i].gameId)){
+                    //     continue;
+                    // }
+                    //  let bet = await betModel.aggregate([
+                    //      {
+                    //          $match:{
+                    //              userId:req.currentUser._id.toString(),
+                    //              $and:[{gameId:{$exists:true}},{gameId:userAcc[i].gameId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
+                    //              closingBalance:{$exists:true}
 
                                  
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                             $group:{
-                                 _id:{
-                                     gameId:"$gameId",
-                                     status:"$status",
-                                     date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
-                                 },
-                                 match:{$first:'$event'},
-                                 marketName:{$first:'$betType'},
-                                 stake:{$first:'$Stake'},
-                                 creditDebitamount:{$sum:'$returns'},
-                                 balance:{$first:'$closingBalance'},
-                                 transactionId:{$first:'$accountdetail.transactionId'}
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                            $limit:(20 - finalresult.length)
-                         }
-                     ])
-                     console.log(bet,'bet in game id')
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //          $group:{
+                    //              _id:{
+                    //                  gameId:"$gameId",
+                    //                  status:"$status",
+                    //                  date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
+                    //              },
+                    //              match:{$first:'$event'},
+                    //              marketName:{$first:'$betType'},
+                    //              stake:{$first:'$Stake'},
+                    //              creditDebitamount:{$sum:'$returns'},
+                    //              balance:{$first:'$closingBalance'},
+                    //              transactionId:{$first:'$accountdetail.transactionId'}
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //         $limit:(20 - finalresult.length)
+                    //      }
+                    //  ])
+                    //  console.log(bet,'bet in game id')
 
-                     if(bet.length !== 0 && !marketidarray.includes(bet[0]._id.gameId)){
-                         marketidarray.push(bet[0]._id.gameId)
-                         finalresult = finalresult.concat(bet)
-                         if(finalresult.length >= 20){
-                             break
-                         }
-                     }
+                    //  if(bet.length !== 0 && !marketidarray.includes(bet[0]._id.gameId)){
+                    //      marketidarray.push(bet[0]._id.gameId)
+                    //      finalresult = finalresult.concat(bet)
+                    //      if(finalresult.length >= 20){
+                    //          break
+                    //      }
+                    //  }
+                    finalresult.push(userAcc[i])
+                    if(finalresult.length >= 20){
+                            break
+                    }
                  }else if(userAcc[i].transactionId && userAcc[i].transactionId.length > 16){
                     if(marketidarray.includes(userAcc[i].marketId)){
-                        console.log('WORKING2')
                         continue;
                     }
                      let bet = await betModel.aggregate([
@@ -989,7 +991,6 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                      }
                  }else if(userAcc[i].marketId){
                     if(marketidarray.includes(userAcc[i].marketId)){
-                        console.log('WORKING3')
                         continue;
                     }
                      let bet = await betModel.aggregate([
