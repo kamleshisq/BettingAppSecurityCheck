3aec0f0517ea8a35c7aa763283dcf3d7cc2b3b8f
jk21085815 <isquare.jayesh@gmail.com>
2024-01-29 19:49:46 +0530

    cmt

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -4815,7 +4815,9 @@ socket.on('connect', () => {
                 if(data.message){
                     alert(data.message)
                     console.log(elementchange_status[0], "elementchange_statuselementchange_statuselementchange_status")
-                    
+                    let ThatELEMENT = elementchange_status[0]
+                    ThatELEMENT.prop("checked", false);
+                    ThatELEMENT.parents('.switch').removeClass("on");
                     // console.log(elementchange_status)
                 }
             }else{
