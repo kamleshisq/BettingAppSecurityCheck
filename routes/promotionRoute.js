ccacb73f622bfa7b37bd46efd4e175bc4febaad8
jk21085815 <isquare.jayesh@gmail.com>
2024-01-31 12:23:05 +0530

    CMT

--- routes/promotionRoute.js
+++ routes/promotionRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router1 = express.Router()
+const authController = require('../controller/authorizationController');
 const promotionController = require("../controller/promotionController");
 
 
 // Admin Panal
-router1.post("/createPromotionPosition", promotionController.createPosition);
-router1.post("/UpdatePromotionPosition", promotionController.updatePosition);
-router1.post('/deletePosition', promotionController.deletePosition);
+router1.post("/createPromotionPosition",authController.isLogin_Admin ,promotionController.createPosition);
+router1.post("/UpdatePromotionPosition",authController.isLogin_Admin, promotionController.updatePosition);
+router1.post('/deletePosition',authController.isLogin_Admin, promotionController.deletePosition);
 
 
 
