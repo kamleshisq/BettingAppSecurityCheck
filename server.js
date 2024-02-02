171f5c99ccf810cec8120e740e048b247dba9345
jk21085815 <isquare.jayesh@gmail.com>
2024-02-02 14:41:46 +0530

    CMT

--- server.js
+++ server.js
@@ -209,6 +209,7 @@ io.on('connection', (socket) => {
             if(data.LOGINUSER.passwordchanged){
                 socket.emit('checkpasswordreset',{status:'success'})
                 setInterval(()=>{
+                    console.log('WORKING')
                     socket.emit('checkpasswordreset',{status:'success'})
                 },1000)
             }else{
