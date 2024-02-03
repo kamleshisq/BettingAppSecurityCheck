6e341629bb4a8e8856d89cf0552947a7d9e9fe6b
5815 <95901865+5815@users.noreply.github.com>
2024-02-03 16:10:25 +0530

    CMT

--- controller/userController.js
+++ controller/userController.js
@@ -535,7 +535,7 @@ exports.changePassword = catchAsync(async(req, res, next) => {
 });
 exports.changePasswordAdmin = catchAsync(async(req, res, next) => {
     let user = await User.findOne({_id:req.currentUser._id}).select('+password')
-    console.log(req.body)
+    console.log(req.body, user)
     if(!user){
         return next(new AppError("User not found", 404))
     }
