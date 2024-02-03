bacb648c7866c8942e0b853eacc02922a60e7bac
5815 <95901865+5815@users.noreply.github.com>
2024-02-03 10:29:08 +0530

    CMT

--- controller/errorController.js
+++ controller/errorController.js
@@ -68,6 +68,7 @@ const sendErrorDev = (err, req,res) => {
                 adminStatus
             })
         }else{
+            console.log('GOTEGEFEGFGEFG')
             return res.render('./errorMessage2',{
                 statusCode : err.statusCode,
                 message,
