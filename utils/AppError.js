39adb2984044900a870e94718300a2d2c262f873
5815 <95901865+5815@users.noreply.github.com>
2024-02-03 10:35:57 +0530

    CMT

--- utils/AppError.js
+++ utils/AppError.js
@@ -1,13 +1,18 @@
+try {
 class AppError extends Error {
-    constructor(message, statusCode) {
-        console.log(message, statusCode)
-        super(message);
-        this.statusCode = statusCode;
-        this.staus = `${statusCode}`.startsWith('4') ? 'fail': 'error';
-        this.isOperational = true;
-
-        Error.captureStackTrace(this, this.constructor);
+        constructor(message, statusCode) {
+            console.log(message, statusCode)
+            super(message);
+            this.statusCode = statusCode;
+            this.staus = `${statusCode}`.startsWith('4') ? 'fail': 'error';
+            this.isOperational = true;
+    
+            Error.captureStackTrace(this, this.constructor);
+        }
+            
     }
-}
+    } catch (error) {
+        console.log(error)
+    }
 
 module.exports = AppError;
