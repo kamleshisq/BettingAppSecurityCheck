495a8444e4f75a5ccbcb8c86fac47f9fc6d2f901
5815 <95901865+5815@users.noreply.github.com>
2024-02-04 13:28:28 +0530

    CMT

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -57232,7 +57232,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52227" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56770" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- utils/mapBetController.js
+++ utils/mapBetController.js
@@ -123,34 +123,9 @@ async function mapBet(data){
                 let description = `Bet for ${bets[bet].match}/Result = ${data.result}/WON`
 
                 let debitAmountForP = debitCreditAmount
-                for(let i = user.parentUsers.length - 1; i >= 1; i--){
-                    let parentUser1 = await userModel.findById(user.parentUsers[i])
-                    let parentUser2 = await userModel.findById(user.parentUsers[i - 1])
-                    let parentUser1Amount = new Decimal(parentUser1.myShare).times(debitAmountForP).dividedBy(100)
-                    let parentUser2Amount = new Decimal(parentUser1.Share).times(debitAmountForP).dividedBy(100);
-                    parentUser1Amount = parentUser1Amount.toDecimalPlaces(4);
-                    parentUser2Amount =  parentUser2Amount.toDecimalPlaces(4);
-                    await userModel.findByIdAndUpdate(user.parentUsers[i], {
-                      $inc: {
-                          downlineBalance: debitCreditAmount,
-                          myPL: -parentUser1Amount,
-                          uplinePL: -parentUser2Amount,
-                          lifetimePL: -parentUser1Amount,
-                          pointsWL: debitCreditAmount
-                      }
-                  })
-                  if (i === 1) {
-                    await userModel.findByIdAndUpdate(user.parentUsers[i - 1], {
-                        $inc: {
-                            downlineBalance: debitCreditAmount,
-                            myPL: -parentUser2Amount,
-                            lifetimePL: -parentUser2Amount,
-                            pointsWL: debitCreditAmount
-                        }
-                    });
+                for(let i = 0; i < user.parentUsers.length; i++){
+                    console.log(user.parentUsers, "user.parentUsersuser.parentUsersuser.parentUsersuser.parentUsersuser.parentUsersuser.parentUsers")
                 }
-                  debitAmountForP = parentUser2Amount
-              }
 
 
               await accModel.create({
