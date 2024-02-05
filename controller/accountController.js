af891c2c2178d60b52d9a5e2cd4e89d4839d4c18
jk21085815 <isquare.jayesh@gmail.com>
2024-02-05 17:39:10 +0530

    CMT

--- controller/accountController.js
+++ controller/accountController.js
@@ -263,7 +263,7 @@ exports.depositSettle = catchAsync(async(req, res, next) => {
         return next(new AppError("Insufficient Credit Limit !"))
     }
     let lifeTimePl = 0
-    let debitAmountForP = childUser.myPL
+    let debitAmountForP = childUser.uplinePL
     for(let i = 1; i < childUser.parentUsers.length; i++){
         let parentUser1 = await User.findById(childUser.parentUsers[i])
         let parentUser1Amount = new Decimal(parentUser1.myShare).times(debitAmountForP).dividedBy(100)
