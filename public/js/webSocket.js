ff28d873208c761410eb9e6624bb384e366d281a
jk21085815 <isquare.jayesh@gmail.com>
2024-02-06 16:22:55 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -8400,7 +8400,7 @@ socket.on('connect', () => {
                       this.removeAttribute("data-bs-toggle");
                         parentElement.classList.add("suspended");
                         $(this).parent().find(".match-status-message").text("Suspended")
-                    }else if(check){
+                    }else if(check2){
                         parentElement.classList.add("suspended");
                           $(this).parent().find(".match-status-message").text("market settled")
                     }else if(sectionData.win_result != 'undefined' && sectionData.win_result != " " && sectionData.win_result != ""){
