99031db19bb288ff40ee3475ced81f5a180d526c
jk21085815 <isquare.jayesh@gmail.com>
2024-02-06 13:52:49 +0530

    CMT

--- utils/parentUserUpdateByBet.js
+++ utils/parentUserUpdateByBet.js
@@ -40,46 +40,38 @@ async function updateParents(user, amount, downLevelBalance){
     // }
 
 
-    let uplinePl
-                for(let i = 1; i < user.parentUsers.length; i++){
-                    if(i === 1){
-                        uplinePl = 0
-                    }
-                    let parentUser1 = await userModel.findById(user.parentUsers[i])
-                    let parentUser1Amount = new Decimal(parentUser1.myShare).times(amount).dividedBy(100)
-                    let parentUser2Amount = new Decimal(parentUser1.Share).times(amount).dividedBy(100);
-                    parentUser1Amount = parentUser1Amount.toDecimalPlaces(4);
-                    parentUser2Amount =  parentUser2Amount.toDecimalPlaces(4);
-                        await userModel.findByIdAndUpdate(user.parentUsers[i - 1], {
-                            $inc: {
-                                downlineBalance: -downLevelBalance,
-                                myPL: parentUser2Amount,
-                                pointsWL: -downLevelBalance
-                            }
-                        });
-                        await userModel.findByIdAndUpdate(user.parentUsers[i], {
-                            $inc : {
-                                uplinePL: parseFloat(parentUser2Amount) + parseFloat(uplinePl),
-                            }
-                        })
-                        if(i === user.parentUsers.length-1 ){
-                            await userModel.findByIdAndUpdate(user.parentUsers[i], {
-                                $inc: {
-                                    downlineBalance: -downLevelBalance,
-                                    myPL: parentUser1Amount,
-                                    pointsWL: -downLevelBalance
-                                }
-                            });
-                        }
-                    
-                   if(parentUser1Amount !== 0){
-                       amount = parentUser1Amount
-                   } 
-                   uplinePl = parseFloat(uplinePl) + parseFloat(parentUser2Amount)
+    let uplinePl = 0
+    for(let i = 1; i < user.parentUsers.length; i++){
+        let parentUser1 = await userModel.findById(user.parentUsers[i])
+        let parentUser1Amount = new Decimal(parentUser1.myShare).times(amount).dividedBy(100)
+        let parentUser2Amount = new Decimal(parentUser1.Share).times(amount).dividedBy(100);
+        parentUser1Amount = parentUser1Amount.toDecimalPlaces(4);
+        parentUser2Amount =  parentUser2Amount.toDecimalPlaces(4);
+        await userModel.findByIdAndUpdate(user.parentUsers[i - 1], {
+            $inc: {
+                downlineBalance: -downLevelBalance,
+                myPL: parentUser2Amount,
+                pointsWL: -downLevelBalance
+            }
+        });
+        await userModel.findByIdAndUpdate(user.parentUsers[i], {
+            $inc : {
+                uplinePL: parseFloat(parentUser2Amount) + parseFloat(uplinePl),
+            }
+        })
 
-                    // console.log(user.parentUsers, "user.parentUsersuser.parentUsersuser.parentUsersuser.parentUsersuser.parentUsersuser.parentUsers")
-                    
+        if(i === user.parentUsers.length-1 ){
+            await userModel.findByIdAndUpdate(user.parentUsers[i], {
+                $inc: {
+                    downlineBalance: -downLevelBalance,
+                    myPL: parentUser1Amount,
+                    pointsWL: -downLevelBalance
                 }
+            });
+        }
+        uplinePl = parseFloat(uplinePl) + parseFloat(parentUser2Amount)
+    }
+                
 }
 
 
