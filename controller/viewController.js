c643f35e8078d19e77c09a6efa50ee096ab00518
jk21085815 <isquare.jayesh@gmail.com>
2024-02-08 13:51:51 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -1148,40 +1148,40 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                     if(marketidarray.includes(userAcc[i].marketId)){
                         continue;
                     }
-                     let bet = await betModel.aggregate([
-                         {
-                             $match:{
-                                 userId:req.currentUser._id.toString(),
-                                 $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
-                                 closingBalance:{$exists:true},
-                                 status:{$ne:'CANCEL'}
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                             $group:{
-                                 _id:{
-                                     eventId:"$eventId",
-                                     marketId:"$marketId",
-                                     date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
-                                 },
-                                 match:{$first:'$match'},
-                                 marketName:{$first:'$marketName'},
-                                 stake:{$first:'$Stake'},
-                                 creditDebitamount:{$sum:'$returns'},
-                                 balance:{$first:'$closingBalance'},
-                                 transactionId:{$first:'$transactionId'}
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                            $limit:(20 - finalresult.length)
-                         }
-                     ])
+                    //  let bet = await betModel.aggregate([
+                    //      {
+                    //          $match:{
+                    //              userId:req.currentUser._id.toString(),
+                    //              $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
+                    //              closingBalance:{$exists:true},
+                    //              status:{$ne:'CANCEL'}
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //          $group:{
+                    //              _id:{
+                    //                  eventId:"$eventId",
+                    //                  marketId:"$marketId",
+                    //                  date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
+                    //              },
+                    //              match:{$first:'$match'},
+                    //              marketName:{$first:'$marketName'},
+                    //              stake:{$first:'$Stake'},
+                    //              creditDebitamount:{$sum:'$returns'},
+                    //              balance:{$first:'$closingBalance'},
+                    //              transactionId:{$first:'$transactionId'}
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //         $limit:(20 - finalresult.length)
+                    //      }
+                    //  ])
                      if(bet.length !== 0 && !marketidarray.includes(bet[0]._id.marketId)){
                          marketidarray.push(bet[0]._id.marketId)
                          finalresult = finalresult.concat(bet)
@@ -1193,39 +1193,39 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                     if(cancelMarketIdArray.includes(userAcc[i].cancelMarketId)){
                         continue;
                     }
-                     let bet = await betModel.aggregate([
-                         {
-                             $match:{
-                                 userId:req.currentUser._id.toString(),
-                                 $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
-                                 closingBalance:{$exists:true}
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                             $group:{
-                                 _id:{
-                                     eventId:"$eventId",
-                                     marketId:"$marketId",
-                                     date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
-                                 },
-                                 match:{$first:'$match'},
-                                 marketName:{$first:'$marketName'},
-                                 stake:{$first:'$Stake'},
-                                 creditDebitamount:{$sum:'$returns'},
-                                 balance:{$first:'$closingBalance'},
-                                 transactionId:{$first:'$transactionId'}
-                             }
-                         },
-                         {
-                            $sort:{settleDate:-1}
-                         },
-                         {
-                            $limit:(20 - finalresult.length)
-                         }
-                     ])
+                    //  let bet = await betModel.aggregate([
+                    //      {
+                    //          $match:{
+                    //              userId:req.currentUser._id.toString(),
+                    //              $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId},{settleDate:{$exists:true}},{settleDate:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
+                    //              closingBalance:{$exists:true}
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //          $group:{
+                    //              _id:{
+                    //                  eventId:"$eventId",
+                    //                  marketId:"$marketId",
+                    //                  date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
+                    //              },
+                    //              match:{$first:'$match'},
+                    //              marketName:{$first:'$marketName'},
+                    //              stake:{$first:'$Stake'},
+                    //              creditDebitamount:{$sum:'$returns'},
+                    //              balance:{$first:'$closingBalance'},
+                    //              transactionId:{$first:'$transactionId'}
+                    //          }
+                    //      },
+                    //      {
+                    //         $sort:{settleDate:-1}
+                    //      },
+                    //      {
+                    //         $limit:(20 - finalresult.length)
+                    //      }
+                    //  ])
                      if(bet.length !== 0 && !marketidarray.includes(bet[0]._id.marketId)){
                          marketidarray.push(bet[0]._id.marketId)
                          finalresult = finalresult.concat(bet)
