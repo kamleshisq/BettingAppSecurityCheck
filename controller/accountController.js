181a9d1f5a007564a09af5bfe4856027eb383277
jk21085815 <isquare.jayesh@gmail.com>
2024-02-08 16:27:05 +0530

    CMT

--- controller/accountController.js
+++ controller/accountController.js
@@ -320,7 +320,7 @@ exports.depositSettle = catchAsync(async(req, res, next) => {
     const parentUser = await User.findById(childUser.parent_id);
     req.body.amount = parseFloat(req.body.amount)
     req.body.clintPL = parseFloat(req.body.clintPL) * -1
-    // // console.log(req.body)
+    console.log(req.body)
     // // console.log(childUser)
     // if(childUser.role.role_level < parentUser.role.role_level){
     //     return next(new AppError("you do not have permission to perform this action", 404))
