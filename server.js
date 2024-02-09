d4511b2d29f8bfc8c0632858f85fa25cd28570e6
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 15:33:19 +0530

    CMT

--- server.js
+++ server.js
@@ -2314,7 +2314,7 @@ io.on('connection', (socket) => {
         // console.log(delay, "delay")
         let delayReal = 0
         if(delay[0] && delay[0].Limits!= 0 && delay[0].Limits.delay){
-            delayReal = delay[0].Limits.delay - 0.7 
+            delayReal = delay[0].Limits.delay - 0.5 
         }
         console.log(delayReal, "delayRealdelayRealdelayReal")
             setTimeout(async function(){
