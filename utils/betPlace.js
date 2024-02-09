7ecff5c1589263d73dafd0cdd18b16367f2c9fc7
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 16:22:53 +0530

    CMT

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -313,7 +313,8 @@ if(data.data.odds2){
     // if(WinAmount > limitData.max_profit){
     //     return 'Win Amount out of range'
     // }
-    console.log(exposureCHECk,check.exposureLimit,  parseFloat(exposureCHECk + creditDebitamount) > check.exposureLimit)
+    console.log(creditDebitamount)
+    console.log(exposureCHECk,check.exposureLimit,  parseFloat(exposureCHECk + creditDebitamount), check.exposureLimit , parseFloat(exposureCHECk + creditDebitamount) > check.exposureLimit)
     if(check.exposureLimit && check.exposureLimit !== 0 && parseFloat(exposureCHECk + creditDebitamount) > check.exposureLimit){
         return "Please try again later, Your exposure Limit is full dfdf"
     }
