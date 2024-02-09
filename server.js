a0594f345f92c98a25c7be4ff3f288b2c8b3d710
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 19:00:52 +0530

    CMT

--- server.js
+++ server.js
@@ -2329,7 +2329,7 @@ io.on('connection', (socket) => {
             // data.data.oldData = data.data.odds
             data.LOGINDATA.IP = data.LOGINDATA.IP.replace('::ffff:','')
             let thatMarket = marketDetails.data.items[0]
-            console.log(thatMarket)
+            console.log(thatMarket, "thatMarket")
             if(data.data.secId.startsWith('odd_Even_')){
                 if(data.data.secId == "odd_Even_Yes"){
                     let odds
