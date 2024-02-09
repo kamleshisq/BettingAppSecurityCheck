f7255047ab2fb8a7d4dc80e1d08895e2a941cc6c
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 18:51:43 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -7950,7 +7950,7 @@ socket.on('connect', () => {
                     // section = item.odds.find(odd => odd.selectionId == id);
                     // return section !== undefined;
                 });
-                console.log(thatMarket)
+                console.log(thatMarket, 'thatMarket')
                 if(this.id == `${section.selectionId}1` ){
 
                     if( section.backPrice1 == "-" || section.backPrice1 == "1,000.00" || section.backPrice1 == "0" || (macLimitStatus && macLimitStatus < section.backPrice1)){
