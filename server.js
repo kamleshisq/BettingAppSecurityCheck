e0c8f8bf5a8c54cfc30bf41c958cd5d09e90e0a5
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 12:11:26 +0530

    CMT

--- server.js
+++ server.js
@@ -7882,6 +7882,7 @@ io.on('connection', (socket) => {
                   ])
                   console.log(betData)
                   betData =  betData[0]
+                  console.log(betData.data)
                 let dataToshow = []
                 for(let i = 0; i < betData.uniqueRuns.length; i++){ 
                     if(betData.uniqueRuns.length === 1){
