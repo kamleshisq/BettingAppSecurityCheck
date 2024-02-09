46b227b7339347529090ef4b7af8d7f93e8994ea
jk21085815 <isquare.jayesh@gmail.com>
2024-02-09 10:10:50 +0530

    CMT

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -57231,7 +57231,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49899" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49968" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
