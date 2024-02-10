e38f8916a8eef7ad8f88e832eab18eae21df4ec5
5815 <95901865+5815@users.noreply.github.com>
2024-02-10 18:51:48 +0530

    CMT

--- server.js
+++ server.js
@@ -8469,7 +8469,7 @@ io.on('connection', (socket) => {
             if(commissionAmount.length != 0 && commissionAmount[0].totalCommission > 0){
                 try{
                     let commission = commissionAmount[0].totalCommission
-                    await User.findByIdAndUpdate(operationUser._id,{$inc:{availableBalance:commission, myPL:commission, uplinePL: -commission, pointsWL:-commission}})
+                    await User.findByIdAndUpdate(operationUser._id,{$inc:{availableBalance:commission, myPL:commission, uplinePL: -commission, pointsWL:commission}})
                     let parenet = await User.findByIdAndUpdate(operationUser.parent_id, {$inc:{myPL:-commission}})
                     for(let i = 0; i < user.parentUsers.length; i++){
                         await User.findByIdAndUpdate(user.parentUsers[i], {$inc :{ downlineBalance: commission}})
