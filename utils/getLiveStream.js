42d86b472ed28b750228bee1b21e5d867b831e13
jk21085815 <isquare.jayesh@gmail.com>
2024-02-12 13:04:40 +0530

    CMT

--- utils/getLiveStream.js
+++ utils/getLiveStream.js
@@ -13,8 +13,8 @@ async function getLiveStream(id, ipv4){
         headers: { 
             'Content-Type': 'application/json',
             'accept': 'application/json' ,
-            "Origin":"http://172.105.58.243",
-            "Referer":"http://172.105.58.243"},
+            "Origin":"http://ollscores.com",
+            "Referer":"http://ollscores.com"},
         body:JSON.stringify(body)
     })
     .then(res =>res.json())
