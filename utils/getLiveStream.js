64a7d684b5aeca89b0358751f36986b58f3a6f3e
jk21085815 <isquare.jayesh@gmail.com>
2024-02-12 13:17:45 +0530

    CMT

--- utils/getLiveStream.js
+++ utils/getLiveStream.js
@@ -4,7 +4,7 @@ async function getLiveStream(id, ipv4){
     let DATA
     // console.log(id)
     let body = {
-            "ipv4":`172.105.58.243`,
+            "ipv4":`${ipv4}`,
             "channel":`${id}`
         };
     var fullUrl = `https://api2.dbm9.com/api/tv-stream`;
