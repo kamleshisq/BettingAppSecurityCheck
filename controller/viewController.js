1a544d7b95fccc46985757be492a343c9e31995f
jk21085815 <isquare.jayesh@gmail.com>
2024-02-12 15:27:24 +0530

    CMT

--- controller/viewController.js
+++ controller/viewController.js
@@ -1156,6 +1156,50 @@ exports.myAccountStatment = catchAsync(async(req, res, next) => {
                              break
                          }
                      }
+                 }else if(userAcc[i].marketId && userAcc[i].rollbackMarketId){
+                    if(marketidarray.includes(userAcc[i].marketId)){
+                        continue;
+                    }
+                     let bet = await accountStatement.aggregate([
+                         {
+                             $match:{
+                                user_id:new ObjectId(req.currentUser._id.toString()),
+                                 $and:[{marketId:{$exists:true}},{marketId:userAcc[i].marketId},{date:{$gte:new Date(tomorrowFormatted),$lte:new Date(new Date(todayFormatted).getTime() + ((24 * 60*60*1000)-1))}}],
+                                 balance:{$exists:true}
+                             }
+                         },
+                         {
+                            $sort:{date:-1}
+                         },
+                         {
+                             $group:{
+                                 _id:{
+                                     eventId:"$eventId",
+                                     marketId:"$marketId",
+                                     date:{ $dateToString: { format: "%d-%m-%Y", date: "$settleDate"} }
+                                 },
+                                 match:{$first:'$event'},
+                                 marketName:{$first:'$marketType'},
+                                //  stake:{$first:'$Stake'},
+                                 creditDebitamount:{$sum:'$creditDebitamount'},
+                                 balance:{$first:'$balance'},
+                                 transactionId:{$first:'$transactionId'}
+                             }
+                         },
+                         {
+                            $sort:{settleDate:-1}
+                         },
+                         {
+                            $limit:(20 - finalresult.length)
+                         }
+                     ])
+                     if(bet.length !== 0 && !marketidarray.includes(bet[0]._id.marketId)){
+                         marketidarray.push(bet[0]._id.marketId)
+                         finalresult = finalresult.concat(bet)
+                         if(finalresult.length >= 20){
+                             break
+                         }
+                     }
                  }else if(userAcc[i].marketId){
                     if(marketidarray.includes(userAcc[i].marketId)){
                         continue;
