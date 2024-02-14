295c4d81884f64185578b7ee004a2b9c656824d7
jk21085815 <isquare.jayesh@gmail.com>
2024-02-14 17:41:04 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -10080,67 +10080,143 @@ socket.on('connect', () => {
                 console.log('Previous value:', prevValue);
                 $(this).data('prevValue', $(this).val());
                 console.log('Current value:', $(this).val());
-                let oldValue = prevValue
-                let newValue = $(this).val()
-                if(oldValue > newValue){
-                    let buttonId = $(this).closest("tr").find(".beton").attr("id").slice(0, -1);
-                    let IdButton = $(`#${buttonId}`)
-                    let spanId =  ($(this).closest("tr").find('.set-stake-form-input2').val())
-                    let Odds = parseFloat($(this).closest('tr').find(".nww-bet-slip-wrp-col1-txt-num").text())
-                    let NewStake = newValue
-                    $(this).closest("tr").find(".set-stake-form-input2").data('prevValue', `${NewStake}`);
-                    let result
-                    let element = $(this)
-                    let staleDiff = Math.abs(oldValue - newValue)
-                    if($(this).closest('tr').hasClass('back-inplaymatch')){
-                        if(IdButton.hasClass('match_odd_Blue') || IdButton.hasClass('winner_Blue')|| IdButton.hasClass("goal_Blue") ){
+                if(prevValue && $(this).val() != 0){
+                    let oldValue = prevValue
+                    let newValue = $(this).val()
+                    if(oldValue > newValue){
+                        let buttonId = $(this).closest("tr").find(".beton").attr("id").slice(0, -1);
+                        let IdButton = $(`#${buttonId}`)
+                        let spanId =  ($(this).closest("tr").find('.set-stake-form-input2').val())
+                        let Odds = parseFloat($(this).closest('tr').find(".nww-bet-slip-wrp-col1-txt-num").text())
+                        let NewStake = newValue
+                        $(this).closest("tr").find(".set-stake-form-input2").data('prevValue', `${NewStake}`);
+                        let result
+                        let element = $(this)
+                        let staleDiff = Math.abs(oldValue - newValue)
+                        if($(this).closest('tr').hasClass('back-inplaymatch')){
+                            if(IdButton.hasClass('match_odd_Blue') || IdButton.hasClass('winner_Blue')|| IdButton.hasClass("goal_Blue") ){
+                                result = (NewStake * Odds) - NewStake;
+                                resultDiff = (staleDiff * Odds) - staleDiff;
+                            }else{
+                                var escapedId = buttonId.replace(/\./g, '\\.');
+                                let IdButton = $(this).closest("tr").prev().find(`#${escapedId}`)
+                                if(IdButton.hasClass('only_over_blue')|| IdButton.hasClass('odd_even_blue')){
+                                    Odds = parseFloat(
+                                        $(this).closest("tr").find(".selection-name").text().split('@')[1]
+                                      );
+                                }
+                                result = (NewStake * Odds) / 100
+                                resultDiff = (staleDiff * Odds) / 100
+                            }
+                            let data = {
+                                result : resultDiff,
+                                element,
+                                status:false,
+                                NewStake : staleDiff,
+                                check : NewStake
+                            }
+                            Onlyminus(data)
+                        }else{
+                            result = NewStake
+                            let resultDiff = 100
+        
+                            if(IdButton.hasClass('match_odd_Red') || IdButton.hasClass('winner_Red') || IdButton.hasClass('goal_Red')){
+                                plusMinus = (100 * Odds) - 100;
+                                 
+                            }else{
+                                plusMinus = (100 * Odds) / 100
+                            }
+                            let data = {
+                                result:resultDiff,
+                                element,
+                                status:true,
+                                NewStake :  Math.abs(oldValue - newValue),
+                                plusMinus,
+                                check:NewStake
+                            }
+                            Onlyminus(data)
+                        }
+                        if(!spanId){
+                            $(this).closest("tr").find('.set-stake-form-input2').val(0)
+                            $(this)
+                            .closest("tr")
+                            .find(".c-gren")
+                            .text(0);
+                        }else if(NewStake < 0){
+                            $(this).closest("tr").find('.set-stake-form-input2').val(0)
+                            $(this)
+                            .closest("tr")
+                            .find(".c-gren")
+                            .text(0);
+                        }
+                        else{
+                            // console.log("WORKING")
+                            $(this).closest("tr").find('.set-stake-form-input2').val(NewStake)
+                            $(this)
+                            .closest("tr")
+                            .find(".c-gren")
+                            .text(result.toFixed(2));
+                        }
+                    }else{
+                        let buttonId = $(this).closest("tr").find(".beton").attr("id").slice(0, -1);
+                        let IdButton = $(`#${buttonId}`)
+                        let spanId =  ($(this).closest("tr").find('.set-stake-form-input2').val())
+                        let Odds = parseFloat($(this).closest('tr').find(".nww-bet-slip-wrp-col1-txt-num").text())
+                        let NewStake = newValue
+                        $(this).closest("tr").find(".set-stake-form-input2").data('prevValue', `${NewStake}`);
+                        let result
+                        let element = $(this)
+                        let plusMinus = 0
+                        let oldResult = $(this).closest("tr").find('.set-stake-form-input2').val()
+                        if($(this).closest('tr').hasClass('back-inplaymatch')){
+                        let diff = 0
+                        if(IdButton.hasClass('match_odd_Blue') || IdButton.hasClass('winner_Blue') || IdButton.hasClass("goal_Blue")){
                             result = (NewStake * Odds) - NewStake;
-                            resultDiff = (staleDiff * Odds) - staleDiff;
+                            diff = (100 * Odds) - 100;
                         }else{
                             var escapedId = buttonId.replace(/\./g, '\\.');
                             let IdButton = $(this).closest("tr").prev().find(`#${escapedId}`)
                             if(IdButton.hasClass('only_over_blue')|| IdButton.hasClass('odd_even_blue')){
                                 Odds = parseFloat(
                                     $(this).closest("tr").find(".selection-name").text().split('@')[1]
-                                  );
+                                );
                             }
                             result = (NewStake * Odds) / 100
-                            resultDiff = (staleDiff * Odds) / 100
+                            diff = (100 * Odds) / 100
                         }
                         let data = {
-                            result : resultDiff,
+                            result : diff,
                             element,
                             status:false,
-                            NewStake : staleDiff,
-                            check : NewStake
+                            NewStake :  Math.abs(oldValue - newValue)
                         }
-                        Onlyminus(data)
+                        marketplusminus(data)
                     }else{
                         result = NewStake
-                        let resultDiff = 100
-    
+                        let diff =  Math.abs(oldValue - newValue)
                         if(IdButton.hasClass('match_odd_Red') || IdButton.hasClass('winner_Red') || IdButton.hasClass('goal_Red')){
                             plusMinus = (100 * Odds) - 100;
-                             
+                            
                         }else{
                             plusMinus = (100 * Odds) / 100
                         }
                         let data = {
-                            result:resultDiff,
+                            result : diff ,
                             element,
                             status:true,
                             NewStake :  Math.abs(oldValue - newValue),
-                            plusMinus,
-                            check:NewStake
+                            plusMinus
                         }
-                        Onlyminus(data)
+                        marketplusminus(data)
                     }
+                    // console.log(result)
+    
                     if(!spanId){
-                        $(this).closest("tr").find('.set-stake-form-input2').val(0)
+                        $(this).closest("tr").find('.set-stake-form-input2').val(NewStake)
                         $(this)
                         .closest("tr")
                         .find(".c-gren")
-                        .text(0);
+                        .text(result.toFixed(2));
                     }else if(NewStake < 0){
                         $(this).closest("tr").find('.set-stake-form-input2').val(0)
                         $(this)
@@ -10156,81 +10232,7 @@ socket.on('connect', () => {
                         .find(".c-gren")
                         .text(result.toFixed(2));
                     }
-                }else{
-                    let buttonId = $(this).closest("tr").find(".beton").attr("id").slice(0, -1);
-                    let IdButton = $(`#${buttonId}`)
-                    let spanId =  ($(this).closest("tr").find('.set-stake-form-input2').val())
-                    let Odds = parseFloat($(this).closest('tr').find(".nww-bet-slip-wrp-col1-txt-num").text())
-                    let NewStake = newValue
-                    $(this).closest("tr").find(".set-stake-form-input2").data('prevValue', `${NewStake}`);
-                    let result
-                    let element = $(this)
-                    let plusMinus = 0
-                    let oldResult = $(this).closest("tr").find('.set-stake-form-input2').val()
-                    if($(this).closest('tr').hasClass('back-inplaymatch')){
-                    let diff = 0
-                    if(IdButton.hasClass('match_odd_Blue') || IdButton.hasClass('winner_Blue') || IdButton.hasClass("goal_Blue")){
-                        result = (NewStake * Odds) - NewStake;
-                        diff = (100 * Odds) - 100;
-                    }else{
-                        var escapedId = buttonId.replace(/\./g, '\\.');
-                        let IdButton = $(this).closest("tr").prev().find(`#${escapedId}`)
-                        if(IdButton.hasClass('only_over_blue')|| IdButton.hasClass('odd_even_blue')){
-                            Odds = parseFloat(
-                                $(this).closest("tr").find(".selection-name").text().split('@')[1]
-                            );
-                        }
-                        result = (NewStake * Odds) / 100
-                        diff = (100 * Odds) / 100
                     }
-                    let data = {
-                        result : diff,
-                        element,
-                        status:false,
-                        NewStake :  Math.abs(oldValue - newValue)
-                    }
-                    marketplusminus(data)
-                }else{
-                    result = NewStake
-                    let diff =  Math.abs(oldValue - newValue)
-                    if(IdButton.hasClass('match_odd_Red') || IdButton.hasClass('winner_Red') || IdButton.hasClass('goal_Red')){
-                        plusMinus = (100 * Odds) - 100;
-                        
-                    }else{
-                        plusMinus = (100 * Odds) / 100
-                    }
-                    let data = {
-                        result : diff ,
-                        element,
-                        status:true,
-                        NewStake :  Math.abs(oldValue - newValue),
-                        plusMinus
-                    }
-                    marketplusminus(data)
-                }
-                // console.log(result)
-
-                if(!spanId){
-                    $(this).closest("tr").find('.set-stake-form-input2').val(NewStake)
-                    $(this)
-                    .closest("tr")
-                    .find(".c-gren")
-                    .text(result.toFixed(2));
-                }else if(NewStake < 0){
-                    $(this).closest("tr").find('.set-stake-form-input2').val(0)
-                    $(this)
-                    .closest("tr")
-                    .find(".c-gren")
-                    .text(0);
-                }
-                else{
-                    // console.log("WORKING")
-                    $(this).closest("tr").find('.set-stake-form-input2').val(NewStake)
-                    $(this)
-                    .closest("tr")
-                    .find(".c-gren")
-                    .text(result.toFixed(2));
-                }
                 }
             })
           })
