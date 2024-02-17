a9951a3823a11a572c6fd8c68c9ab78fcd61af44
kamleshisq <58976792+kamleshisq@users.noreply.github.com>
2024-02-17 22:26:46 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -24097,16 +24097,22 @@ socket.on('connect', () => {
 			var SelectedWhiteLabel = $(this).val();
 			// Add or update the selwhitelbl parameter in the URL
 			var newUrl = updateUrlParameter(url, 'selwhitelbl', SelectedWhiteLabel);
-			
+			setCookie("WhiteLabelSelected",SelectedWhiteLabel,3);
 			// Redirect to the updated URL
 			window.location.href = newUrl;		
 //			window.location=url+"&selwhitelbl="+$(this).val();           
          });
 		 var urlParams = new URLSearchParams(window.location.search);
 		 var reqParam = urlParams.get('selwhitelbl');
+		 if(reqParam=="" || reqParam==null)
+		 {
+		 	reqParam = getCookie("WhiteLabelSelected");
+		 }
+		 if(reqParam=="" || reqParam==null)
+		 {
+		 	$('#whiteLabel option[value="'+reqParam+'"]').attr('selected', 'selected');
+		 }
 		 
-		 console.log(reqParam); 
-		 $('#whiteLabel option[value="'+reqParam+'"]').attr('selected', 'selected')
 		
       });
 })
@@ -24119,7 +24125,27 @@ function updateUrlParameter(url, param, value) {
     }
     return url + (url.indexOf('?') > -1 ? '&' : '?') + param + '=' + value;
 }
-
+function setCookie(cname, cvalue, hours) {
+  const d = new Date();
+  d.setTime(d.getTime() + (hours*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 function attemptReconnect() {
     const maxReconnectAttempts = 5; // Maximum number of reconnect attempts
