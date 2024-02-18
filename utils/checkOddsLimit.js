dda0c61618370bb5eaa7eeced0226334606c867c
5815 <95901865+5815@users.noreply.github.com>
2024-02-18 13:40:03 +0530

    CMT

--- utils/checkOddsLimit.js
+++ utils/checkOddsLimit.js
@@ -134,6 +134,7 @@ async function checkLimit(data){
                             }
                         }
                     }else{
+                        console.log(marketsDetails.data.items[i], "marketsDetails.data.items[i]")
                         if(marketsDetails.data.items[i].title.toLowerCase().startsWith('book') || marketsDetails.data.items[i].title.toLowerCase().startsWith('toss')){
                             thatMarketLimit = await betLimitModel.findOne({type : `${sport_name}/bookMaker`})
                         }else{
