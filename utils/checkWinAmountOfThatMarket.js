aae7f23e7367afa35e541a1901b7e89018f04fb2
jk21085815 <isquare.jayesh@gmail.com>
2024-02-20 19:27:40 +0530

    CMT

--- utils/checkWinAmountOfThatMarket.js
+++ utils/checkWinAmountOfThatMarket.js
@@ -440,9 +440,15 @@ async function checkExpoOfThatMarket( bet ){
                 }
                 betsMarketIdWise[0].selections.push(objectTopush)
             }else{
-                betsMarketIdWise[0].selections[index].totalAmount = betsMarketIdWise[0].selections[index].totalAmount + parseFloat(bet.WinAmount)
-                betsMarketIdWise[0].selections[index].exposure = betsMarketIdWise[0].selections[index].exposure + parseFloat(bet.exposure)
-                betsMarketIdWise[0].selections[index].Stake = betsMarketIdWise[0].selections[index].Stake - bet.Stake
+                if(bet.bettype2 === "LAY"){
+                    betsMarketIdWise[0].selections[index].totalAmount = betsMarketIdWise[0].selections[index].totalAmount - parseFloat(bet.WinAmount)
+                    betsMarketIdWise[0].selections[index].exposure = betsMarketIdWise[0].selections[index].exposure - parseFloat(bet.exposure)
+                    betsMarketIdWise[0].selections[index].Stake = betsMarketIdWise[0].selections[index].Stake + bet.Stake
+                }else{
+                    betsMarketIdWise[0].selections[index].totalAmount = betsMarketIdWise[0].selections[index].totalAmount + parseFloat(bet.WinAmount)
+                    betsMarketIdWise[0].selections[index].exposure = betsMarketIdWise[0].selections[index].exposure + parseFloat(bet.exposure)
+                    betsMarketIdWise[0].selections[index].Stake = betsMarketIdWise[0].selections[index].Stake - bet.Stake
+                }
             }
             let runnerData = await runnerDataModel.findOne({marketId:bet.marketId})
             if(runnerData){
