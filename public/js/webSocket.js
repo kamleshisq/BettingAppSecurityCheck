ac8f4719d8f830977c25cb8e1c9f7a201a520a23
jk21085815 <isquare.jayesh@gmail.com>
2024-02-20 17:29:15 +0530

    CMT

--- public/js/webSocket.js
+++ public/js/webSocket.js
@@ -85,7 +85,7 @@ socket.on('connect', () => {
             }
             setTimeout(function () {
                 $(".alert-popup").fadeOut(500);
-            }, 1000);
+            }, 3000);
     }
     
 
