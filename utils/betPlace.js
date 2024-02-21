30891d5a44afeda438c613b78094c05690bba2d9
jk21085815 <isquare.jayesh@gmail.com>
2024-02-21 18:13:48 +0530

    CMT

--- utils/betPlace.js
+++ utils/betPlace.js
@@ -472,7 +472,7 @@ if(await commissionMarketModel.findOne({marketId:data.data.market})){
         return 'Win Amount out of range'
     }
     let looseAmount = await checkExposureincludingBet(betPlaceData)
-    console.log(looseAmount, "looseAmountlooseAmount")
+    console.log(looseAmount, "looseAmountlooseAmountdsdsdsdsd")
     if( Math.abs(looseAmount) != 0 && (check.availableBalance < Math.abs(looseAmount))){
         return "You do not have sufficient balance for bet"
     }
